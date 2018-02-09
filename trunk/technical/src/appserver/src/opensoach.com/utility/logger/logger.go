4d62c6e8662677c23e789e47054b94ff4f40d94f
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-02-09 13:27:49 +0000

    Committing read configuration functionality

--- trunk/technical/src/appserver/src/opensoach.com/utility/logger/logger.go
+++ trunk/technical/src/appserver/src/opensoach.com/utility/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/snowzach/rotatefilehook"
@@ -10,21 +11,36 @@ import (
 
 var Instance *logrus.Logger
 
-func Init() {
+type Instance struct{
+	
+}
 
-	//logrus.SetFormatter(&logrus.JSONFormatter{})
+func Init(filename string, maxsize int, maxbackups int, maxage int, loglevel string) {
 
-	Instance := logrus.New()
+	var level logrus.Level
+	switch loglevel {
+	case "DEBUG":
+		level = logrus.DebugLevel
+		break
+	case "ERROR":
+		level = logrus.ErrorLevel
+		break
+	case "WARN":
+		level = logrus.WarnLevel
+		break
+	}
+
+	instance := logrus.New()
 
 	rotateFileHook, err := rotatefilehook.NewRotateFileHook(rotatefilehook.RotateFileConfig{
-		Filename:   "logfile.log",
-		MaxSize:    1,
-		MaxBackups: 3,
-		MaxAge:     7,
-		Level:      logrus.DebugLevel,
-		Formatter: &logrus.JSONFormatter{
-			TimestampFormat: "2006-01-02 15:04:05",
-		},
+		Filename:   filename,
+		MaxSize:    maxsize,
+		MaxBackups: maxbackups,
+		MaxAge:     maxage,
+		Level:      level,
+		//		Formatter: &logrus.JSONFormatter{
+		//			TimestampFormat: "2006-01-02 15:04:05",
+		//		},
 	})
 
 	if err != nil {
@@ -33,22 +49,38 @@ func Init() {
 		return
 	}
 
-	Instance.AddHook(rotateFileHook)
+	instance.AddHook(rotateFileHook)
 
 	//	Instance.Formatter = &logrus.JSONFormatter{
 	//		//TimestampFormat: "2006-01-02 15:04:05",
 
 	//	}
 
-	Instance.Out = os.Stdout
+	instance.Out = os.Stdout
 
-	//	for i := 0; ; i++ {
+	Instance = instance
 
-	//		fmt.Println(time.Now())
-	//		Instance.WithField("Count", i).WithField("Count1", i).Error("this is logger test", i)
-	//		Instance.WithField("Mount", i).Warn("this is logger test", i)
-	//		Instance.WithField("Xount", i).Debug("this is logger test", i)
-	//		time.Sleep(time.Microsecond * 100)
-	//	}
+	writeLog(0)
+	//writeLog(500)
+}
 
+func Debug(msg string,args ...interface) {
+	
+	
+
+}
+
+func Error(msg string,args ...interface) {
+
+}
+
+func writeLog(startpt int) {
+	for i := startpt; i < 100; i++ {
+		fmt.Println(time.Now())
+		Instance.Error("this is logger test", i)
+		Instance.WithField("Count", i).WithField("Count1", i).Error("this is logger test", i)
+		Instance.WithField("Mount", i).Warn("this is logger test", i)
+		Instance.WithField("Xount", i).Debug("this is logger test", i)
+		time.Sleep(time.Millisecond * 100)
+	}
 }
