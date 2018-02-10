5535fbaa2cb2de2e3f99cd552dff16ea67102e79
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-02-10 09:44:00 +0000

    Added session management module

--- trunk/technical/src/appserver/src/opensoach.com/utility/logger/logger.go
+++ trunk/technical/src/appserver/src/opensoach.com/utility/logger/logger.go
@@ -3,16 +3,14 @@ package logger
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/snowzach/rotatefilehook"
 )
 
-var Instance *logrus.Logger
+//var Instance *logrus.Logger
 
-type Instance struct{
-	
+type Instance struct {
 }
 
 func Init(filename string, maxsize int, maxbackups int, maxage int, loglevel string) {
@@ -58,29 +56,27 @@ func Init(filename string, maxsize int, maxbackups int, maxage int, loglevel str
 
 	instance.Out = os.Stdout
 
-	Instance = instance
+	//	Instance = instance
 
-	writeLog(0)
+	//writeLog(0)
 	//writeLog(500)
 }
 
-func Debug(msg string,args ...interface) {
-	
-	
+func Debug(msg string, args ...interface{}) {
 
 }
 
-func Error(msg string,args ...interface) {
+func Error(msg string, args ...interface{}) {
 
 }
 
-func writeLog(startpt int) {
-	for i := startpt; i < 100; i++ {
-		fmt.Println(time.Now())
-		Instance.Error("this is logger test", i)
-		Instance.WithField("Count", i).WithField("Count1", i).Error("this is logger test", i)
-		Instance.WithField("Mount", i).Warn("this is logger test", i)
-		Instance.WithField("Xount", i).Debug("this is logger test", i)
-		time.Sleep(time.Millisecond * 100)
-	}
-}
+//func writeLog(startpt int) {
+//	for i := startpt; i < 100; i++ {
+//		fmt.Println(time.Now())
+//		Instance.Error("this is logger test", i)
+//		Instance.WithField("Count", i).WithField("Count1", i).Error("this is logger test", i)
+//		Instance.WithField("Mount", i).Warn("this is logger test", i)
+//		Instance.WithField("Xount", i).Debug("this is logger test", i)
+//		time.Sleep(time.Millisecond * 100)
+//	}
+//}
