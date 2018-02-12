4d7757969e1d40fd0a0bd41aee56d3bb6632d5dc
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-02-12 15:11:48 +0000

    changed database name to all smalls.

--- trunk/technical/src/database/splive_master/data/data_1.0.0.sql
+++ trunk/technical/src/database/splive_master/data/data_1.0.0.sql
@@ -8,7 +8,7 @@ INSERT INTO `spl_master_product_tbl` VALUES (1,'SPL_HKT');
 -- Dumping data for table `spl_master_database_instance_tbl`
 --
 
-INSERT INTO `spl_master_database_instance_tbl` VALUES (1,'SPL_HKT_NODE_0001','root:welcome@tcp(localhost:3306)/SPL_HKT_NODE_0001?parseTime=true',1);
+INSERT INTO `spl_master_database_instance_tbl` VALUES (1,'spl_hkt_node_0001','root:welcome@tcp(localhost:3306)/spl_hkt_node_0001?parseTime=true',1);
 
 --
 -- Dumping data for table `spl_master_total_count_tbl`
