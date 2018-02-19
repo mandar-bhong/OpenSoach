0f356a8c7e2631e54e3b1be22b2b62653a19e69b
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-02-19 12:13:55 +0000

    Create session completed

--- trunk/technical/src/appserver/src/opensoach.com/models/dbUser.go
+++ trunk/technical/src/appserver/src/opensoach.com/models/dbUser.go
@@ -1,9 +1,9 @@
 package models
 
 type DBMasterUserRowModel struct {
-	ID           string `db:"id" json:"id"`
+	ID           int64  `db:"id" json:"id"`
 	UserName     string `db:"usr_name" json:"username"`
 	Password     string `db:"usr_password" json:"password"`
-	UserState    string `db:"usr_state" json:"state"`
-	UserCategory string `db:"usr_category" json:"category"`
+	UserState    int    `db:"usr_state" json:"state"`
+	UserCategory int    `db:"usr_category" json:"category"`
 }
