28afdb4965b86287e8c7f20906cd861cd4589ab3
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-02-22 09:04:31 +0000

    Added schema for 'spl_hkt_master' database.

---
+++ technical/src/database/spl_hkt_master/data/data_1.0.0.sql
@@ -0,0 +1,6 @@
+--
+-- Dumping data for table `spl_hkt_sp_category_tbl`
+--
+
+INSERT INTO `spl_hkt_sp_category_tbl` (`spc_name`) VALUES ('Washroom - Mens');
+INSERT INTO `spl_hkt_sp_category_tbl` (`spc_name`) VALUES ('Washroom - Womens');
