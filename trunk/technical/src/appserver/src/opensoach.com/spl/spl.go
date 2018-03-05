b4ea0ae27e7f8d2403b49760e6c45fd3d2c3dd67
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-03-05 13:24:03 +0000

    Added basic configuration reading from setting file

--- trunk/technical/src/appserver/src/opensoach.com/spl/spl.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/spl.go
@@ -1,4 +1,54 @@
 package main
 
+import (
+	"os"
+
+	"fmt"
+
+	ghelper "opensoach.com/core/helper"
+	gmodels "opensoach.com/models"
+)
+
 func main() {
+
+	isSuccess, dbconfig := ReadDBConfiguration()
+
+	if !isSuccess {
+		ShutDown(100)
+		return
+	}
+
+	ReadConfiguration(dbconfig)
+
+}
+
+func ReadDBConfiguration() (bool, *gmodels.ConfigDB) {
+
+	currentPath := ghelper.GetExeFolder()
+
+	isReadSuccess, readContent, errorMsg := ghelper.ReadFileContent(currentPath, "settings", "win.config.json")
+
+	if !isReadSuccess {
+		fmt.Printf("Unable to configuration file. Error: %s \n", errorMsg)
+		return false, nil
+	}
+
+	fmt.Println(string(readContent))
+
+	settings := gmodels.ConfigSettings{}
+	isJSONConvertSuccess := ghelper.ConvertFromJSONBytes(readContent, &settings)
+
+	if !isJSONConvertSuccess {
+		return false, nil
+	}
+
+	return isReadSuccess, &settings.DBConfig
+}
+
+func ReadConfiguration(config *gmodels.ConfigDB) {
+
+}
+
+func ShutDown(errorCode int) {
+	os.Exit(errorCode)
 }
