4f3704413e663eddbfc88b3a1cc20229c3b614b6
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-03-07 09:42:49 +0000

    Added system initialization flow

--- technical/src/appserver/src/opensoach.com/spl/spl.go
+++ technical/src/appserver/src/opensoach.com/spl/spl.go
@@ -9,41 +9,27 @@ import (
 	ghelper "opensoach.com/core/helper"
 	gmodels "opensoach.com/models"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
-
-	dbmgr "opensoach.com/core/manager/db"
-
-	"opensoach.com/core"
 	"opensoach.com/core/logger"
-	coremodels "opensoach.com/core/models"
+	"opensoach.com/spl/manager"
 )
 
 func main() {
 
-	isSuccess, dbconfig := ReadConfiguration()
+	isSuccess, dbconfig := readConfiguration()
 
 	if !isSuccess {
-		ShutDown(100)
+		shutDown(100)
 		return
 	}
 
-	ReadConfiguration()
-
-	err, configData := GetConfiguration(dbconfig)
-
-	if err != nil {
-		fmt.Println("Error occured while fetching configuration data: ", err.Error())
-	}
-
-	InitModules(configData)
+	manager.InitilizeModues(dbconfig)
 
 	logger.Context().LogDebug("Main", "Starting Application")
 
 	time.Sleep(time.Second * 5)
 }
 
-func ReadConfiguration() (bool, *gmodels.ConfigDB) {
+func readConfiguration() (bool, *gmodels.ConfigDB) {
 
 	currentPath := ghelper.GetExeFolder()
 
@@ -64,50 +50,9 @@ func ReadConfiguration() (bool, *gmodels.ConfigDB) {
 		return false, nil
 	}
 
-	return true, &settings.DBConfig
+	return true, settings.DBConfig
 }
 
-func GetConfiguration(config *gmodels.ConfigDB) (error, *[]gmodels.DBMasterConfigRowModel) {
-
-	dbEngine, err := sqlx.Connect(config.DBDriver, config.ConnectionString)
-
-	configRows := &[]gmodels.DBMasterConfigRowModel{}
-
-	if err != nil {
-		fmt.Printf("DB Error %#+v \n", err.Error())
-		return err, nil
-	}
-
-	selCtx := dbmgr.SelectProcContext{}
-	selCtx.Engine = dbEngine
-	selCtx.SPName = "sp_mst_get_configuration"
-	selCtx.Dest = configRows
-
-	selErr := selCtx.Select()
-
-	if selErr != nil {
-		fmt.Printf("DB Error %#+v \n", selErr.Error())
-		return selErr, nil
-	}
-
-	return nil, configRows
-}
-
-func InitModules(configData *[]gmodels.DBMasterConfigRowModel) {
-	InitCoreModule(configData)
-}
-
-func InitCoreModule(configData *[]gmodels.DBMasterConfigRowModel) {
-
-	logger.Init()
-	logger.SetModule("SPL")
-	logger.SetLogLevel(logger.Debug)
-	logger.SetLoggingService(logger.LoggingServiceFmt)
-
-	coreConfig := &coremodels.CoreConfig{}
-	core.Init(coreConfig)
-}
-
-func ShutDown(errorCode int) {
+func shutDown(errorCode int) {
 	os.Exit(errorCode)
 }
