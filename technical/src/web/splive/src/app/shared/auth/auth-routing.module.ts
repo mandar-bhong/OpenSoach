55e253f294ddabe00450fc048b955af114d04419
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-05 09:24:35 +0000

    moved auth-layout  module.

--- technical/src/web/splive/src/app/shared/auth/auth-routing.module.ts
+++ technical/src/web/splive/src/app/shared/auth/auth-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { AuthLayoutComponent } from '../layouts/auth-layout/auth-layout.component';
 import { LoginComponent } from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
