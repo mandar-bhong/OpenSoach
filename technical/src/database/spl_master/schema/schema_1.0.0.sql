50d5f89695f104e6ed38a08b9a343424c7eb8466
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-09 09:50:27 +0000

    Description : Added created_on and updated_on for product table.. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/database/spl_master/schema/schema_1.0.0.sql
+++ technical/src/database/spl_master/schema/schema_1.0.0.sql
@@ -22,6 +22,8 @@ use spl_master;
 CREATE TABLE `spl_master_product_tbl` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `prod_code` varchar(7) NOT NULL COMMENT 'All product code starts with prefix "SPL" and 3 character individual product suffix separated by underscore',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `prod_code_UNIQUE` (`prod_code`)
 ) ENGINE=InnoDB COMMENT='Short Name for Table: prod';
