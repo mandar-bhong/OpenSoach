e7b901b501ceee811e8b9f93029be91460f816c3
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-09 12:28:47 +0000

    Description: Modified execution structure and removed client request property Reviewed By: Self Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/models/server.go
+++ trunk/technical/src/appserver/src/opensoach.com/models/server.go
@@ -3,5 +3,5 @@ package models
 type ExecutionContext struct {
 	SessionInfo             UserSessionInfo
 	SelectedCustomerProduct int64
-	Request                 interface{}
+	RequestJSON             string
 }
