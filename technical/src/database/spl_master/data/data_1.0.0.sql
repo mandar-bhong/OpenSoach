58c03b4f04889136ad17dc7089e226d68d15b4c7
gokul.udaiwal <gokul.udaiwal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-09 12:32:08 +0000

    Description : Added corp_id_fk in spl_master_cusomer_tbl in the database. Changelog : NA. Bug ID: NA Reviewed By: Sanjay

--- technical/src/database/spl_master/data/data_1.0.0.sql
+++ technical/src/database/spl_master/data/data_1.0.0.sql
@@ -2,25 +2,25 @@
 -- Dumping data for table `spl_master_product_tbl`
 --
 
-INSERT INTO `spl_master_product_tbl` VALUES (1,'SPL_HKT');
+INSERT INTO `spl_master_product_tbl` (`id`,`prod_code`) VALUES (1,'SPL_HKT');
 
 --
 -- Dumping data for table `spl_master_database_instance_tbl`
 --
 
-INSERT INTO `spl_master_database_instance_tbl` VALUES (1,'spl_hkt_node_0001','root:welcome@tcp(localhost:3306)/spl_hkt_node_0001?parseTime=true',1);
+INSERT INTO `spl_master_database_instance_tbl` (`id`,`dbi_name`,`connection_string`,`prod_id_fk`) VALUES (1,'spl_hkt_node_0001','root:welcome@tcp(localhost:3306)/spl_hkt_node_0001?parseTime=true',1);
 
 --
 -- Dumping data for table `spl_master_total_count_tbl`
 --
 
-INSERT INTO `spl_master_total_count_tbl` VALUES (1,0,0,0,0,0,0);
+INSERT INTO `spl_master_total_count_tbl` (`id`,`cust_cnt`,`usr_cnt`,`dev_cnt`,`sp_cnt`,`dev_unallocated_cnt`,`dev_active_cnt`) VALUES (1,0,0,0,0,0,0);
 
 --
 -- Dumping data for table `spl_master_user_role_tbl`
 --
 
-INSERT INTO `spl_master_user_role_tbl` (`urole_code`,`urole_name`) VALUES ('ADMIN','Administrator');
+INSERT INTO `spl_master_user_role_tbl` (`id`,`urole_code`,`urole_name`) VALUES (1,'ADMIN','Administrator');
 
 --
 -- Dumping data for table `spl_master_config`
--- technical/src/database/spl_master/testdata/testdata_1.0.0.sql
+++ technical/src/database/spl_master/testdata/testdata_1.0.0.sql
@@ -1,44 +1,52 @@
 --
+-- Dumping data for table `spl_master_corp_tbl`
+--
+   
+INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (1,'Corporation 1','1234568','corp@gmail.com','01919191');
+
+
+
+--
 -- Dumping data for table `spl_master_customer_tbl`
 --
 
-INSERT INTO `spl_master_customer_tbl` VALUES (1,'Customer 1',1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_customer_tbl` (`id`,`corp_id_fk`,`cust_name`,`cust_state`,`cust_state_since`) VALUES (1,1,'Customer 1',1,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_cust_prod_mapping_tbl`
 --
 
-INSERT INTO `spl_master_cust_prod_mapping_tbl` VALUES (1,1,1,1,1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_cust_prod_mapping_tbl` (`id`,`cust_id_fk`,`prod_id_fk`,`dbi_id_fk`,`cpm_state`,`cpm_state_since`) VALUES (1,1,1,1,1,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_device_tbl`
 --
 
-INSERT INTO `spl_master_device_tbl` VALUES (1,'1234567890123456',1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_device_tbl` (`id`,`serialno`,`dev_state`,`dev_state_since`) VALUES (1,'1234567890123456',1,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_dev_status_tbl`
 --
 
-INSERT INTO `spl_master_dev_status_tbl` VALUES ('1', '2', UTC_TIMESTAMP, '1', UTC_TIMESTAMP, '0', UTC_TIMESTAMP);
+INSERT INTO `spl_master_dev_status_tbl` (`dev_id_fk`,`connection_state`,`connection_state_since`,`sync_state`,`sync_state_since`,`battery_level`,`battery_level_since`) VALUES ('1', '2', UTC_TIMESTAMP, '1', UTC_TIMESTAMP, '0', UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_cpm_dev_mapping_tbl`
 --
 
-INSERT INTO `spl_master_cpm_dev_mapping_tbl` VALUES (1,1);
+INSERT INTO `spl_master_cpm_dev_mapping_tbl` (`cpm_id_fk`,`dev_id_fk`) VALUES (1,1);
 
 --
 -- Dumping data for table `spl_master_servicepoint_tbl`
 --
 
-INSERT INTO `spl_master_servicepoint_tbl` VALUES (1,1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_servicepoint_tbl` (`id`,`sp_state`,`sp_state_since`) VALUES (1,1,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_cpm_sp_mapping_tbl`
 --
 
-INSERT INTO `spl_master_cpm_sp_mapping_tbl` VALUES (1,1);
+INSERT INTO `spl_master_cpm_sp_mapping_tbl` (`cpm_id_fk`,`sp_id_fk`) VALUES (1,1);
 
 --
 -- Dumping data for table `spl_master_total_count_tbl`
@@ -49,17 +57,17 @@ UPDATE `spl_master_total_count_tbl` SET `cust_cnt`='1', `usr_cnt`='1', `dev_cnt`
 --
 -- Dumping data for table `spl_master_user_tbl`
 --
-INSERT INTO `spl_master_user_tbl` VALUES (1,'admin@servicepoint.live','admin',1,1,1,UTC_TIMESTAMP);
-INSERT INTO `spl_master_user_tbl` VALUES (2,'admin@customer1.com','admin',2,null,1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (1,'admin@servicepoint.live','admin',1,1,1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (2,'admin@customer1.com','admin',2,null,1,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_usr_cpm_tbl`
 --
 
-INSERT INTO `spl_master_usr_cpm_tbl` VALUES (1,1,1,1);
+INSERT INTO `spl_master_usr_cpm_tbl` (`id`,`user_id_fk`,`cpm_id_fk`,`urole_id_fk`) VALUES (1,1,1,1);
 
 --
 -- Dumping data for table `spl_master_cust_prod_count_tbl`
 --
 
-INSERT INTO `spl_master_cust_prod_count_tbl` VALUES (1,1,1,1,1);
+INSERT INTO `spl_master_cust_prod_count_tbl` (`id`,`cpm_id_fk`,`dev_cnt`,`sp_cnt`,`usr_cnt`) VALUES (1,1,1,1,1);
