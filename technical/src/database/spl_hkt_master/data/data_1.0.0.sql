6be702f93b7be376daa0fce0e13a7d9eabb463df
gokul.udaiwal <gokul.udaiwal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-09 13:38:40 +0000

    Description : change database name Changelog : NA. Bug ID: NA Reviewed By: Sanjay

--- technical/src/database/spl_hkt_master/data/data_1.0.0.sql
+++ technical/src/database/spl_hkt_master/data/data_1.0.0.sql
@@ -1,5 +1,5 @@
 --
--- Dumping data for table `spl_hkt_sp_category_tbl`
+-- Dumping data for table `spl_hkt_master_sp_category_tbl`
 --
 
 INSERT INTO `spl_hkt_master_sp_category_tbl` (`id`,`spc_name`) VALUES (1,'Washroom - Mens');
