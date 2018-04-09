3c7e36a6c0e9b3cb3b73f9a1f475d41f6ef99398
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-09 14:34:22 +0000

    Description: Handled cross domain access Reviewed By: Self Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/webserver.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/webserver.go
@@ -2,9 +2,11 @@ package webserver
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"github.com/itsjamie/gin-cors"
 	gmodels "opensoach.com/models"
 	lmodels "opensoach.com/spl/models"
 	custmodule "opensoach.com/spl/webserver/customer"
@@ -16,6 +18,8 @@ func Init(configSetting *gmodels.ConfigSettings) error {
 
 	ginEngine := gin.Default()
 	pprof.Register(ginEngine)
+
+	enableCrossDomain(ginEngine)
 
 	webConfig := &lmodels.WebServerConfiguration{}
 	webConfig.WebHandlerEngine = ginEngine
@@ -31,4 +35,16 @@ func Init(configSetting *gmodels.ConfigSettings) error {
 	err := ginEngine.Run(fmt.Sprintf("%s", configSetting.WebConfig.ServiceAddress))
 
 	return err
+}
+
+func enableCrossDomain(c *gin.Engine) {
+	c.Use(cors.Middleware(cors.Config{
+		Origins:         "*",
+		Methods:         "GET, PUT, POST, DELETE",
+		RequestHeaders:  "Origin, Authorization, Content-Type, Cookies",
+		MaxAge:          5000 * time.Second, // original value was 50
+		Credentials:     true,
+		ValidateHeaders: true,
+		ExposedHeaders:  "Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma",
+	}))
 }
