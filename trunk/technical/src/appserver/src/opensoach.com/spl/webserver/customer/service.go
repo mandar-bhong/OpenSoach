29ba5161828303e7a49f3fec00c03e66f91ff6cf
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-10 11:56:50 +0000

    Description: added respective service and dbaccess methods Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/webserver/customer/service.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/webserver/customer/service.go
@@ -1,8 +1,13 @@
 package customer
 
 import (
+	"opensoach.com/core/logger"
 	gmodels "opensoach.com/models"
+	repo "opensoach.com/spl/repository"
+	"opensoach.com/spl/webserver/customer/dbaccess"
 )
+
+var SUB_MODULE_NAME = "SPL.Customer"
 
 type CustomerService struct {
 	ExeCtx *gmodels.ExecutionContext
@@ -10,4 +15,18 @@ type CustomerService struct {
 
 func (service CustomerService) UpdateCustomerDetails() (isSuccess bool, successErrorData interface{}) {
 	return false, nil
+}
+
+func (service CustomerService) GetCustomerDetails(customerID int64) (bool, interface{}) {
+
+	dbErr, customerDetails := dbaccess.GetCustomerById(repo.Instance().Context.Dynamic.DB, customerID)
+	if dbErr != nil {
+		logger.Context().LogError(SUB_MODULE_NAME, logger.Normal, "Database error occured while validating user.", dbErr)
+
+		errModel := gmodels.APIResponseError{}
+		errModel.Code = gmodels.MOD_OPER_ERR_DATABASE
+		return false, errModel
+	}
+
+	return true, customerDetails
 }
--- trunk/technical/src/appserver/src/opensoach.com/spl/webserver/customer/webhandler.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/webserver/customer/webhandler.go
@@ -6,19 +6,21 @@ import (
 	gmodels "opensoach.com/models"
 	"opensoach.com/spl/constants"
 	lhelper "opensoach.com/spl/helper"
-
+	lmodels "opensoach.com/spl/models"
 	repo "opensoach.com/spl/repository"
 )
 
 func registerRouters(router *gin.RouterGroup) {
-	//Register
+	
+	router.GET(constants.API_CUSTOMER_MASTER_OSU_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_MASTER_CU_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
 }
 
 func requestHandler(pContext *gin.Context) (bool, interface{}) {
 	var resultData interface{}
 	isSuccess := false
 
-	switch pContext.Request.RequestURI {
+	switch pContext.Request.URL.Path {
 
 	case constants.API_CUSTOMER_OSU_UPDATE_DETAILS:
 
@@ -35,6 +37,35 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 		break
 
 	case constants.API_CUSTOMER_CU_UPDATE_DETAILS:
+
+		break
+
+	case constants.API_CUSTOMER_MASTER_OSU_DETAILS:
+
+		recReq := lmodels.RecordIdRequest{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &recReq)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CustomerService.GetCustomerDetails(CustomerService{}, recReq.RecId)
+
+		break
+
+	case constants.API_CUSTOMER_MASTER_CU_DETAILS:
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CustomerService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.GetCustomerDetails(successErrorData.(*gmodels.ExecutionContext).SessionInfo.CustomerID)
+
 		break
 	}
 
