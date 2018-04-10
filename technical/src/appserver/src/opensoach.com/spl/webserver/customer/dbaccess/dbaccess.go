66decff6b8ba032ebea5fb673242a01a7026da1e
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-10 11:56:50 +0000

    Description: added respective service and dbaccess methods Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/customer/dbaccess/dbaccess.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/customer/dbaccess/dbaccess.go
@@ -1,1 +1,24 @@
 package dbaccess
+
+import (
+	"github.com/jmoiron/sqlx"
+	dbmgr "opensoach.com/core/manager/db"
+	"opensoach.com/spl/constants"
+	"opensoach.com/spl/constants/dbquery"
+	lmodels "opensoach.com/spl/models"
+)
+
+func GetCustomerById(dbEngine *sqlx.DB, customerId int64) (error, *lmodels.DBCustomerInfoDataModel) {
+	selDBCtx := dbmgr.SelectContext{}
+	data := &lmodels.DBCustomerInfoDataModel{}
+	selDBCtx.Engine = dbEngine
+	selDBCtx.Query = dbquery.QUERY_GET_CUSTOMER_INFO_BY_ID
+	selDBCtx.QueryType = dbmgr.Query
+	selDBCtx.Dest = data
+	selDBCtx.TableName = constants.DB_TABLE_CUSTOMER_TBL
+	selErr := selDBCtx.Get(customerId)
+	if selErr != nil {
+		return selErr, &lmodels.DBCustomerInfoDataModel{}
+	}
+	return nil, data
+}
--- technical/src/appserver/src/opensoach.com/spl/webserver/login/dbaccess/dbaccess.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/login/dbaccess/dbaccess.go
@@ -6,6 +6,7 @@ import (
 	dbmgr "opensoach.com/core/manager/db"
 
 	"opensoach.com/spl/constants"
+	"opensoach.com/spl/constants/dbquery"
 	lmodels "opensoach.com/spl/models"
 )
 
@@ -18,7 +19,7 @@ func ValidateAuth(dbEngine *sqlx.DB, username, password string) (error, *[]lmode
 	data := &[]lmodels.DBMasterUserRowModel{}
 	selDBCtx := dbmgr.SelectContext{}
 	selDBCtx.Engine = dbEngine
-	selDBCtx.Query = QUERY_MUST_CHECK_USER_LOGIN
+	selDBCtx.Query = dbquery.QUERY_MUST_CHECK_USER_LOGIN
 	selDBCtx.QueryType = dbmgr.Query
 	selDBCtx.Dest = data
 	selDBCtx.TableName = constants.DB_TABLE_USER_TBL
@@ -38,13 +39,43 @@ func GetUserAuthInfo(dbEngine *sqlx.DB, prodcode string) (error, *[]lmodels.DBUs
 	selDBCtx := dbmgr.SelectContext{}
 	data := &[]lmodels.DBUserAuthInfo{}
 	selDBCtx.Engine = dbEngine
-	selDBCtx.Query = QUERY_GET_USER_AUTH_INFO
+	selDBCtx.Query = dbquery.QUERY_GET_USER_AUTH_INFO
 	selDBCtx.QueryType = dbmgr.Query
 	selDBCtx.Dest = data
 	selDBCtx.TableName = constants.DB_TABLE_PRODUCT_TBL
 	selErr := selDBCtx.SelectByFilter(filter, "prod_code")
 	if selErr != nil {
 		return selErr, &[]lmodels.DBUserAuthInfo{}
+	}
+	return nil, data
+}
+
+func GetUserLoginInfo(dbEngine *sqlx.DB, userid int64) (error, *lmodels.DBUserInfoMinDataModel) {
+	selDBCtx := dbmgr.SelectContext{}
+	data := &lmodels.DBUserInfoMinDataModel{}
+	selDBCtx.Engine = dbEngine
+	selDBCtx.Query = dbquery.QUERY_GET_USER_LOGIN_INFO
+	selDBCtx.QueryType = dbmgr.Query
+	selDBCtx.Dest = data
+	selDBCtx.TableName = constants.DB_TABLE_USER_DETAILS_TBL
+	selErr := selDBCtx.Get(userid)
+	if selErr != nil {
+		return selErr, &lmodels.DBUserInfoMinDataModel{}
+	}
+	return nil, data
+}
+
+func GetCustomerLoginInfo(dbEngine *sqlx.DB, customerId int64) (error, *lmodels.DBCustomerLoginInfoDataModel) {
+	selDBCtx := dbmgr.SelectContext{}
+	data := &lmodels.DBCustomerLoginInfoDataModel{}
+	selDBCtx.Engine = dbEngine
+	selDBCtx.Query = dbquery.QUERY_GET_CUSTOMER_LOGIN_INFO
+	selDBCtx.QueryType = dbmgr.Query
+	selDBCtx.Dest = data
+	selDBCtx.TableName = constants.DB_TABLE_CUSTOMER_TBL
+	selErr := selDBCtx.Get(customerId)
+	if selErr != nil {
+		return selErr, &lmodels.DBCustomerLoginInfoDataModel{}
 	}
 	return nil, data
 }
