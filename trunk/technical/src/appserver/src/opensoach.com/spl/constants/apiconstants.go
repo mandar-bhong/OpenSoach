419b8cc3dac57270554fa3a267f57a3b5de6e7a2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-10 11:39:39 +0000

    Description: Added API constants for Login and Customer Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/constants/apiconstants.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/constants/apiconstants.go
@@ -1,6 +1,12 @@
 package constants
 
-const API_USER_LOGIN = "/login"
+const API_USER_LOGIN = "/api/v1/login"
+const API_USER_LOGIN_INFO = "/api/v1/user/info/login"
+const API_CUSTOMER_LOGIN_INFO = "/api/cu/v1/customer/info/login"
+const API_USER_LOGOUT = "/api/v1/logout"
 
 const API_CUSTOMER_OSU_UPDATE_DETAILS = "/api/osu/v1/customer/update/details"
 const API_CUSTOMER_CU_UPDATE_DETAILS = "/api/cu/v1/customer/update/details"
+
+const API_CUSTOMER_MASTER_OSU_DETAILS = "/api/osu/v1/customer/info/master"
+const API_CUSTOMER_MASTER_CU_DETAILS = "/api/cu/v1/customer/info/master"
