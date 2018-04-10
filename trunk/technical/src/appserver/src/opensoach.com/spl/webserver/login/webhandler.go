09b7ee9f7abc3c9a42d89b4a982ad221bd70c0a7
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-10 11:51:48 +0000

    Description: added api for login Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/webserver/login/webhandler.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/webserver/login/webhandler.go
@@ -1,13 +1,13 @@
 package login
 
 import (
-	
 	"github.com/gin-gonic/gin"
-	
+
 	gmodels "opensoach.com/models"
 	"opensoach.com/spl/constants"
 	lhelper "opensoach.com/spl/helper"
 	lmodels "opensoach.com/spl/models"
+	repo "opensoach.com/spl/repository"
 )
 
 func registerRouters(router *gin.RouterGroup) {
@@ -15,7 +15,9 @@ func registerRouters(router *gin.RouterGroup) {
 	//logger.Instance.Debug("Registering log module")
 
 	router.POST(constants.API_USER_LOGIN, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
-
+	router.GET(constants.API_USER_LOGIN_INFO, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_USER_LOGOUT, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_LOGIN_INFO, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
 	return
 }
 
@@ -27,7 +29,7 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 
 	switch pContext.Request.RequestURI {
 
-	case constants.API_USER_LOGIN:
+	case pContext.Request.URL.Path:
 
 		authReq := lmodels.AuthRequest{}
 
@@ -42,6 +44,38 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 		}
 
 		isSuccess, resultData = AuthService.Auth(AuthService{}, authReq.UserName, authReq.Password, authReq.ProdCode)
+
+		break
+
+	case constants.API_USER_LOGIN_INFO:
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = AuthService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.GetUserLoginDetails()
+
+		break
+
+	case constants.API_USER_LOGOUT:
+		isLogoutSuccess := AuthService.UserLogout(AuthService{}, pContext)
+		return isLogoutSuccess, nil
+
+	case constants.API_CUSTOMER_LOGIN_INFO:
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = AuthService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.GetCustomerLoginDetails()
 
 		break
 
