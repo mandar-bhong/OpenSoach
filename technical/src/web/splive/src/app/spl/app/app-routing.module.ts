e7c1a8de201ff44fab0353a098d58bb635b0c97a
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-12 08:03:52 +0000

    Description : Added missing files in spl. moved logo files. Changelog : NA. Bug ID: NA Reviewed By: Self

---
+++ technical/src/web/splive/src/app/spl/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from '../../shared/auth-guard';
+import { AppContainerComponent } from '../../shared/layouts/app-layout/app-container/app-container.component';
+import { AuthLayoutComponent } from '../../shared/layouts/auth-layout/auth-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        canActivate: [AuthGuard],
+        component: AppContainerComponent,
+        children: [
+          {
+            path: 'devices',
+            loadChildren: '../../shared/modules/devices/devices.module#DevicesModule'
+          },
+        ]
+      },
+    ]
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    loadChildren: '../../shared/modules/auth/auth.module#AuthModule'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
