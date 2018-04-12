966d403f3f16c745f049097e90a83fe71915d617
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-12 09:35:23 +0000

    Description: Added queries respective to apis dbquery/customer.go              Modified login user query added condition userid              Added respective api constants and dbconstants
    Reviewed By: Mandar
    Change Log: 
    Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/constants/dbquery/customer.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/constants/dbquery/customer.go
@@ -1,5 +1,13 @@
 package dbquery
 
-const QUERY_GET_CUSTOMER_INFO_BY_ID = `Select id,corp_id_fk,cust_name,cust_state,cust_state_since,created_on,updated_on 
-										FROM spl_master_customer_tbl 
-										WHERE id = ?`
+const QUERY_GET_CUSTOMER_TABLE_INFO_BY_ID = `Select id,corp_id_fk,cust_name,cust_state,cust_state_since,created_on,updated_on 
+											FROM spl_master_customer_tbl 
+											WHERE id = ?`
+
+const QUERY_GET_CUSTOMER_DETAILS_TABLE_INFO_BY_ID = `SELECT cust_id_fk,poc1_name,poc1_email_id,poc1_mobile_no,poc2_name,poc2_email_id,poc2_mobile_no,address,address_state,address_city,address_pincode 
+											FROM spl_master_cust_details_tbl
+											WHERE cust_id_fk = ?`
+
+const QUERY_GET_CORP_TABLE_INFO_BY_CUSTOMER_ID = `SELECT corp.id,corp_name,corp_mobile_no,corp_email_id,corp_landline_no,corp.created_on,corp.updated_on from spl_master_corp_tbl corp
+											INNER JOIN spl_master_customer_tbl cust  ON corp.id = cust.corp_id_fk
+											WHERE cust.id = ?`
