e5f7485a6a88d4f5a00921f5a5cf794e1508c1b7
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-12 09:31:54 +0000

    Description: added api customer/info/master,customer/info/details, corporate/info for CU and OSU user
    Reviewed By: Mandar
    Change Log: 
    Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/customer/webhandler.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/customer/webhandler.go
@@ -11,9 +11,13 @@ import (
 )
 
 func registerRouters(router *gin.RouterGroup) {
-	
-	router.GET(constants.API_CUSTOMER_MASTER_OSU_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
-	router.GET(constants.API_CUSTOMER_MASTER_CU_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+
+	router.GET(constants.API_CUSTOMER_OSU_INFO_MASTER, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_CU_INFO_MASTER, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_OSU_INFO_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_CU_INFO_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_OSU_CORPORATE_INFO, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CUSTOMER_CU_CORPORATE_INFO, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
 }
 
 func requestHandler(pContext *gin.Context) (bool, interface{}) {
@@ -40,7 +44,7 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 
 		break
 
-	case constants.API_CUSTOMER_MASTER_OSU_DETAILS:
+	case constants.API_CUSTOMER_OSU_INFO_MASTER:
 
 		recReq := lmodels.RecordIdRequest{}
 
@@ -50,11 +54,11 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 			return false, successErrorData
 		}
 
-		isSuccess, resultData = CustomerService.GetCustomerDetails(CustomerService{}, recReq.RecId)
+		isSuccess, resultData = CustomerService.GetCustomerInfo(CustomerService{}, recReq.RecId)
 
 		break
 
-	case constants.API_CUSTOMER_MASTER_CU_DETAILS:
+	case constants.API_CUSTOMER_CU_INFO_MASTER:
 
 		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
 
@@ -64,9 +68,66 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 
 		isSuccess, resultData = CustomerService{
 			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
-		}.GetCustomerDetails(successErrorData.(*gmodels.ExecutionContext).SessionInfo.CustomerID)
+		}.GetCustomerInfo(successErrorData.(*gmodels.ExecutionContext).SessionInfo.CustomerID)
 
 		break
+
+	case constants.API_CUSTOMER_OSU_INFO_DETAILS:
+
+		recReq := lmodels.RecordIdRequest{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &recReq)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CustomerService.GetCustomerDetailsInfo(CustomerService{}, recReq.RecId)
+
+		break
+
+	case constants.API_CUSTOMER_CU_INFO_DETAILS:
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CustomerService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.GetCustomerDetailsInfo(successErrorData.(*gmodels.ExecutionContext).SessionInfo.CustomerID)
+
+		break
+
+	case constants.API_CUSTOMER_OSU_CORPORATE_INFO:
+
+		recReq := lmodels.RecordIdRequest{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &recReq)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CustomerService.GetCorpInfo(CustomerService{}, recReq.RecId)
+
+		break
+
+	case constants.API_CUSTOMER_CU_CORPORATE_INFO:
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+
+		if isPrepareExeSuccess == false {
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CustomerService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.GetCorpInfo(successErrorData.(*gmodels.ExecutionContext).SessionInfo.CustomerID)
+
+		break
+
 	}
 
 	return isSuccess, resultData
