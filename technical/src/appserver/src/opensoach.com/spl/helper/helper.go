5932dec65e5ed8d03206477ef1cd4fb704f7e201
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-14 10:18:18 +0000

    Description: Added method to get db tag from json tag Reviewed By: Self Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/helper/helper.go
+++ technical/src/appserver/src/opensoach.com/spl/helper/helper.go
@@ -1,1 +1,9 @@
 package helper
+
+import (
+	"opensoach.com/core/manager/db"
+)
+
+func GetDBTagFromJSONTag(model interface{}, jsonTag string) string {
+	return db.GetDBTagFromJSONTag(model, jsonTag)
+}
