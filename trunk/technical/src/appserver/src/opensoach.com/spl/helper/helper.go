f8b5cef5f81430450c796c5672f40e7bba98aa7a
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-14 10:24:34 +0000

    Description: Added helper methods to validate db parameters Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/helper/helper.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/helper/helper.go
@@ -1,8 +1,13 @@
 package helper
 
 import (
+	ghelper "opensoach.com/core/helper"
 	"opensoach.com/core/manager/db"
 )
+
+func DBQueryParamValidate(queryInput string) bool {
+	return ghelper.DBQueryParamValidate(queryInput, true)
+}
 
 func GetDBTagFromJSONTag(model interface{}, jsonTag string) string {
 	return db.GetDBTagFromJSONTag(model, jsonTag)
