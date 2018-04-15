c2ec2ecd8488ad4e0b2dd6ab89db8213767defb3
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-15 08:14:22 +0000

    Description: Added build install script Reviewed By: Self Change Log: Bug ID:

---
+++ trunk/technical/tools/scripts/installbuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+echo "Curent Path:" pwd
+
+export builddir=/opt/build/spl
+
+cd $builddir
+
+chmod  +777 $builddir/SPLBuild/spl 
+
+
+service spl stop
+
+tar xvzf SPLBuild.tar.gz
+
+
+#executing command from shell prompt 
+mysql -u root -pwelcome -Bse "drop database spl_master;"
+mysql -u root -pwelcome -Bse "create database spl_master;"
+#session will be terminiated hence executing all the commands in same session
+mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/SPLBuild/spl_master/schema/schema_1.0.0.sql;source /opt/build/spl/SPLBuild/spl_master/data/data_1.0.0.sql;source /opt/build/spl/SPLBuild/spl_master/testdata/testdata_1.0.0.sql;"
+
+service spl start
+
