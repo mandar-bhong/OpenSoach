0e6462053aabf882ca787d018b6acc99444ddf78
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-16 13:26:44 +0000

    Description: Added user apis, add user api,add or update user details api , change user state api, change user password api Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/user/dbaccess/dbaccess.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/user/dbaccess/dbaccess.go
@@ -7,6 +7,8 @@ import (
 	lmodels "opensoach.com/spl/models"
 )
 
+var SUB_MODULE_NAME = "SPL.User.DB"
+
 func SplMasterUserTableInsert(dbConn string, insrtStruct lmodels.DBSplMasterUserTableRowModel) (error, int64) {
 	insDBCtx := dbmgr.InsertContext{}
 	insDBCtx.DBConnection = dbConn
@@ -19,4 +21,90 @@ func SplMasterUserTableInsert(dbConn string, insrtStruct lmodels.DBSplMasterUser
 		return insErr, 0
 	}
 	return nil, insDBCtx.InsertID
+}
+
+func GetSplMasterUserDetailsTableById(dbConn string, userid int64) (error, *[]lmodels.DBSplMasterUsrDetailsTableRowModel) {
+	selDBCtx := dbmgr.SelectContext{}
+	data := &[]lmodels.DBSplMasterUsrDetailsTableRowModel{}
+	selDBCtx.DBConnection = dbConn
+	selDBCtx.Query = dbquery.QUERY_SPL_MASTER_USR_DETAILS_TABLE_SELECT_BY_ID
+	selDBCtx.QueryType = dbmgr.Query
+	selDBCtx.Dest = data
+
+	selErr := selDBCtx.Select(userid)
+	if selErr != nil {
+		return selErr, &[]lmodels.DBSplMasterUsrDetailsTableRowModel{}
+	}
+	return nil, data
+}
+
+func SplMasterUserDetailsTableInsert(dbConn string, insrtStruct lmodels.DBSplMasterUsrDetailsTableRowModel) (error, int64) {
+	insDBCtx := dbmgr.InsertContext{}
+	insDBCtx.DBConnection = dbConn
+	insDBCtx.Args = insrtStruct
+	insDBCtx.Query = dbquery.QUERY_SPL_MASTER_USR_DETAILS_TABLE_INSERT
+	insDBCtx.QueryType = dbmgr.Query
+	insDBCtx.TableName = constants.DB_TABLE_USER_DETAILS_TBL
+	insErr := insDBCtx.Insert()
+	if insErr != nil {
+		return insErr, 0
+	}
+	return nil, insDBCtx.InsertID
+}
+
+func SplMasterUserDetailsTableUpdate(dbConn string, updtStruct lmodels.DBSplMasterUsrDetailsTableRowModel) (error, int64) {
+	updtDBCtx := dbmgr.UpdateDeleteContext{}
+	updtDBCtx.DBConnection = dbConn
+	updtDBCtx.Args = updtStruct
+	updtDBCtx.Query = dbquery.QUERY_SPL_MASTER_USR_DETAILS_TABLE_UPDATE
+	updtDBCtx.QueryType = dbmgr.Query
+	updtDBCtx.TableName = constants.DB_TABLE_USER_DETAILS_TBL
+	updateErr := updtDBCtx.Update()
+	if updateErr != nil {
+		return updateErr, 0
+	}
+	return nil, updtDBCtx.AffectedRows
+}
+
+func SplMasterUserTableUpdateState(dbConn string, updtStruct lmodels.DBSplMasterUserTableRowModel) (error, int64) {
+	updateCtx := dbmgr.UpdateDeleteContext{}
+	updateCtx.DBConnection = dbConn
+	updateCtx.Args = updtStruct
+	updateCtx.Query = dbquery.QUERY_SPL_MASTER_USER_TABLE_UPDATE_STATE
+	updateCtx.QueryType = dbmgr.Query
+	updateCtx.TableName = constants.DB_TABLE_USER_TBL
+	updateErr := updateCtx.Update()
+	if updateErr != nil {
+		return updateErr, 0
+	}
+	return nil, updateCtx.AffectedRows
+}
+
+func CheckOldPasswordExists(dbConn string, userid int64, oldPass string) (error, *[]lmodels.DBSplMasterUserTableRowModel) {
+	selDBCtx := dbmgr.SelectContext{}
+	data := &[]lmodels.DBSplMasterUserTableRowModel{}
+	selDBCtx.DBConnection = dbConn
+	selDBCtx.Query = dbquery.QUERY_SPL_MASTER_USER_TABLE_SELECT_BY_ID_PASSWORD
+	selDBCtx.QueryType = dbmgr.Query
+	selDBCtx.Dest = data
+
+	selErr := selDBCtx.Select(userid, oldPass)
+	if selErr != nil {
+		return selErr, &[]lmodels.DBSplMasterUserTableRowModel{}
+	}
+	return nil, data
+}
+
+func SplMasterUserTableUpdatePassword(dbConn string, updtStruct lmodels.DBSplMasterUserTableRowModel) (error, int64) {
+	updateCtx := dbmgr.UpdateDeleteContext{}
+	updateCtx.DBConnection = dbConn
+	updateCtx.Args = updtStruct
+	updateCtx.Query = dbquery.QUERY_SPL_MASTER_USER_TABLE_CHANGE_PASSWORD
+	updateCtx.QueryType = dbmgr.Query
+	updateCtx.TableName = constants.DB_TABLE_USER_TBL
+	updateErr := updateCtx.Update()
+	if updateErr != nil {
+		return updateErr, 0
+	}
+	return nil, updateCtx.AffectedRows
 }
--- technical/src/appserver/src/opensoach.com/spl/webserver/user/webhandler.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/user/webhandler.go
@@ -2,48 +2,135 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"opensoach.com/core/logger"
 
 	gmodels "opensoach.com/models"
 	"opensoach.com/spl/constants"
 	lhelper "opensoach.com/spl/helper"
+	lmodels "opensoach.com/spl/models"
 	repo "opensoach.com/spl/repository"
 )
 
 func registerRouters(router *gin.RouterGroup) {
 	router.POST(constants.API_USER_OSU_ADD, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_USER_OSU_UPDATE_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_USER_CU_UPDATE_DETAILS, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_USER_OSU_UPDATE_STATE, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_USER_CU_UPDATE_STATE, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_USER_CHANGE_PASSWORD, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
 }
 
 func requestHandler(pContext *gin.Context) (bool, interface{}) {
 	var resultData interface{}
 	isSuccess := false
 
+	logger.Context().WithField("Request: ", pContext.Request.URL.Path).LogDebug(SUB_MODULE_NAME, logger.Normal, "API request received.")
+
 	switch pContext.Request.URL.Path {
 
-	case constants.API_USER_OSU_UPDATE_DETAILS:
+	case constants.API_USER_OSU_ADD:
 
-		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+		userReqData := lmodels.DBSplMasterUserTableRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &userReqData)
 
 		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
 			return false, successErrorData
 		}
 
 		isSuccess, resultData = UserService{
 			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
-		}.UpdateUserDetails()
+		}.AddUser(userReqData)
+
+		break
+
+	case constants.API_USER_OSU_UPDATE_DETAILS:
+
+		usrDetailsReqData := lmodels.DBSplMasterUsrDetailsTableRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &usrDetailsReqData)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = UserService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.UpdateUserDetails(usrDetailsReqData)
 
 		break
 
 	case constants.API_USER_CU_UPDATE_DETAILS:
 
-		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+		usrDetailsReqData := lmodels.DBSplMasterUsrDetailsTableRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &usrDetailsReqData)
 
 		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		usrDetailsReqData.UsrIdFk = successErrorData.(*gmodels.ExecutionContext).SessionInfo.UserID
+
+		isSuccess, resultData = UserService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.UpdateUserDetails(usrDetailsReqData)
+
+		break
+
+	case constants.API_USER_OSU_UPDATE_STATE:
+
+		userReqData := lmodels.DBSplMasterUserTableRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &userReqData)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
 			return false, successErrorData
 		}
 
 		isSuccess, resultData = UserService{
 			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
-		}.UpdateUserDetails()
+		}.UpdateUserState(userReqData)
+
+		break
+
+	case constants.API_USER_CU_UPDATE_STATE:
+
+		userReqData := lmodels.DBSplMasterUserTableRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &userReqData)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		userReqData.Id = successErrorData.(*gmodels.ExecutionContext).SessionInfo.UserID
+
+		isSuccess, resultData = UserService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.UpdateUserState(userReqData)
+
+		break
+
+	case constants.API_USER_CHANGE_PASSWORD:
+
+		userReqData := lmodels.RecordChangePassRequest{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &userReqData)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = UserService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.ChangeUserPassword(userReqData, successErrorData.(*gmodels.ExecutionContext).SessionInfo.UserID)
 
 		break
 
