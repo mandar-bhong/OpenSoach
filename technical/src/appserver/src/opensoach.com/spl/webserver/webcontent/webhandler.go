d50c3734414becc3b28e96e67f25a264dcead715
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-16 11:18:11 +0000

    Description: Added shared hander for web content Reviewed By: Self Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/webcontent/webhandler.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/webcontent/webhandler.go
@@ -7,5 +7,6 @@ import (
 func registerRouters(router *gin.RouterGroup) {
 	router.Static("/web", "./web/spl/")
 	router.Static("/assets/", "./web/spl/assets")
+	router.Static("/shared/", "./web/spl/shared")
 	router.StaticFile("/", "./web/spl/index.html")
 }
