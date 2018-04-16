7af965909d899a4d4f1383580f9b8c5c2d11d305
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-16 16:00:22 +0000

    Description: Added custome data list api Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/customer/service.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/customer/service.go
@@ -3,6 +3,7 @@ package customer
 import (
 	"opensoach.com/core/logger"
 	gmodels "opensoach.com/models"
+	lmodels "opensoach.com/spl/models"
 	repo "opensoach.com/spl/repository"
 	"opensoach.com/spl/webserver/customer/dbaccess"
 )
@@ -27,7 +28,7 @@ func (service CustomerService) GetCustomerInfo(customerID int64) (bool, interfac
 		errModel.Code = gmodels.MOD_OPER_ERR_DATABASE
 		return false, errModel
 	}
-
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Successfully fetched customer master details")
 	return true, customerDetails
 }
 
@@ -41,7 +42,7 @@ func (service CustomerService) GetCustomerDetailsInfo(customerID int64) (bool, i
 		errModel.Code = gmodels.MOD_OPER_ERR_DATABASE
 		return false, errModel
 	}
-
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Successfully fetched customer details")
 	return true, customerDetails
 }
 
@@ -55,6 +56,45 @@ func (service CustomerService) GetCorpInfo(customerID int64) (bool, interface{})
 		errModel.Code = gmodels.MOD_OPER_ERR_DATABASE
 		return false, errModel
 	}
-
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Successfully fetched customer corporate details.")
 	return true, customerDetails
+}
+
+func (CustomerService) GetCustomerDataList(custListReqData lmodels.DataListRequest) (bool, interface{}) {
+
+	custListResData := lmodels.DataListResponse{}
+
+	filterModel := custListReqData.Filter.(*lmodels.DBSearchCustomerDataModel)
+
+	dbErr, customerFilteredRecords := dbaccess.GetSplMasterCustomerTableTotalFilteredRecords(repo.Instance().Context.Master.DBConn, filterModel)
+	if dbErr != nil {
+		logger.Context().LogError(SUB_MODULE_NAME, logger.Normal, "Database error occured while validating user.", dbErr)
+
+		errModel := gmodels.APIResponseError{}
+		errModel.Code = gmodels.MOD_OPER_ERR_DATABASE
+		return false, errModel
+	}
+	dbCustomerFilteredRecords := *customerFilteredRecords
+	custListResData.FilteredRecords = dbCustomerFilteredRecords.TotalRecords
+
+	CurrentPage := custListReqData.CurrentPage
+	startingRecord := ((CurrentPage - 1) * custListReqData.Limit)
+
+	dbErr, customerFilterData := dbaccess.SplMasterCustomerTableSelectByFilter(repo.Instance().Context.Master.DBConn, custListReqData, filterModel, startingRecord)
+	if dbErr != nil {
+		logger.Context().LogError(SUB_MODULE_NAME, logger.Normal, "Database error occured while validating user.", dbErr)
+
+		errModel := gmodels.APIResponseError{}
+		errModel.Code = gmodels.MOD_OPER_ERR_DATABASE
+		return false, errModel
+	}
+
+	dbCustomerFilterRecord := *customerFilterData
+
+	for i := 0; i < len(dbCustomerFilterRecord); i++ {
+		custListResData.Records = append(custListResData.Records, dbCustomerFilterRecord[i])
+	}
+
+	return true, custListResData
+
 }
