0508e724826bd4548a068001d6099bbbf3501ea1
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-17 08:35:02 +0000

    Description: Added user models Reviewed By: Change Log: Bug ID:

--- trunk/technical/src/web/splive/src/app/shared/models/api/user-models.ts
+++ trunk/technical/src/web/splive/src/app/shared/models/api/user-models.ts
@@ -2,3 +2,29 @@ export class UserLoginInfoResponse {
     fname: string;
     lname: string;
 }
+
+export class UserAddRequest {
+    usrname: string;
+    usrpassword: string;
+    usrcategory: number;
+	uroleidfk: number;
+	usrstate: number;
+}
+
+export class UserAddDetailsRequest {
+    usridfk: number;
+    fname: string;
+    lname: string;
+    mobileno: string;
+    alternatecontactno: string;
+}
+
+export class UserUpdateStateRequest {
+	id: number;
+	usrstate: number;
+}
+
+export class RecordChangePassRequest {
+	oldpassword: string;
+	newpassword: string;
+}
