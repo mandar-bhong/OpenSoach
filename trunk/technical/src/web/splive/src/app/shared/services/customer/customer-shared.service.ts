119a4c895c43601aac8a30fd050144d6d2189b1e
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-17 12:06:35 +0000

    Description : Added Customer service. moved CustomerAddDetailsRequest from spl to shared. Changelog : NA. Bug ID: NA Reviewed By: Self

--- trunk/technical/src/web/splive/src/app/shared/services/customer/customer-shared.service.ts
+++ trunk/technical/src/web/splive/src/app/shared/services/customer/customer-shared.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { EnvironmentProvider } from '../../environment-provider';
-import { CustomerLoginInfoResponse } from '../../models/api/customer-models';
+import { CustomerAddDetailsRequest, CustomerLoginInfoResponse } from '../../models/api/customer-models';
 import { PayloadResponse } from '../../models/api/payload-models';
 import { ServerApiInterfaceService } from '../../services/api/server-api-interface.service';
 
@@ -12,5 +12,11 @@ export class CustomerSharedService {
     constructor(private serverApiInterfaceService: ServerApiInterfaceService) { }
     getCusomerLoginInfo(implicitErrorHandling = true): Observable<PayloadResponse<CustomerLoginInfoResponse>> {
         return this.serverApiInterfaceService.get(EnvironmentProvider.baseurl + '/api/cu/v1/customer/info/login', implicitErrorHandling);
+    }
+
+    updateCustomerDetails(customerAddDetailsRequest: CustomerAddDetailsRequest, implicitErrorHandling = true):
+        Observable<PayloadResponse<null>> {
+        return this.serverApiInterfaceService.post(EnvironmentProvider.baseurl + '/api/cu/v1/customer/update/details',
+            customerAddDetailsRequest, implicitErrorHandling);
     }
 }
---
+++ trunk/technical/src/web/splive/src/app/spl/app/services/customer.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { EnvironmentProvider } from '../../../shared/environment-provider';
+import { RecordAddResponse } from '../../../shared/models/api/common-models';
+import { CustomerAddDetailsRequest } from '../../../shared/models/api/customer-models';
+import { PayloadResponse } from '../../../shared/models/api/payload-models';
+import { ServerApiInterfaceService } from '../../../shared/services/api/server-api-interface.service';
+import { CustomerAddRequest } from '../models/api/customer-models';
+
+@Injectable()
+export class CustomerService {
+
+    constructor(private serverApiInterfaceService: ServerApiInterfaceService) { }
+    addCustomer(customerAddRequest: CustomerAddRequest, implicitErrorHandling = true):
+        Observable<PayloadResponse<RecordAddResponse>> {
+        return this.serverApiInterfaceService.post(EnvironmentProvider.baseurl + '/api/osu/v1/customer/add',
+            customerAddRequest, implicitErrorHandling);
+    }
+
+    updateCustomerDetails(customerAddDetailsRequest: CustomerAddDetailsRequest, implicitErrorHandling = true):
+        Observable<PayloadResponse<null>> {
+        return this.serverApiInterfaceService.post(EnvironmentProvider.baseurl + '/api/osu/v1/customer/update/details',
+            customerAddDetailsRequest, implicitErrorHandling);
+    }
+}
