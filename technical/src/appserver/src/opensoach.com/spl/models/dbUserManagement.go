cd6aaeb5bbdd3299d60d9f75f8c0810782f4db4d
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-18 08:57:46 +0000

    Description: Modified changepasswordmodel to UpdatePasswordRequest Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/models/dbUserManagement.go
+++ technical/src/appserver/src/opensoach.com/spl/models/dbUserManagement.go
@@ -4,12 +4,18 @@ type DBUserAuthInfo struct {
 	Connectionstring string `db:"connection_string" json:"connectionstring"`
 	CpmId            int64  `db:"cpm_id" json:"id"`
 	CustomerId       int64  `db:"cust_id_fk" json:"customerid"`
+	UserRoleId       int64  `db:"urole_id_fk" json:"userroleid"`
 	UserRoleCode     string `db:"urole_code" json:"userrolecode"`
 }
 
 type DBUserInfoMinDataModel struct {
 	FirstName *string `db:"fname" json:"fname"`
 	LastName  *string `db:"lname" json:"lname"`
+}
+
+type UpdatePasswordRequest struct {
+	OldPassword string `json:"oldpassword"`
+	NewPassword string `json:"newpassword"`
 }
 
 type DBCustomerLoginInfoDataModel struct {
