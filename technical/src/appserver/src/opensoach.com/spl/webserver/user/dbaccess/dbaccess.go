2c562bd79f3f43da277c9668697faa1a46ec9891
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-18 08:28:20 +0000

    Description: Added user listing api Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/user/dbaccess/dbaccess.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/user/dbaccess/dbaccess.go
@@ -1,10 +1,15 @@
 package dbaccess
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	"opensoach.com/core/logger"
 	dbmgr "opensoach.com/core/manager/db"
 	"opensoach.com/spl/constants"
 	"opensoach.com/spl/constants/dbquery"
+	lhelper "opensoach.com/spl/helper"
 	lmodels "opensoach.com/spl/models"
 )
 
@@ -129,4 +134,67 @@ func SplMasterUserTableUpdatePassword(dbConn string, updtStruct lmodels.DBSplMas
 		return updateErr, 0
 	}
 	return nil, updateCtx.AffectedRows
+}
+
+func GetSplMasterUserTableTotalFilteredRecords(dbConn string, filterModel *lmodels.DBSearchUserRequestFilterDataModel) (error, *lmodels.DBTotalRecordsModel) {
+
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing GetSplMasterUserTableTotalFilteredRecords")
+
+	whereCondition := lhelper.GetFilterConditionFormModel(*filterModel)
+
+	if whereCondition != "" {
+		whereCondition = " where " + whereCondition
+	}
+
+	query := strings.Replace(dbquery.QUERY_GET_SPL_MASTER_USER_TABLE_TOTAL_FILTERED_COUNT, "$WhereCondition$", whereCondition, 1)
+
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "User Filter Record list filter count : "+query)
+
+	selectCtx := dbmgr.SelectContext{}
+	data := &lmodels.DBTotalRecordsModel{}
+	selectCtx.DBConnection = dbConn
+	selectCtx.Dest = data
+	selectCtx.Query = query
+	selectCtx.QueryType = dbmgr.Query
+	selectErr := selectCtx.Get()
+	if selectErr != nil {
+		return selectErr, &lmodels.DBTotalRecordsModel{}
+	}
+	return nil, data
+}
+
+func SplMasterUserTableSelectByFilter(dbConn string, listdatareq lmodels.DataListRequest, filterModel *lmodels.DBSearchUserRequestFilterDataModel, startingRow int) (error, *[]lmodels.DBSplMasterUserTableFilterResponseModel) {
+
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing SplMasterUserTableSelectByFilter")
+
+	if isParamValid := lhelper.DBQueryParamValidate(listdatareq.OrderBy) &&
+		lhelper.DBQueryParamValidate(listdatareq.OrderDirection); isParamValid == false {
+		return errors.New(fmt.Sprintf("Invalid query paramter %s or %s ", listdatareq.OrderBy, listdatareq.OrderDirection)), nil
+	}
+
+	dbMatchedTag := lhelper.GetDBTagFromJSONTag(lmodels.DBSplMasterUserTableFilterResponseModel{}, listdatareq.OrderBy)
+
+	whereCondition := lhelper.GetFilterConditionFormModel(*filterModel)
+
+	if whereCondition != "" {
+		whereCondition = " where " + whereCondition
+	}
+
+	query := strings.Replace(dbquery.QUERY_SPL_MASTER_USER_TABLE_SELECT_BY_FILTER, "$OrderByDirection$", dbMatchedTag+" "+listdatareq.OrderDirection, 1)
+	query = strings.Replace(query, "$WhereCondition$", whereCondition, 1)
+
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "User Filter Record list filter : "+query)
+
+	limit := listdatareq.Limit
+	selectCtx := dbmgr.SelectContext{}
+	data := &[]lmodels.DBSplMasterUserTableFilterResponseModel{}
+	selectCtx.DBConnection = dbConn
+	selectCtx.Dest = data
+	selectCtx.Query = query
+	selectCtx.QueryType = dbmgr.Query
+	selectErr := selectCtx.Select(startingRow, limit)
+	if selectErr != nil {
+		return selectErr, &[]lmodels.DBSplMasterUserTableFilterResponseModel{}
+	}
+	return nil, data
 }
