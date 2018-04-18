350a156fdef40ac35ba3d295a1fa53dd7e9fac90
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-18 09:15:34 +0000

    Description : Added getCustomerList APi. Changelog : Changed CustomerDataListingModel to CustomerDataListingItemResponse. Bug ID: NA Reviewed By: Self

--- technical/src/web/splive/src/app/spl/app/services/customer.service.ts
+++ technical/src/web/splive/src/app/spl/app/services/customer.service.ts
@@ -6,7 +6,7 @@ import { RecordAddResponse } from '../../../shared/models/api/common-models';
 import { CustomerAddDetailsRequest } from '../../../shared/models/api/customer-models';
 import { PayloadResponse } from '../../../shared/models/api/payload-models';
 import { ServerApiInterfaceService } from '../../../shared/services/api/server-api-interface.service';
-import { CustomerAddRequest } from '../models/api/customer-models';
+import { CustomerAddRequest, CustomerFilterRequest, CustomerDataListingItemResponse } from '../models/api/customer-models';
 
 @Injectable()
 export class CustomerService {
@@ -22,5 +22,11 @@ export class CustomerService {
         Observable<PayloadResponse<null>> {
         return this.serverApiInterfaceService.post(EnvironmentProvider.baseurl + '/api/osu/v1/customer/update/details',
             customerAddDetailsRequest, implicitErrorHandling);
+    }
+
+    getCustomerList(customerFilterRequest: CustomerFilterRequest, implicitErrorHandling = true):
+        Observable<PayloadResponse<CustomerDataListingItemResponse>> {
+        return this.serverApiInterfaceService.getWithQueryParams(EnvironmentProvider.baseurl + '/api/osu/v1/customer/list',
+        customerFilterRequest, implicitErrorHandling);
     }
 }
