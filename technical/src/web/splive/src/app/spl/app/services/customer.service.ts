fed89b37ff2a8449f8d73b9de2061b87d2ac9f3c
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-18 09:28:15 +0000

    Description : Moved data-list-models.ts. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/web/splive/src/app/spl/app/services/customer.service.ts
+++ technical/src/web/splive/src/app/spl/app/services/customer.service.ts
@@ -4,9 +4,10 @@ import { Observable } from 'rxjs/Observable';
 import { EnvironmentProvider } from '../../../shared/environment-provider';
 import { RecordAddResponse } from '../../../shared/models/api/common-models';
 import { CustomerAddDetailsRequest } from '../../../shared/models/api/customer-models';
+import { DataListRequest, DataListResponse } from '../../../shared/models/api/data-list-models';
 import { PayloadResponse } from '../../../shared/models/api/payload-models';
 import { ServerApiInterfaceService } from '../../../shared/services/api/server-api-interface.service';
-import { CustomerAddRequest, CustomerFilterRequest, CustomerDataListingItemResponse } from '../models/api/customer-models';
+import { CustomerAddRequest, CustomerDataListingItemResponse, CustomerFilterRequest } from '../models/api/customer-models';
 
 @Injectable()
 export class CustomerService {
@@ -24,9 +25,9 @@ export class CustomerService {
             customerAddDetailsRequest, implicitErrorHandling);
     }
 
-    getCustomerList(customerFilterRequest: CustomerFilterRequest, implicitErrorHandling = true):
-        Observable<PayloadResponse<CustomerDataListingItemResponse>> {
+    getCustomerList(dataListFilter: DataListRequest<CustomerFilterRequest>, implicitErrorHandling = true):
+        Observable<PayloadResponse<DataListResponse<CustomerDataListingItemResponse>>> {
         return this.serverApiInterfaceService.getWithQueryParams(EnvironmentProvider.baseurl + '/api/osu/v1/customer/list',
-        customerFilterRequest, implicitErrorHandling);
+            dataListFilter, implicitErrorHandling);
     }
 }
