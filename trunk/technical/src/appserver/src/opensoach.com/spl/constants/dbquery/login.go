40d8af0d4af9bcacbd0ce441e9afaf286005e0e2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-18 08:52:08 +0000

    Description: Added queries respective to customer listing,user listing,validate token apis Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/constants/dbquery/login.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/constants/dbquery/login.go
@@ -2,12 +2,14 @@ package dbquery
 
 const QUERY_MUST_CHECK_USER_LOGIN = "SELECT id,usr_name,usr_password,usr_category,usr_state,urole_id_fk FROM spl_master_user_tbl WHERE usr_name = ? AND usr_password = ?"
 
-const QUERY_GET_USER_AUTH_INFO = `select cpm.id as cpm_id, cpm.cust_id_fk,role.urole_code,di.connection_string from 
+const QUERY_GET_USER_AUTH_INFO = `SELECT urole_code FROM spl_master_user_role_tbl WHERE id = ?`
+
+const QUERY_GET_USER_AUTH_INFO_CATEGORY_CUSTOMER = `select cpm.id as cpm_id, cpm.cust_id_fk,usrcpm.urole_id_fk,role.urole_code,dbinst.connection_string from 
 spl_master_cust_prod_mapping_tbl cpm
 INNER JOIN spl_master_product_tbl prod  ON cpm.prod_id_fk = prod.id
 INNER JOIN spl_master_usr_cpm_tbl usrcpm ON  usrcpm.cpm_id_fk = cpm.id
 INNER JOIN spl_master_user_role_tbl role ON usrcpm.urole_id_fk = role.id
-INNER JOIN spl_master_database_instance_tbl di ON cpm.dbi_id_fk = di.id
+INNER JOIN spl_master_database_instance_tbl dbinst ON cpm.dbi_id_fk = dbinst.id
 where prod.prod_code = ? AND usrcpm.user_id_fk = ?`
 
 const QUERY_GET_USER_LOGIN_INFO = `SELECT fname,lname FROM spl_master_usr_details_tbl where usr_id_fk = ?`
