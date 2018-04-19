267f155b2de5d3f13b9ff297b17fcc1b4abea4ff
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-19 09:10:27 +0000

    Description: Added testing helpers Reviewed By: Self Change Log: Bug ID:

---
+++ technical/src/appserver/src/opensoach.com/spl/helper/webtestinghelper.go
@@ -0,0 +1,65 @@
+package helper
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	gmodels "opensoach.com/models"
+	repo "opensoach.com/spl/repository"
+	"opensoach.com/core"
+)
+
+func PrepareTestSetup() {
+
+	setting := gmodels.ConfigSettings{}
+	setting.DBConfig = &gmodels.ConfigDB{}
+	setting.DBConfig.ConnectionString = "root:welcome@tcp(localhost:3306)/spl_master?parseTime=true"
+
+	ctx := core.Context{}
+
+	repo.Init(&setting, &ctx)
+	repo.Instance().Context.Master.DBConn = setting.DBConfig.ConnectionString
+	repo.Instance().Context.Master.Cache.CacheAddress = `{"address":"localhost:6379","password":"","db":0}`
+
+}
+
+func ExecuteTestRequest(t *testing.T,
+	reqMethod string,
+	API string,
+	handler RequestHandler,
+	reqJSONData string,
+	token string) (webResponse string, jsonPathStruct interface{}) {
+	router := gin.Default()
+	gin.SetMode(gin.TestMode)
+
+	var req *http.Request
+	var reqErr error
+	
+	if reqMethod == "POST"{
+		router.POST(API, func(c *gin.Context) { CommonWebRequestHandler(c, handler) })
+		req, reqErr = http.NewRequest(reqMethod, API, bytes.NewBuffer([]byte(reqJSONData)))
+	}else{
+		router.GET(API, func(c *gin.Context) { CommonWebRequestHandler(c, handler) })
+		req, reqErr = http.NewRequest(reqMethod, API + "?"+reqJSONData, bytes.NewBuffer([]byte(reqJSONData)))
+	}	
+	
+
+	if reqErr != nil{
+		t.Fatalf("Unable to create request. Error : %s ",reqErr.Error())
+		return	"",nil
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", token)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	webResponse = resp.Body.String()
+	json.Unmarshal([]byte(webResponse), &jsonPathStruct)
+
+	return webResponse, jsonPathStruct
+}
