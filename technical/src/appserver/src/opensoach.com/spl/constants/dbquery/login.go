34bae1033c43e26575419cd9dad47dd7fce63635
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-19 10:46:18 +0000

    Description: Modified query,model for login info api,added username field Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/constants/dbquery/login.go
+++ technical/src/appserver/src/opensoach.com/spl/constants/dbquery/login.go
@@ -12,7 +12,7 @@ INNER JOIN spl_master_user_role_tbl role ON usrcpm.urole_id_fk = role.id
 INNER JOIN spl_master_database_instance_tbl dbinst ON cpm.dbi_id_fk = dbinst.id
 where prod.prod_code = ? AND usrcpm.user_id_fk = ?`
 
-const QUERY_GET_USER_LOGIN_INFO = `SELECT fname,lname FROM spl_master_usr_details_tbl where usr_id_fk = ?`
+const QUERY_GET_USER_LOGIN_INFO = `SELECT usr_name,fname,lname FROM spl_master_user_tbl usr LEFT JOIN spl_master_usr_details_tbl usrd ON usr.id = usrd.usr_id_fk WHERE usr.id = ?`
 
 const QUERY_GET_CUSTOMER_LOGIN_INFO = `SELECT corp_name,cust_name FROM spl_master_customer_tbl cust
 INNER JOIN spl_master_corp_tbl corp ON corp.id = cust.corp_id_fk
