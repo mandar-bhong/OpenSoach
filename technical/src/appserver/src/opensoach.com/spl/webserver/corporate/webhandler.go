36d3c5d611ba3c572db9d7b7b3a26578f74cba51
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-20 08:39:32 +0000

    Description: Added add corporate,update corporate,corporate short data list apis and respective api models Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/corporate/webhandler.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/corporate/webhandler.go
@@ -12,6 +12,9 @@ import (
 
 func registerRouters(router *gin.RouterGroup) {
 	router.POST(constants.API_CORPORATE_OSU_LIST, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.GET(constants.API_CORPORATE_OSU_LIST_SHORT, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_CORPORATE_OSU_ADD, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
+	router.POST(constants.API_CORPORATE_OSU_UPDATE, func(c *gin.Context) { lhelper.CommonWebRequestHandler(c, requestHandler) })
 }
 
 func requestHandler(pContext *gin.Context) (bool, interface{}) {
@@ -37,6 +40,55 @@ func requestHandler(pContext *gin.Context) (bool, interface{}) {
 		isSuccess, resultData = CorporateService{
 			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
 		}.GetCorpDataList(corpListReq)
+
+		break
+
+	case constants.API_CORPORATE_OSU_LIST_SHORT:
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionData(repo.Instance().Context, pContext)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CorporateService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.GetCorpShortDataList()
+
+		break
+
+	case constants.API_CORPORATE_OSU_ADD:
+
+		reqData := &lmodels.DBSplCorpRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &reqData)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CorporateService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.AddCorp(reqData)
+
+		break
+
+	case constants.API_CORPORATE_OSU_UPDATE:
+
+		reqData := &lmodels.DBSplCorpRowModel{}
+
+		isPrepareExeSuccess, successErrorData := lhelper.PrepareExecutionReqData(repo.Instance().Context, pContext, &reqData)
+
+		if isPrepareExeSuccess == false {
+			logger.Context().Log(SUB_MODULE_NAME, logger.Normal, logger.Error, "Error occured while preparing execution data.")
+			return false, successErrorData
+		}
+
+		isSuccess, resultData = CorporateService{
+			ExeCtx: successErrorData.(*gmodels.ExecutionContext),
+		}.UpdateCorp(reqData)
 
 		break
 	}
