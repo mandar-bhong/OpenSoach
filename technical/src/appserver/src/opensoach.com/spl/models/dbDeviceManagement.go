2c33858883795f337d44afc1862be793d256c1d5
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-21 12:12:32 +0000

    Description: Added file dbDeviceManagement.go, dbmodels for device api Reviewed By: Mandar Change Log: Bug ID:

---
+++ technical/src/appserver/src/opensoach.com/spl/models/dbDeviceManagement.go
@@ -0,0 +1,25 @@
+package models
+
+import "time"
+
+type DBSplMasterDeviceRowModel struct {
+	DevId         int       `dbattr:"pri,auto" db:"id" json:"devid"`
+	Serialno      string    `db:"serialno" json:"serialno"`
+	DevState      int       `db:"dev_state" json:"devstate"`
+	DevStateSince time.Time `db:"dev_state_since" json:"devstatesince"`
+}
+
+type DBSplMasterDevDetailsRowModel struct {
+	DevId       int64   `dbattr:"pri" db:"dev_id_fk" json:"devid"`
+	DevName     *string `db:"dev_name" json:"devname"`
+	Make        *string `db:"make" json:"make"`
+	Technology  *string `db:"technology" json:"technology"`
+	TechVersion *string `db:"tech_version" json:"techversion"`
+	ShortDesc   *string `db:"short_desc" json:"shortdesc"`
+}
+
+type DevStateRowModel struct {
+	DevId         int       `dbattr:"pri,auto" db:"id" json:"devid"`
+	DevState      int       `db:"dev_state" json:"devstate"`
+	DevStateSince time.Time `db:"dev_state_since" json:"devstatesince"`
+}
