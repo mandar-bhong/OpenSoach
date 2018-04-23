c54dca1d71b8db8d473d60ccc4a16798986f73f1
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-23 08:19:33 +0000

    Description : changed database name. Changelog : NA. Bug ID: NA Reviewed By: Self

--- trunk/technical/src/database/spl_hkt_node/runmetestdata.bat
+++ trunk/technical/src/database/spl_hkt_node/runmetestdata.bat
@@ -6,7 +6,7 @@ echo Starting sql script executing
 SET Server=localhost
 SET User=root
 SET Password=welcome
-SET Database=splive_master
+SET Database=spl_hkt_node_xxxx
 ::----------------End ----------------------------
 
 "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -h%Server% -u%User% -p%Password% %Database% < %~dp0\testdata\testdata_1.0.0.sql
