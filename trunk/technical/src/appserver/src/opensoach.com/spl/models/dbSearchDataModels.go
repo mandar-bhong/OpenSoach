1cb228b891130f2faacde51e146f875f288712c0
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-25 07:31:49 +0000

    Description: Modified customersearchfilterresponse model Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/spl/models/dbSearchDataModels.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/models/dbSearchDataModels.go
@@ -46,9 +46,9 @@ type DBSearchCustomerResponseFilterDataModel struct {
 	CorpID       int64     `db:"corp_id_fk" json:"corpid"`
 	CustName     string    `db:"cust_name" json:"custname"`
 	CorpName     string    `db:"corp_name" json:"corpname"`
-	Poc1Name     string    `db:"poc1_name" json:"poc1name"`
-	Poc1EmailID  string    `db:"poc1_email_id" json:"poc1emailid"`
-	Poc1MobileNo string    `db:"poc1_mobile_no" json:"poc1mobileno"`
+	Poc1Name     *string   `db:"poc1_name" json:"poc1name"`
+	Poc1EmailID  *string   `db:"poc1_email_id" json:"poc1emailid"`
+	Poc1MobileNo *string   `db:"poc1_mobile_no" json:"poc1mobileno"`
 	CreatedOn    time.Time `db:"created_on" json:"createdon"`
 	UpdateOn     time.Time `db:"updated_on" json:"updateon"`
 }
@@ -56,6 +56,7 @@ type DBSearchCustomerResponseFilterDataModel struct {
 type DBSearchDeviceRequestFilterDataModel struct {
 	Serialno *string `db:"serialno" json:"serialno"`
 	CustId   *int64  `dbattr:"pri,auto" db:"cust.id" json:"custid"` //This is specical condition as query has multiple table join
+	CustName *string `db:"cust_name" json:"custname"`
 	DevName  *string `db:"dev_name" json:"devname"`
 }
 
