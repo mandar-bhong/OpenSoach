ac5ad822816660c8a3a47bfb919751e4923241c0
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-26 15:20:30 +0000

    Description: Added product core module to handle common scenario across product line Reviewed By: Self Change Log: Bug ID:

---
+++ technical/src/appserver/src/opensoach.com/prodcore/helper/sessionhelper.go
@@ -0,0 +1,62 @@
+package helper
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	gcore "opensoach.com/core"
+	ghelper "opensoach.com/core/helper"
+	//gcache "opensoach.com/core/manager/cache"
+	gmodels "opensoach.com/models"
+)
+
+func SessionCreate(osContext *gcore.Context, pSessionData *gmodels.UserSessionInfo) (bool, string) {
+	isTokenCreateSuccess, sessionToken := ghelper.CreateToken()
+	if !isTokenCreateSuccess {
+		//logger.Log(MODULENAME, logger.ERROR, "SessionCreate : Unable to create session token")
+		fmt.Println("Error occured while creating token")
+		return false, ""
+	}
+
+	isJsonSuccess, jsonData := ghelper.ConvertToJSON(pSessionData)
+
+	if !isJsonSuccess {
+		//logger.Log(MODULENAME, logger.ERROR, "createSession : Unable to convert session data to JSON")
+		return false, ""
+	}
+
+	isSetSuccess := osContext.Master.Cache.Set(sessionToken, jsonData, time.Minute*60)
+
+	return isSetSuccess, sessionToken
+}
+
+func SessionGet(osContext *gcore.Context, ginContext *gin.Context) (bool, *gmodels.UserSessionInfo) {
+
+	userInfo := &gmodels.UserSessionInfo{}
+	token := ginContext.GetHeader(gmodels.SESSION_CLIENT_HEADER_KEY)
+
+	isSuccess, jsonData := osContext.Master.Cache.Get(token)
+
+	if !isSuccess {
+		return false, nil
+	}
+
+	isJsonConvSuccess := ghelper.ConvertFromJSONString(jsonData, userInfo)
+
+	if !isJsonConvSuccess {
+		return false, nil
+	}
+
+	return true, userInfo
+}
+
+func SessionUpdate(osContext *gcore.Context, ginContext *gin.Context) bool {
+	token := ginContext.GetHeader(gmodels.SESSION_CLIENT_HEADER_KEY)
+	return osContext.Master.Cache.Update(token, time.Minute*2)
+}
+
+func SessionDelete(osContext *gcore.Context, ginContext *gin.Context) bool {
+	token := ginContext.GetHeader(gmodels.SESSION_CLIENT_HEADER_KEY)
+	return osContext.Master.Cache.Remove(token)
+}
