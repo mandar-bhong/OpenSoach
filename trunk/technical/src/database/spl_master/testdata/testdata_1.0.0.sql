39b99baa7288b41a4565e0f83dacb78b406cf437
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-26 09:27:19 +0000

    Description: Added user tbl and user details tbl test data Reviewed By: Change Log: Bug ID:

--- trunk/technical/src/database/spl_master/testdata/testdata_1.0.0.sql
+++ trunk/technical/src/database/spl_master/testdata/testdata_1.0.0.sql
@@ -2,10 +2,10 @@
 -- Dumping data for table `spl_master_corp_tbl`
 --
    
-INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (1,'Corporation 1','1234568','corp@gmail.com','01919191');
-INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (2,'Corporation 2','1324343','corp2@gmail.com','04342123');
-INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (3,'Corporation 3','4353432','corp3@gmail.com','01132323');
-INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (4,'Corporation 4','3423454','corp4@gmail.com','04322434');
+INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (1,'Corporate 1','1234568','corp@gmail.com','01919191');
+INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (2,'Corporate 2','1324343','corp2@gmail.com','04342123');
+INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (3,'Corporate 3','4353432','corp3@gmail.com','01132323');
+INSERT INTO `spl_master_corp_tbl` (`id`,`corp_name`,`corp_mobile_no`,`corp_email_id`,`corp_landline_no`) VALUES (4,'Corporate 4','3423454','corp4@gmail.com','04322434');
 
 
 
@@ -17,8 +17,8 @@ INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_st
 INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (2, 1, 'Customer 2', 1,UTC_TIMESTAMP);
 INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (3, 1, 'Customer 3', 1,UTC_TIMESTAMP);
 INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (4, 2, 'Customer 4', 1,UTC_TIMESTAMP);
-INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (5, 2, 'Customer 5', 1,UTC_TIMESTAMP);
-INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (6, 3, 'Customer 6', 1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (5, 2, 'Customer 5', 2,UTC_TIMESTAMP);
+INSERT INTO `spl_master_customer_tbl` (`id`, `corp_id_fk`, `cust_name`, `cust_state`, `cust_state_since`) VALUES (6, 3, 'Customer 6', 2,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_cust_prod_mapping_tbl`
@@ -67,12 +67,15 @@ UPDATE `spl_master_total_count_tbl` SET `cust_cnt`='1', `usr_cnt`='1', `dev_cnt`
 --
 INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (1,'admin@servicepoint.live','admin',1,1,1,UTC_TIMESTAMP);
 INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (2,'admin@customer1.com','admin',2,null,1,UTC_TIMESTAMP);
+INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (3,'admin@servicepoint2.live','admin',1,1,2,UTC_TIMESTAMP);
+INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (4,'admin@customer2.com','admin',2,null,2,UTC_TIMESTAMP);
+INSERT INTO `spl_master_user_tbl` (`id`,`usr_name`,`usr_password`,`usr_category`,`urole_id_fk`,`usr_state`,`usr_state_since`) VALUES (5,'cust@customer3.com','admin',2,null,1,UTC_TIMESTAMP);
 
 --
 -- Dumping data for table `spl_master_usr_cpm_tbl`
 --
 
-INSERT INTO `spl_master_usr_cpm_tbl` (`id`,`user_id_fk`,`cpm_id_fk`,`urole_id_fk`) VALUES (1,1,1,1);
+INSERT INTO `spl_master_usr_cpm_tbl` (`id`,`user_id_fk`,`cpm_id_fk`,`urole_id_fk`) VALUES (1,2,1,1);
 
 --
 -- Dumping data for table `spl_master_cust_prod_count_tbl`
@@ -84,7 +87,14 @@ INSERT INTO `spl_master_cust_prod_count_tbl` (`id`,`cpm_id_fk`,`dev_cnt`,`sp_cnt
 --
 -- Dumping data for table `spl_master_cust_details_tbl`
 --
-INSERT INTO `spl_master_cust_details_tbl` (`cust_id_fk`, `poc1_name`, `poc1_email_id`, `poc1_mobile_no`) VALUES (1, 'poc1', 'poc1@email.com', '1244324');
+INSERT INTO `spl_master_cust_details_tbl` (`cust_id_fk`, `poc1_name`, `poc1_email_id`, `poc1_mobile_no`) VALUES (1, 'poc1', 'poc1@email.com', '12443241');
 INSERT INTO `spl_master_cust_details_tbl` (`cust_id_fk`, `poc1_name`, `poc1_email_id`, `poc1_mobile_no`) VALUES (3, 'poc3', 'poc3@email.com', '12453434');
 INSERT INTO `spl_master_cust_details_tbl` (`cust_id_fk`, `poc1_name`, `poc1_email_id`, `poc1_mobile_no`) VALUES (4, 'poc4', 'poc4@email.com', '15633434');
-INSERT INTO `spl_master_cust_details_tbl` (`cust_id_fk`, `poc1_name`, `poc1_email_id`, `poc1_mobile_no`) VALUES (6, 'poc6', 'poc6@email.com', '15633434');
+INSERT INTO `spl_master_cust_details_tbl` (`cust_id_fk`, `poc1_name`, `poc1_email_id`, `poc1_mobile_no`) VALUES (6, 'poc6', 'poc6@email.com', '15622434');
+
+--
+-- Dumping data for table `spl_master_usr_details_tbl`
+--
+
+INSERT INTO `spl_master_usr_details_tbl` (`usr_id_fk`, `fname`, `lname`, `mobile_no`, `alternate_contact_no`) VALUES (1, 'fname1', 'lname1', '9813123121', '9713131334');
+INSERT INTO `spl_master_usr_details_tbl` (`usr_id_fk`, `fname`, `lname`, `mobile_no`, `alternate_contact_no`) VALUES (2, 'fname2', 'lname2', '9672123121', '9123131334');
