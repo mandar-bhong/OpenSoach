0b15abf056e488274e13e9816c3dceb4f816e7e4
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-04-28 10:30:15 +0000

    Description: Added HKT constants and models Reviewed By: Self Change Log: Bug ID:

---
+++ technical/src/appserver/src/opensoach.com/hkt/models/dbTaskRowModel.go
@@ -0,0 +1,8 @@
+package models
+
+type DBTaskLibRowModel struct {
+	CPMID        int64  `db:"cpm_id" json:"cpmid"`
+	SPCategoryID int64  `db:"spc_id_fk" json:"spcid"`
+	TaskName     string `db:"task_name" json:"taskname"`
+	Description  string `db:"short_desc" json:"desc"`
+}
