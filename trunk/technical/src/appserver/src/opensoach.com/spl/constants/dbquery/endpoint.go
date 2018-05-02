0dd71d7907defd4c92774e4b906a23b390510efc
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-02 11:42:37 +0000

    Description: Added endpoint validation (android device validation) api and respective models and query Reviewed By: Mandar Change Log: Bug ID:

---
+++ trunk/technical/src/appserver/src/opensoach.com/spl/constants/dbquery/endpoint.go
@@ -0,0 +1,7 @@
+package dbquery
+
+const QUERY_GET_DEVICE_INFO_BY_DEVICE_SERIAL_NO = `Select id,dev_state from spl_master_device_tbl where serialno = ?`
+const QUERY_GET_DEVICE_AUTH_INFO = `Select cpm.id as id ,sreg.server_address as server_address From spl_master_cust_prod_mapping_tbl cpm
+ Inner join spl_master_cpm_dev_mapping_tbl cpmd on cpmd.cpm_id_fk = cpm.id
+ Inner join spl_master_server_register sreg on sreg.prod_id_fk = cpm.prod_id_fk
+ where dev_id_fk = ?`
