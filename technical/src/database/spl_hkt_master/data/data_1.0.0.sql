3f93d8bf01a45747cedbd4e2339780e5985ee36c
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-03 15:52:30 +0000

    Description : renamed references of table name changes in HKT master.. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/database/spl_hkt_master/data/data_1.0.0.sql
+++ technical/src/database/spl_hkt_master/data/data_1.0.0.sql
@@ -1,20 +1,22 @@
 --
--- Dumping data for table `spl_hkt_master_sp_category_tbl`
+-- Dumping data for table `spl_prod_master_sp_category_tbl`
 --
 
-INSERT INTO `spl_hkt_master_sp_category_tbl` (`id`,`spc_name`) VALUES (1,'Washroom - Mens');
-INSERT INTO `spl_hkt_master_sp_category_tbl` (`id`,`spc_name`) VALUES (2,'Washroom - Womens');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (1,'Washroom - Mens');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (2,'Washroom - Womens');
 
+--
+-- Dumping data for table `spl_prod_master_config`
+--
 
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Web.Service.Address',':90');
 
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Web.Service.Address',':90');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Host','localhost');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Port','6379');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Password','');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.DB','2');
 
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Host','localhost');
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Port','6379');
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Password','');
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.DB','2');
-
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Host','localhost');
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Port','6379');
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Password','');
-INSERT INTO `spl_hkt_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.DB','2');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Host','localhost');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Port','6379');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Password','');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.DB','2');
