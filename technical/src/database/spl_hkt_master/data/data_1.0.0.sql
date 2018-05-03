9e5b495e7bcbc6127d90aa900dc00e9b2df7cea9
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-03 15:54:42 +0000

    Description : Added meta data for service configuration table. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/database/spl_hkt_master/data/data_1.0.0.sql
+++ technical/src/database/spl_hkt_master/data/data_1.0.0.sql
@@ -20,3 +20,5 @@ INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Prod
 INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Port','6379');
 INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Password','');
 INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.DB','2');
+
+INSERT INTO `spl_prod_serv_conf_type_tbl` (`conf_type_code`) VALUES ('SERVICE_DAILY_CHART');
