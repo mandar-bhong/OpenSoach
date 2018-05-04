f5b134bec4fda6b3bd30c23a5c412fa7a323b2fa
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-04 12:02:43 +0000

    Description: Added customer Product association, list and update state api Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/models/dbCustomerManagement.go
+++ technical/src/appserver/src/opensoach.com/spl/models/dbCustomerManagement.go
@@ -9,3 +9,16 @@ type DBCustProdMappingInsertRowModel struct {
 	CpmState      int       `db:"cpm_state" json:"cpmstate"`
 	CpmStateSince time.Time `db:"cpm_state_since" json:"cpmstatesince"`
 }
+
+type DBCustProdAssociationInfoRowModel struct {
+	ProdId   int64  `db:"prod_id_fk" json:"prodid"`
+	DbiId    int64  `db:"dbi_id_fk" json:"dbiid"`
+	ProdCode string `db:"prod_code" json:"prodcode"`
+	DbiName  string `db:"dbi_name" json:"dbiname"`
+}
+
+type DBCpmStateUpdateRowModel struct {
+	CpmId         int64     `dbattr:"pri,auto" db:"id" json:"cpmid"`
+	CpmState      int       `db:"cpm_state" json:"cpmstate"`
+	CpmStateSince time.Time `db:"cpm_state_since" json:"cpmstatesince"`
+}
