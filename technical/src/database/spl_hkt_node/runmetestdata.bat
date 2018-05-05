998ace620a743ac4516afa77628f23afabd4c890
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-05 06:28:47 +0000

    Description: Change database name Reviewed By: Self Change Log: Bug ID:

--- technical/src/database/spl_hkt_node/runmetestdata.bat
+++ technical/src/database/spl_hkt_node/runmetestdata.bat
@@ -6,7 +6,7 @@ echo Starting sql script executing
 SET Server=localhost
 SET User=root
 SET Password=welcome
-SET Database=spl_hkt_node_xxxx
+SET Database=spl_hkt_node_0001
 ::----------------End ----------------------------
 
 "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -h%Server% -u%User% -p%Password% %Database% < %~dp0\testdata\testdata_1.0.0.sql
