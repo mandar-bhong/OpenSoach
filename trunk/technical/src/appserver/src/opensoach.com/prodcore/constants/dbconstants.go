cdf8e86398ece26342ebd483d54e349879c46c76
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-05 05:56:31 +0000

    Description: Added common endpoint handler Reviewed By: Self Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/prodcore/constants/dbconstants.go
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/constants/dbconstants.go
@@ -2,7 +2,7 @@ package constants
 
 const DB_CONFIG_HKT_MASTER_DB_CONNECTION = "HKT.Master.DB.Connection"
 
-const DB_CONFIG_WEB_SERVICE_ADDRESS string = "Web.Service.Address"
+
 const DB_CONFIG_CACHE_ADDRESS_HOST string = "Cache.Address.Host"
 const DB_CONFIG_CACHE_ADDRESS_PORT string = "Cache.Address.Port"
 const DB_CONFIG_CACHE_ADDRESS_PASSWORD string = "Cache.Address.Password"
@@ -15,6 +15,8 @@ const DB_CONFIG_QUE_ADDRESS_DB string = "Que.Address.DB"
 
 const DB_CONFIG_SERVER_WIN_BASE_DIRECTORY string = "Server.Win.BaseDir"
 const DB_CONFIG_SERVER_LIN_BASE_DIRECTORY string = "Server.Lin.BaseDir"
+
+const DB_CONFIG_WEB_SERVICE_ADDRESS string = "Product.Web.Service.Address"
 
 const DB_CONFIG_PRODUCT_CACHE_ADDRESS_HOST string = "Product.Cache.Address.Host"
 const DB_CONFIG_PRODUCT_CACHE_ADDRESS_PORT string = "Product.Cache.Address.Port"
