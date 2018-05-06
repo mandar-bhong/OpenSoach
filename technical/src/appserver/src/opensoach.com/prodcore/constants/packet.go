1f600d14ecafe901d0ce3532f4fd211300a214e6
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-06 09:05:19 +0000

    Description: Added draft version of endpoint communication. Added missing model Reviewed By: Self Change Log: Bug ID:

---
+++ technical/src/appserver/src/opensoach.com/prodcore/constants/packet.go
@@ -0,0 +1,9 @@
+package constants
+
+const DEVICE_CMD_PRE_EXECUTOR string = "PreExecuteProcessor"
+
+const DEVICE_CMD_KEY int = 0
+const DEVICE_CMD_ACK_KEY int = 1
+
+const DEVICE_CMD_CAT_DEVICE_VALIDATION int = 1
+const DEVICE_CMD_DEVICE_AUTH int = 1
