bc93449d1e0ce6919e972048751687be7361b2f2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-07 12:50:34 +0000

    Description: Added task module apis Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/hkt/api/webserver/task/dbaccess/dbaccess.go
+++ technical/src/appserver/src/opensoach.com/hkt/api/webserver/task/dbaccess/dbaccess.go
@@ -9,7 +9,7 @@ import (
 
 var SUB_MODULE_NAME = "HKT.Task.DB"
 
-func Insert(dbConn string, insrtStruct *hktmodels.DBTaskLibRowModel) (error, int64) {
+func Insert(dbConn string, insrtStruct *hktmodels.DBTaskLibInsertRowModel) (error, int64) {
 
 	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing Task insert.")
 
@@ -17,7 +17,7 @@ func Insert(dbConn string, insrtStruct *hktmodels.DBTaskLibRowModel) (error, int
 	insDBCtx.DBConnection = dbConn
 	insDBCtx.Args = *insrtStruct
 	insDBCtx.QueryType = dbmgr.AutoQuery
-	insDBCtx.TableName = constants.DB_TABLE_TASK_LIB
+	insDBCtx.TableName = constants.DB_TABLE_HKT_TASK_LIB
 	insertErr := insDBCtx.Insert()
 	if insertErr != nil {
 		return insertErr, 0
@@ -25,14 +25,53 @@ func Insert(dbConn string, insrtStruct *hktmodels.DBTaskLibRowModel) (error, int
 	return nil, insDBCtx.InsertID
 }
 
-func Update(dbConn string) {
+func UpdateByFilter(dbConn string, updtStruct *hktmodels.DBTaskLibUpdateRowModel) (error, int64) {
 
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing Task UpdateByFilter")
+
+	updateCtx := dbmgr.UpdateDeleteContext{}
+	updateCtx.DBConnection = dbConn
+	updateCtx.Args = *updtStruct
+	updateCtx.QueryType = dbmgr.AutoQuery
+	updateCtx.TableName = constants.DB_TABLE_HKT_TASK_LIB
+	updateErr := updateCtx.UpdateByFilter("TaskLibId", "CpmId")
+	if updateErr != nil {
+		return updateErr, 0
+	}
+	return nil, updateCtx.AffectedRows
 }
 
-func SelectByID(dbConn string) {
+func TaskSelectByID(dbConn string, taskId int64) (error, *[]hktmodels.DBSplHktTaskLibTableRowModel) {
 
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing TaskSelectByID")
+
+	selDBCtx := dbmgr.SelectContext{}
+	data := &[]hktmodels.DBSplHktTaskLibTableRowModel{}
+	selDBCtx.DBConnection = dbConn
+	selDBCtx.QueryType = dbmgr.AutoQuery
+	selDBCtx.TableName = constants.DB_TABLE_HKT_TASK_LIB
+	selDBCtx.Dest = data
+	selErr := selDBCtx.SelectById(taskId)
+	if selErr != nil {
+		return selErr, nil
+	}
+	return nil, data
 }
 
-func SelectAll(dbConn string) {
+func TaskSelectAll(dbConn string) (error, *[]hktmodels.DBSplHktTaskLibTableRowModel) {
+
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing TaskSelectAll")
+
+	selDBCtx := dbmgr.SelectContext{}
+	data := &[]hktmodels.DBSplHktTaskLibTableRowModel{}
+	selDBCtx.DBConnection = dbConn
+	selDBCtx.QueryType = dbmgr.AutoQuery
+	selDBCtx.TableName = constants.DB_TABLE_HKT_TASK_LIB
+	selDBCtx.Dest = data
+	selErr := selDBCtx.SelectAll()
+	if selErr != nil {
+		return selErr, nil
+	}
+	return nil, data
 
 }
--- technical/src/appserver/src/opensoach.com/hkt/models/dbTaskRowModel.go
+++ technical/src/appserver/src/opensoach.com/hkt/models/dbTaskRowModel.go
@@ -1,8 +1,20 @@
 package models
 
-type DBTaskLibRowModel struct {
-	CPMID        int64  `db:"cpm_id_fk" json:"cpmid"`
-	SPCategoryID int64  `db:"spc_id_fk" json:"spcid"`
-	TaskName     string `db:"task_name" json:"taskname"`
-	Description  string `db:"short_desc" json:"desc"`
+type DBTaskLibDataModel struct {
+	SpcId     *int64  `db:"spc_id_fk" json:"spcid"`
+	TaskName  string  `db:"task_name" json:"taskname"`
+	ShortDesc *string `db:"short_desc" json:"shortdesc"`
+}
+
+type DBTaskLibInsertRowModel struct {
+	CPMID int64 `db:"cpm_id_fk" json:"cpmid"`
+	DBTaskLibDataModel
+}
+
+type DBTaskLibUpdateRowModel struct {
+	TaskLibId int64   `db:"id" dbattr:"pri,auto"  json:"tasklibid"`
+	CpmId     int64   `db:"cpm_id_fk" json:"cpmid"`
+	SpcId     *int64  `db:"spc_id_fk" json:"spcid"`
+	TaskName  string  `db:"task_name" json:"taskname"`
+	ShortDesc *string `db:"short_desc" json:"shortdesc"`
 }
