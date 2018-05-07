5111307efd452a2047d8f10381951ca9c9555b34
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-07 12:48:38 +0000

    Description: Added complaint module and respective apis Reviewed By: Mandar Change Log: Bug ID:

---
+++ trunk/technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/complaint.go
@@ -0,0 +1,7 @@
+package dbquery
+
+const QUERY_GET_COMPLAINT_TABLE_TOTAL_FILTERED_COUNT = `Select count(*) as count  From spl_hkt_sp_complaint_tbl spcomplaint
+Inner Join spl_node_sp_tbl sp on sp.sp_id_fk = spcomplaint.sp_id_fk $WhereCondition$`
+
+const QUERY_COMPLAINT_TABLE_SELECT_BY_FILTER = `Select spcomplaint.id as id,sp_name,complaint_title,description,complaint_by,raised_on,complaint_state,closed_on  From spl_hkt_sp_complaint_tbl spcomplaint
+Inner Join spl_node_sp_tbl sp on sp.sp_id_fk = spcomplaint.sp_id_fk $WhereCondition$ ORDER BY $OrderByDirection$ Limit ?,?`
