fc36b5aff18813953ad5a63e4aeb827a77688c11
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-08 08:58:26 +0000

    Description: Modified device listing filter query added column devid in select query Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/constants/dbquery/device.go
+++ technical/src/appserver/src/opensoach.com/spl/constants/dbquery/device.go
@@ -8,7 +8,7 @@ Left Join spl_master_dev_details_tbl devd on devd.dev_id_fk = dev.id
 Left Join spl_master_dev_status_tbl devstate on devstate.dev_id_fk = dev.id
 $WhereCondition$`
 
-const QUERY_SPL_MASTER_DEVICE_TABLE_SELECT_BY_FILTER = `Select cust.id as id,cust.cust_name as cust_name,dev.serialno as serialno,dev.dev_state as dev_state,dev.dev_state_since as dev_state_since,dev.created_on as created_on,dev.updated_on as updated_on,devd.dev_name as dev_name,devstate.connection_state as connection_state, devstate.connection_state_since as connection_state_since,devstate.sync_state as sync_state,devstate.sync_state_since as sync_state_since,devstate.battery_level as battery_level,devstate.battery_level_since 
+const QUERY_SPL_MASTER_DEVICE_TABLE_SELECT_BY_FILTER = `Select dev.id as id,dev.cust_id_fk as cust_id_fk,cust.cust_name as cust_name,dev.serialno as serialno,dev.dev_state as dev_state,dev.dev_state_since as dev_state_since,dev.created_on as created_on,dev.updated_on as updated_on,devd.dev_name as dev_name,devstate.connection_state as connection_state, devstate.connection_state_since as connection_state_since,devstate.sync_state as sync_state,devstate.sync_state_since as sync_state_since,devstate.battery_level as battery_level,devstate.battery_level_since 
 From spl_master_device_tbl dev
 Left Join spl_master_customer_tbl  cust on cust.id = dev.cust_id_fk
 Left Join spl_master_dev_details_tbl devd on devd.dev_id_fk = dev.id
