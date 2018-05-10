927f06e13462f3c8786985a591a574ac4809966d
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-10 07:22:54 +0000

    Description: Added task models and task constants Reviewed By: Self Change Log: Bug ID:

---
+++ technical/src/appserver/src/opensoach.com/models/task.go
@@ -0,0 +1,7 @@
+package models
+
+type TaskAPICustProdAssociatedModel struct {
+	CustId int64 `db:"cust_id_fk" json:"custid"`
+	ProdId int64 `db:"prod_id_fk" json:"prodid"`
+	DbiId  int64 `db:"dbi_id_fk" json:"dbiid"`
+}
