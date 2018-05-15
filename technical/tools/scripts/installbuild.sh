03b65f6dbd17457cf57eb13ae30e211663bbaf1a
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-15 14:18:08 +0000

    Description: Modified, build with test data Reviewed By: Mandar Change Log: Bug ID:

--- technical/tools/scripts/installbuild.sh
+++ technical/tools/scripts/installbuild.sh
@@ -9,11 +9,11 @@ chmod  +777 $builddir/Build/spl
 
 
 service spl stop
-#service hkt stop
+service hkt stop
 
 #this should be remove, currently service spl stop not working hence explicity kill
 killall -9 spl
-#killall -9 hkt
+killall -9 hkt
 
 tar xvzf Build.tar.gz
 
@@ -22,25 +22,27 @@ tar xvzf Build.tar.gz
 mysql -u root -pwelcome -Bse "drop database spl_master;"
 mysql -u root -pwelcome -Bse "create database spl_master;"
 #session will be terminiated hence executing all the commands in same session
-mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_master/data/data_1.0.0.sql;source /opt/build/spl/Build/database/spl_master/testdata/testdata_1.0.0.sql;"
+mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_master/data/data_1.0.0.sql;"
+mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/testdata/testdata_1.0.0.sql;"
 
 #executing command from shell prompt 
-#mysql -u root -pwelcome -Bse "drop database spl_hkt_master;"
-#mysql -u root -pwelcome -Bse "create database spl_hkt_master;"
+mysql -u root -pwelcome -Bse "drop database spl_hkt_master;"
+mysql -u root -pwelcome -Bse "create database spl_hkt_master;"
 #session will be terminiated hence executing all the commands in same session
-#mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_master/data/data_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_master/testdata/testdata_1.0.0.sql;"
-
+mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_master/data/data_1.0.0.sql;"
+#mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/testdata/testdata_1.0.0.sql;"
 
 #executing command from shell prompt 
-#mysql -u root -pwelcome -Bse "drop database spl_hkt_node_0001;"
-#mysql -u root -pwelcome -Bse "create database spl_hkt_node_0001;"
+mysql -u root -pwelcome -Bse "drop database spl_hkt_node_0001;"
+mysql -u root -pwelcome -Bse "create database spl_hkt_node_0001;"
 #session will be terminiated hence executing all the commands in same session
-#mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_node/data/data_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_node/testdata/testdata_1.0.0.sql;"
+mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_node/data/data_1.0.0.sql;"
+#mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/testdata/testdata_1.0.0.sql;"
 
 
 
 sleep 5
 service spl start
 
-#service hkt start
+service hkt start
 
