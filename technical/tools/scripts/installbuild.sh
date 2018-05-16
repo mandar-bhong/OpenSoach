827616fae9a1bc42452ab825f0d0ca6f573bf936
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-16 06:58:24 +0000

    Description: Modified, build with test data flag added Reviewed By: Mandar Change Log: Bug ID:

--- technical/tools/scripts/installbuild.sh
+++ technical/tools/scripts/installbuild.sh
@@ -23,21 +23,27 @@ mysql -u root -pwelcome -Bse "drop database spl_master;"
 mysql -u root -pwelcome -Bse "create database spl_master;"
 #session will be terminiated hence executing all the commands in same session
 mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_master/data/data_1.0.0.sql;"
-mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/testdata/testdata_1.0.0.sql;"
+
 
 #executing command from shell prompt 
 mysql -u root -pwelcome -Bse "drop database spl_hkt_master;"
 mysql -u root -pwelcome -Bse "create database spl_hkt_master;"
 #session will be terminiated hence executing all the commands in same session
 mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_master/data/data_1.0.0.sql;"
-#mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/testdata/testdata_1.0.0.sql;"
+
 
 #executing command from shell prompt 
 mysql -u root -pwelcome -Bse "drop database spl_hkt_node_0001;"
 mysql -u root -pwelcome -Bse "create database spl_hkt_node_0001;"
 #session will be terminiated hence executing all the commands in same session
 mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_node/data/data_1.0.0.sql;"
-#mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/testdata/testdata_1.0.0.sql;"
+
+echo "ExecTestData Value:" $1
+if [ "$1" == "true" ]; then
+	mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/testdata/testdata_1.0.0.sql;"
+  mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/testdata/testdata_1.0.0.sql;"
+  mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/testdata/testdata_1.0.0.sql;"
+fi
 
 
 
