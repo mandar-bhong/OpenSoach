1e6082e65c13100e963e7a32553c715f90c5f0cb
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-16 08:01:18 +0000

    Description : Cleared toastr messages on navigating to a different url. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/web/splive/src/app/shared/services/notification/app-notification.service.ts
+++ technical/src/web/splive/src/app/shared/services/notification/app-notification.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs/Subscription';
 
 import { TranslatePipe } from '../../pipes/translate/translate.pipe';
 
 @Injectable()
 export class AppNotificationService {
+    routerEventSubscription: Subscription;
     constructor(private translatePipe: TranslatePipe,
-        private toastr: ToastrService) { }
+        private toastr: ToastrService,
+        private router: Router) {
+            this.clearMessagesOnNavigation();
+         }
 
+    clearMessagesOnNavigation() {
+        this.routerEventSubscription = this.router.events
+            .subscribe((event) => {
+                if (event instanceof NavigationStart) {
+                    this.toastr.clear();
+                }
+            });
+    }
     success(content?: any, override?: any): any {
         return this.toastr.success(content, this.translatePipe.transform('AppNotificationSuccess'),
             { timeOut: 2000, extendedTimeOut: 1000 });
