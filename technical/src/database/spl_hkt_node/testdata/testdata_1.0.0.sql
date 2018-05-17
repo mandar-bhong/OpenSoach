443d1b20e70d71802cd2dcb1d6bf9a5f17f29151
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-17 13:05:27 +0000

    Description: Added test data - cpm,dev tbl Reviewed By: Mandar Change Log: Bug ID:

---
+++ technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
@@ -0,0 +1,11 @@
+--
+-- Dumping data for table `spl_node_cpm_tbl`
+--
+
+INSERT INTO `spl_node_cpm_tbl` (`cpm_id_fk`) VALUES ('1');
+
+--
+-- Dumping data for table `spl_node_cpm_tbl`
+--
+
+INSERT INTO `spl_node_dev_tbl` (`dev_id_fk`, `cpm_id_fk`) VALUES ('1', '1');
