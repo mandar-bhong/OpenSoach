90093bbe564570d264da4cb6dca932d110bfc94f
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-17 14:23:20 +0000

    Description: Task constants added Reviewed By: Self Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/models/taskconstants.go
+++ technical/src/appserver/src/opensoach.com/models/taskconstants.go
@@ -1,8 +1,17 @@
 package models
 
+const SPL_SERVER_DEFAULT_TASK_QUEUE = "SPL.Task.Queue"
+const HKT_SERVER_DEFAULT_TASK_QUEUE = "HKT.Task.Queue"
+
 const SPL_TASK_QUEUE string = "SPL.Queue"
 const TASK_HANDLER_API_SPL_CONTROLLER_KEY string = "SPL.API.Task.Controller"
 const TASK_API_CUST_PROD_ASSOCIATED string = "SPL.API.Task.Cust.Prod.Associated"
 const TASK_API_DEV_PROD_ASSOCIATED string = "SPL.API.Task.Dev.Prod.Associated"
+
+const TASK_SPL_EP_CONNECTED string = "SPL.EndPoint.Connected"
+const TASK_SPL_EP_DISCONNECTED string = "SPL.EndPoint.DisConnected"
+
+const TASK_HKT_EP_CONNECTED string = "HKT.EndPoint.Connected"
+const TASK_HKT_EP_DISCONNECTED string = "HKT.EndPoint.DisConnected"
 
 const MOD_TASK_OPER_ERR_DATABASE int = 7000
