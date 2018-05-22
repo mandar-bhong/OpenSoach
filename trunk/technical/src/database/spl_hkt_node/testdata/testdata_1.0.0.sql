d9edd814df430dd746fce76a12166de95d1c517d
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-22 15:42:41 +0000

    Description : Added test data for location and service configuration. Changelog : NA. Bug ID: NA Reviewed By: Self

--- trunk/technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
+++ trunk/technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
@@ -17,3 +17,12 @@ INSERT INTO `spl_node_dev_tbl` (`dev_id_fk`, `cpm_id_fk`) VALUES ('1', '1');
 
 INSERT INTO `spl_node_sp_category_tbl` (`id`, `cpm_id_fk` ,`spc_name`) VALUES (1,1, 'Washroom - Mens');
 INSERT INTO `spl_node_sp_category_tbl` (`id`, `cpm_id_fk` ,`spc_name`) VALUES (2,1, 'Washroom - Womens');
+
+
+INSERT INTO `spl_node_sp_tbl` (`sp_id_fk`, `cpm_id_fk`, `spc_id_fk`, `sp_name`, `sp_state`, `sp_state_since`) VALUES ('1', '1', '1', 'Service Point 1', '1', '2018-05-22 21:03:08');
+
+INSERT INTO `spl_node_dev_sp_mapping` (`dev_id_fk`, `sp_id_fk`, `cpm_id_fk`) VALUES ('1', '1', '1');
+
+INSERT INTO `spl_node_service_conf_tbl` (`cpm_id_fk`, `spc_id_fk`, `conf_type_code`, `serv_conf_name`, `serv_conf`) VALUES ('1', '1', 'SERVICE_DAILY_CHART', 'Chart 1', '{"timeconf":{"starttime":480, "interval":30, "endtime": 1020}, "taskconf":{"tasks":[{"taskname": "task1"}, {"taskname": "task2"}, {"taskname": "task3"},{"taskname": "task4"}, {"taskname": "task5"}]}}');
+
+INSERT INTO `spl_node_service_instance_tbl` (`cpm_id_fk`, `serv_conf_id_fk`, `sp_id_fk`) VALUES ('1', '1', '1');
