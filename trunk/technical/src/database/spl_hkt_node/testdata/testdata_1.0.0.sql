04627c4799cf9035d918e9b9e054835f6a9f5750
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-22 10:07:49 +0000

    Description: Added node spc tbl test data Change Log: Bug ID:

--- trunk/technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
+++ trunk/technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
@@ -5,7 +5,15 @@
 INSERT INTO `spl_node_cpm_tbl` (`cpm_id_fk`) VALUES ('1');
 
 --
--- Dumping data for table `spl_node_cpm_tbl`
+-- Dumping data for table `spl_node_dev_tbl`
 --
 
 INSERT INTO `spl_node_dev_tbl` (`dev_id_fk`, `cpm_id_fk`) VALUES ('1', '1');
+
+
+--
+-- Dumping data for table `spl_node_sp_category_tbl`
+--
+
+INSERT INTO `spl_node_sp_category_tbl` (`id`, `cpm_id_fk` ,`spc_name`) VALUES (1,1, 'Washroom - Mens');
+INSERT INTO `spl_node_sp_category_tbl` (`id`, `cpm_id_fk` ,`spc_name`) VALUES (2,1, 'Washroom - Womens');
