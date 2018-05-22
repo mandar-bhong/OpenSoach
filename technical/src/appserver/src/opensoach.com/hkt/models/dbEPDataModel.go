4c47516bb35ef77fe6f498f30a12756d65739dac
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-22 15:17:20 +0000

    Description: Updated json tag Reviewed By: Self Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/hkt/models/dbEPDataModel.go
+++ technical/src/appserver/src/opensoach.com/hkt/models/dbEPDataModel.go
@@ -2,8 +2,8 @@ package models
 
 type DBEPSPDataModel struct {
 	ID           int64  `db:"sp_id_fk" json:"spid"`
-	Name         string `db:"sp_name" json:"name"`
-	CategoryName string `db:"spc_name" json:"catname"`
+	Name         string `db:"sp_name" json:"spname"`
+	CategoryName string `db:"spc_name" json:"spcname"`
 }
 
 type DBEPSPServConfDataModel struct {
