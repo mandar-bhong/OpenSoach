5902aa87d1feda9240eb8b6e66667773bc1e26e1
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-22 09:41:00 +0000

    Description: Added task list api Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hkt/api/webserver/task/dbaccess/dbaccess.go
+++ trunk/technical/src/appserver/src/opensoach.com/hkt/api/webserver/task/dbaccess/dbaccess.go
@@ -4,6 +4,7 @@ import (
 	"opensoach.com/core/logger"
 	dbmgr "opensoach.com/core/manager/db"
 	"opensoach.com/hkt/constants"
+	"opensoach.com/hkt/constants/dbquery"
 	hktmodels "opensoach.com/hkt/models"
 )
 
@@ -74,4 +75,21 @@ func TaskSelectAll(dbConn string) (error, *[]hktmodels.DBSplHktTaskLibTableRowMo
 	}
 	return nil, data
 
+}
+
+func TaskSelectBySPCID(dbConn string, spcid int64) (error, *[]hktmodels.DBSplHktTaskLibTableRowModel) {
+
+	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Executing TaskSelectByID")
+
+	selDBCtx := dbmgr.SelectContext{}
+	data := &[]hktmodels.DBSplHktTaskLibTableRowModel{}
+	selDBCtx.DBConnection = dbConn
+	selDBCtx.QueryType = dbmgr.Query
+	selDBCtx.Query = dbquery.QUERY_GET_HKT_TASK_LIB_TBL_BY_SCP_ID
+	selDBCtx.Dest = data
+	selErr := selDBCtx.SelectById(spcid)
+	if selErr != nil {
+		return selErr, nil
+	}
+	return nil, data
 }
