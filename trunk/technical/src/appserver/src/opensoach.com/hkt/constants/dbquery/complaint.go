2a045707644110d66e688a3316c5b800962f2bb5
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-22 10:05:07 +0000

    Description: Added task get top complaints api Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/complaint.go
+++ trunk/technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/complaint.go
@@ -5,3 +5,7 @@ Inner Join spl_node_sp_tbl sp on sp.sp_id_fk = spcomplaint.sp_id_fk $WhereCondit
 
 const QUERY_COMPLAINT_TABLE_SELECT_BY_FILTER = `Select spcomplaint.id as id,sp_name,complaint_title,description,complaint_by,severity,raised_on,complaint_state,closed_on  From spl_hkt_sp_complaint_tbl spcomplaint
 Inner Join spl_node_sp_tbl sp on sp.sp_id_fk = spcomplaint.sp_id_fk $WhereCondition$ ORDER BY $OrderByDirection$ Limit ?,?`
+
+const QUERY_GET_TOP_FIVE_COMPLAINTS = `select * from spl_hkt_sp_complaint_tbl where complaint_state = 1
+order by severity desc ,raised_on desc
+limit ?`
