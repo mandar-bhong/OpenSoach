892721ab46deea2a8fb0d71fcab676900330d533
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-23 16:08:46 +0000

    Description: User authorization is handled at prodcore Reviewed By: Self Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/prodcore/webserver/middleware/requestlogger.go
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/webserver/middleware/requestlogger.go
@@ -5,28 +5,41 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"opensoach.com/core/logger"
 )
 
 func RequestLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		buf, _ := ioutil.ReadAll(c.Request.Body)
+		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf)) //We have to create a new Buffer, because rdr1 will be read.
+
+		beforeReqestTime := time.Now()
+		logger.Context().LogDebug("RequestLogger", logger.Instrumentation, fmt.Sprintf("-----------------%s-----------------", c.Request.RequestURI))
+
 		switch c.Request.Method {
 		case "POST":
-			buf, _ := ioutil.ReadAll(c.Request.Body)
-			rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
-			rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf)) //We have to create a new Buffer, because rdr1 will be read.
-
-			fmt.Println("---------------------------------------------------------")
-			fmt.Printf("Request Body: %s \n", readBody(rdr1)) // Print request body
-			fmt.Println("---------------------------------------------------------")
-			c.Request.Body = rdr2
-			c.Next()
+			logger.Context().LogDebug("RequestLogger", logger.Instrumentation, fmt.Sprintf("Request Body: %s", readBody(rdr1))) // Print request body
 			break
 		case "GET":
-
+			requestParams := c.Request.URL.Query()
+			requestParamsData := fmt.Sprintf("Request Param: %+v", requestParams)
+			logger.Context().LogDebug("RequestLogger", logger.Instrumentation, requestParamsData) // Print request body
 		}
+
+		logger.Context().LogDebug("RequestLogger", logger.Instrumentation, "---------------------------------------------------------")
+		c.Request.Body = rdr2
+		c.Next()
+
+		afterReqestTime := time.Now()
+		diff := afterReqestTime.Sub(beforeReqestTime)
+		logger.Context().LogDebug("", logger.Performace, fmt.Sprintf("Time taken for execution '%s' request: '%s'", c.Request.RequestURI, diff.String()))
+
+		logger.Context().LogDebug("RequestLogger", logger.Instrumentation, "---------------------------------------------------------")
 	}
 }
 
--- trunk/technical/src/appserver/src/opensoach.com/spl/webserver/middleware/webhandeler.go
+++ trunk/technical/src/appserver/src/opensoach.com/spl/webserver/middleware/webhandeler.go
@@ -1,15 +1,13 @@
 package middleware
 
 import (
+	//pcmiddleware "opensoach.com/prodcore/webserver/middleware"
 	"opensoach.com/spl/constants"
 	lmodels "opensoach.com/spl/models"
 )
 
 func registerRouters(config *lmodels.WebServerConfiguration) {
 	allAuthorizedRouter := config.WebHandlerEngine.Group("/")
-
-	//allAuthorizedRouter.Use(RequestLogger)
-	allAuthorizedRouter.Use(authorizationHandler)
 
 	config.AuthorizedRouterHandler[constants.API_AUTHORIZATION_ROUTER_GROUP_KEY] = allAuthorizedRouter
 }
