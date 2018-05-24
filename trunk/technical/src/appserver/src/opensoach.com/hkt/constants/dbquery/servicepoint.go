9a0353286fee261f57fc156ab3a006e4d07a27ba
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-24 09:08:56 +0000

    Description: Modified service point listing query Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/servicepoint.go
+++ trunk/technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/servicepoint.go
@@ -7,15 +7,15 @@ const QUERY_GET_SP_CATEGORY_SHORT_LIST = `select id,spc_name from spl_node_sp_ca
 const QUERY_DELETE_DEV_SP_MAPPING_TABLE_ROW = `Delete From spl_node_dev_sp_mapping Where dev_id_fk = :dev_id_fk And cpm_id_fk = :cpm_id_fk`
 
 const QUERY_GET_NODE_SP_TABLE_TOTAL_FILTERED_COUNT = `Select count(*) as count from spl_node_sp_tbl sp 
-inner join spl_node_dev_sp_mapping devsp  on devsp.sp_id_fk = sp.sp_id_fk
+left join spl_node_dev_sp_mapping devsp  on devsp.sp_id_fk = sp.sp_id_fk
 inner join spl_node_sp_category_tbl spc on spc.id = sp.spc_id_fk
-inner join spl_node_dev_tbl dev on dev.dev_id_fk = devsp.dev_id_fk
+left join spl_node_dev_tbl dev on dev.dev_id_fk = devsp.dev_id_fk
 left join spl_node_service_conf_tbl serv_conf on serv_conf.spc_id_fk = sp.spc_id_fk $WhereCondition$`
 
 const QUERY_NODE_SP_TABLE_SELECT_BY_FILTER = `select sp.sp_id_fk,sp.sp_name,sp.spc_id_fk,spc.spc_name,devsp.dev_id_fk,dev.dev_name,serv_conf.id as id,sp.sp_state,sp.sp_state_since 
 from spl_node_sp_tbl sp 
-inner join spl_node_dev_sp_mapping devsp  on devsp.sp_id_fk = sp.sp_id_fk
+left join spl_node_dev_sp_mapping devsp  on devsp.sp_id_fk = sp.sp_id_fk
 inner join spl_node_sp_category_tbl spc on spc.id = sp.spc_id_fk
-inner join spl_node_dev_tbl dev on dev.dev_id_fk = devsp.dev_id_fk
+left join spl_node_dev_tbl dev on dev.dev_id_fk = devsp.dev_id_fk
 left join spl_node_service_conf_tbl serv_conf on serv_conf.spc_id_fk = sp.spc_id_fk
  $WhereCondition$ ORDER BY $OrderByDirection$ Limit ?,?`
