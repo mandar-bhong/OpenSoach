5c2b899a8a365389fccededdfac8ced813df8696
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-24 12:13:50 +0000

    Description: Only one device is mapped Reviewed By: Self Change Log: Bug ID:

--- technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
+++ technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
@@ -24,7 +24,7 @@ INSERT INTO `spl_node_sp_tbl` (`sp_id_fk`, `cpm_id_fk`, `spc_id_fk`, `sp_name`, 
 INSERT INTO `spl_node_sp_tbl` (`sp_id_fk`, `cpm_id_fk`, `spc_id_fk`, `sp_name`, `sp_state`, `sp_state_since`) VALUES ('2', '1', '1', 'Service Point 2', '1', '2018-05-22 21:03:08');
 
 INSERT INTO `spl_node_dev_sp_mapping` (`dev_id_fk`, `sp_id_fk`, `cpm_id_fk`) VALUES ('1', '1', '1');
---INSERT INTO `spl_node_dev_sp_mapping` (`dev_id_fk`, `sp_id_fk`, `cpm_id_fk`) VALUES ('1', '2', '1');
+
 
 INSERT INTO `spl_node_service_conf_tbl` (`cpm_id_fk`, `spc_id_fk`, `conf_type_code`, `serv_conf_name`, `serv_conf`) VALUES ('1', '1', 'SERVICE_DAILY_CHART', 'Chart 1', '{"timeconf":{"starttime":480, "interval":30, "endtime": 1020}, "taskconf":{"tasks":[{"taskname": "task1"}, {"taskname": "task2"}, {"taskname": "task3"},{"taskname": "task4"}, {"taskname": "task5"}]}}');
 
