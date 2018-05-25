d25c433e7b036a0cd6fabc392667d9c2df759b05
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-25 13:04:33 +0000

    Description : read the spl base url from server. removed environment variable base url from environment.prod.ts and environment.ts. Changelog : NA. Bug ID: NA Reviewed By: Self

--- trunk/technical/src/web/splive/src/app/hkt/environments/environment.ts
+++ trunk/technical/src/web/splive/src/app/hkt/environments/environment.ts
@@ -5,7 +5,6 @@
 
 export const environment = {
   production: false,
-  baseurl: 'http://172.105.232.148:80',
   appbaseurl: 'http://172.105.232.148:90',
   prodcode: 'SPL_HKT'
 };
---
+++ trunk/technical/src/web/splive/src/app/prod-shared/services/spl-conf.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { EnvironmentProvider } from '../../shared/environment-provider';
+import { PayloadResponse } from '../../shared/models/api/payload-models';
+import { ServerApiInterfaceService } from '../../shared/services/api/server-api-interface.service';
+import { SplBaseURLResponse } from '../models/api/spl-conf-models';
+
+@Injectable()
+export class SplConfService {
+    constructor(
+        private serverApiInterfaceService: ServerApiInterfaceService) { }
+
+    getSplBaseUrl(implicitErrorHandling = true):
+        Observable<PayloadResponse<SplBaseURLResponse>> {
+        return this.serverApiInterfaceService.get(EnvironmentProvider.appbaseurl + '/api/v1/splprod/baseurl',
+            implicitErrorHandling);
+    }
+}
+
