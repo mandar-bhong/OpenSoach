4635c069da72589b4cf54f2e9bc1481991e1f6f3
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-27 06:02:51 +0000

    Description: Removed db method Reviewed By: Self Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/prodcore/manager/manager.go
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/manager/manager.go
@@ -1,22 +1,3 @@
 package manager
 
-import (
-	"opensoach.com/core/logger"
-	gmodels "opensoach.com/models"
-	"opensoach.com/prodcore/dbaccess"
-)
-
 var SUB_MODULE_NAME = "ProdCore.Manager"
-
-func GetTaskExecutionContext(mstdbconn string, cpmid int64) (bool, *gmodels.TaskExeContextModel) {
-
-	dbErr, data := dbaccess.GetCPMDBDetails(mstdbconn, cpmid)
-
-	if dbErr != nil {
-		logger.Context().LogError(SUB_MODULE_NAME, logger.Normal, "Error occured while get db details by cpmid", dbErr)
-		return false, data
-	}
-
-	return true, data
-
-}
