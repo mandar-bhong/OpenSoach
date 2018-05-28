69b9bd62f5de957cbf9a00c540c96f3799364634
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-28 12:04:59 +0000

    Description: Implemented methods to get service data from database Reviewed By: Self Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hkt/server/dbaccess/task.go
+++ trunk/technical/src/appserver/src/opensoach.com/hkt/server/dbaccess/task.go
@@ -2,17 +2,20 @@ package dbaccess
 
 import (
 	dbmgr "opensoach.com/core/manager/db"
-	//"opensoach.com/hkt/server/constants/dbquery"
+	hktmodels "opensoach.com/hkt/models"
+	"opensoach.com/hkt/server/constants/dbquery"
 )
 
-func TaskGetSerInstDetailsById(instDBConn string, servConfIn int64) {
+func TaskGetSerConfDetailsByConfInstId(instDBConn string, servConfInID int64) (error, []hktmodels.DBDeviceSerConfigModel) {
 
+	dbDeviceSerConfigModel := []hktmodels.DBDeviceSerConfigModel{}
 	selectContext := dbmgr.SelectContext{}
 	selectContext.DBConnection = instDBConn
-	selectContext.Query = ""
+	selectContext.Query = dbquery.QUERY_GET_SER_CONFIG_BY_SER_CONF_INS_ID
 	selectContext.QueryType = dbmgr.Query
-	//selectContext.Dest
+	selectContext.Dest = &dbDeviceSerConfigModel
 
-	//selectContext.Select()
+	dbErr := selectContext.Select(servConfInID)
 
+	return dbErr, dbDeviceSerConfigModel
 }
