8b4209a5539829998273b95e99e38aa1b659227e
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-28 12:05:36 +0000

    Description: Added models and constants Reviewed By: Self Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/prodcore/constants/packet.go
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/constants/packet.go
@@ -4,6 +4,7 @@ const DEVICE_CMD_PRE_EXECUTOR string = "PreExecuteProcessor"
 
 const DEVICE_CMD_EP_HANDLER_CONNECTED string = "EndPoint.Connected"
 const DEVICE_CMD_EP_HANDLER_DISCONNECTED string = "EndPoint.DisConnected"
+const DEVICE_CMD_EP_HANDLER_SEND_PACKET string = "EndPoint.SendPacket"
 
 const DEVICE_CMD_CAT_DEVICE_VALIDATION int = 1
 const DEVICE_CMD_DEVICE_AUTH int = 1
---
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/constants/task.go
@@ -0,0 +1,3 @@
+package constants
+
+const TASK_EP_SEND_PACKET string = "Task.EP.SendPacket"
