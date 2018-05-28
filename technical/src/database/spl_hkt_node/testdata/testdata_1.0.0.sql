b127ae95cc0ea3b035ba990d046c759703348fcf
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-05-28 07:25:08 +0000

    Description : modified test data. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
+++ technical/src/database/spl_hkt_node/testdata/testdata_1.0.0.sql
@@ -26,7 +26,7 @@ INSERT INTO `spl_node_sp_tbl` (`sp_id_fk`, `cpm_id_fk`, `spc_id_fk`, `sp_name`, 
 INSERT INTO `spl_node_dev_sp_mapping` (`dev_id_fk`, `sp_id_fk`, `cpm_id_fk`) VALUES ('1', '1', '1');
 
 
-INSERT INTO `spl_node_service_conf_tbl` (`cpm_id_fk`, `spc_id_fk`, `conf_type_code`, `serv_conf_name`, `serv_conf`) VALUES ('1', '1', 'SERVICE_DAILY_CHART', 'Chart 1', '{"timeconf":{"starttime":480, "interval":30, "endtime": 1020}, "taskconf":{"tasks":[{"taskname": "task1"}, {"taskname": "task2"}, {"taskname": "task3"},{"taskname": "task4"}, {"taskname": "task5"}]}}');
+INSERT INTO `spl_node_service_conf_tbl` (`cpm_id_fk`, `spc_id_fk`, `conf_type_code`, `serv_conf_name`, `serv_conf`) VALUES ('1', '1', 'SERVICE_DAILY_CHART', 'Chart 1', '{"timeconf":{"starttime":480, "interval":30, "endtime": 1020}, "taskconf":{"tasks":[{"taskname": "Urinal"}, {"taskname": "Wash Basin"}, {"taskname": "Hand Wash"},{"taskname": "Tissue Roll"},{"taskname": "Sanitary Ware"},{"taskname": "Dustbins"},{"taskname": "Mopping"},{"taskname": "Air Freshner"},{"taskname": "Doors"}, {"taskname": "Wash Platform"}]}}');
 
 INSERT INTO `spl_node_service_instance_tbl` (`cpm_id_fk`, `serv_conf_id_fk`, `sp_id_fk`) VALUES ('1', '1', '1');
 INSERT INTO `spl_node_service_instance_tbl` (`cpm_id_fk`, `serv_conf_id_fk`, `sp_id_fk`) VALUES ('1', '1', '2');
