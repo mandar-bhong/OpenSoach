770164fc513dccf290f37e0cf76928feac7186b6
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-01 14:01:56 +0000

    Description: Modified feedback insert model added feedback_comment field Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/hkt/models/dbfeedbackRowModel.go
+++ technical/src/appserver/src/opensoach.com/hkt/models/dbfeedbackRowModel.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type DBFeedbackInsertRowModel struct {
-	CpmIdFk  int64     `db:"cpm_id_fk" json:"cpmidfk"`
-	SpIdFk   int64     `db:"sp_id_fk" json:"spidfk"`
-	Feedback int       `db:"feedback" json:"feedback"`
-	RaisedOn time.Time `db:"raised_on" json:"raisedon"`
+	CpmIdFk         int64     `db:"cpm_id_fk" json:"cpmidfk"`
+	SpIdFk          int64     `db:"sp_id_fk" json:"spidfk"`
+	Feedback        int       `db:"feedback" json:"feedback"`
+	FeedbackComment *string   `db:"feedback_comment" json:"feedbackcomment"`
+	RaisedOn        time.Time `db:"raised_on" json:"raisedon"`
 }
