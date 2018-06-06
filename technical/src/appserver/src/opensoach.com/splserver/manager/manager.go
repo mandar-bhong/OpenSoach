5a9a2dd375564b49e74878613020e50ca49c9ee8
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-06 12:15:37 +0000

    Description: Removed Endpoint handler connection function Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/splserver/manager/manager.go
+++ technical/src/appserver/src/opensoach.com/splserver/manager/manager.go
@@ -10,6 +10,4 @@ var SUB_MODULE_NAME = "SPL.Server.Manager"
 func RegisterHandler(handler map[string]interface{}) {
 	handler[gmodels.TASK_HANDLER_API_SPL_CONTROLLER_KEY] = processor.APITaskController
 
-	handler[gmodels.TASK_SPL_EP_CONNECTED] = processor.EndPointHandlerOnConnection
-	handler[gmodels.TASK_SPL_EP_DISCONNECTED] = processor.EndPointHandlerOnDisConnection
 }
