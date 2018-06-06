11797ac98348127a42cc0cee67d6463b7c487cdf
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-06 13:12:59 +0000

    Description: Modified  response as single record instead of list Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/spl/webserver/user/service.go
+++ technical/src/appserver/src/opensoach.com/spl/webserver/user/service.go
@@ -468,7 +468,7 @@ func (service UserService) GetUserProdAssociation(userID int64) (bool, interface
 	dbRecords := *data
 
 	logger.Context().LogDebug(SUB_MODULE_NAME, logger.Normal, "Successfully fetched User Product association list")
-	return true, dbRecords
+	return true, dbRecords[0]
 }
 
 func (service UserService) UpdateUcpmState(reqData *lmodels.DBUsrCpmStateUpdateRowModel) (isSuccess bool, successErrorData interface{}) {
