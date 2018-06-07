c92b47e2f6abcc93ea517c3fc4d7f8e7dcdf3437
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-07 08:14:17 +0000

    Description: Modified top active complaint api, Added noofcomplaints per month dashboard api Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/complaint.go
+++ technical/src/appserver/src/opensoach.com/hkt/constants/dbquery/complaint.go
@@ -6,6 +6,9 @@ Inner Join spl_node_sp_tbl sp on sp.sp_id_fk = spcomplaint.sp_id_fk $WhereCondit
 const QUERY_COMPLAINT_TABLE_SELECT_BY_FILTER = `Select spcomplaint.id as id,sp_name,complaint_title,description,complaint_by,severity,raised_on,complaint_state,closed_on  From spl_hkt_sp_complaint_tbl spcomplaint
 Inner Join spl_node_sp_tbl sp on sp.sp_id_fk = spcomplaint.sp_id_fk $WhereCondition$ ORDER BY $OrderByDirection$ Limit ?,?`
 
-const QUERY_GET_TOP_FIVE_COMPLAINTS = `select * from spl_hkt_sp_complaint_tbl where complaint_state = 1
+const QUERY_GET_TOP_COMPLAINTS = `select id,complaint_title,raised_on,complaint_state,severity from spl_hkt_sp_complaint_tbl $WhereCondition$
 order by severity desc ,raised_on desc
 limit ?`
+
+const QUERY_GET_NO_OF_COMPLAINTS_PER_MONTH = `select year(raised_on) as year,month(raised_on) as month,count(*) as count from spl_hkt_sp_complaint_tbl
+group by month,year`
