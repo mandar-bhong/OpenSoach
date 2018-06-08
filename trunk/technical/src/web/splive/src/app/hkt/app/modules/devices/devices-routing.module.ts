1da225a9ca65172d6b6259d9602d098e3c42163d
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-08 13:05:16 +0000

    Description :Description :  device details edit device name
    Changelog : NA.
    Bug ID: NA
    Reviewed By: Sanjay

--- trunk/technical/src/web/splive/src/app/hkt/app/modules/devices/devices-routing.module.ts
+++ trunk/technical/src/web/splive/src/app/hkt/app/modules/devices/devices-routing.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import {
-  DeviceDetailsViewComponent,
-} from '../../../../prod-shared/modules/devices/device-details-view/device-details-view.component';
 import { DeviceListComponent } from '../../../../prod-shared/modules/devices/device-list/device-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DeviceListComponent
-  },
-  {
-    path: 'details',
-    component: DeviceDetailsViewComponent
   },
 ];
 
