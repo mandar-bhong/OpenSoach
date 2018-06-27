86f98be2de6183b5ae9906cedd34a3441f1c9d36
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-27 08:26:36 +0000

    Description: corrected report tbl data Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/database/spl_hkt_node/data/data_1.0.0.sql
+++ technical/src/database/spl_hkt_node/data/data_1.0.0.sql
@@ -14,7 +14,7 @@ INSERT INTO `spl_node_report_template_tbl` (`id`, `report_code`, `report_desc`, 
 	where serv_in_txn.cpm_id_fk = $WhereCpmIdValue$ and sp_id_fk = ? and txn_date between ? and ?
 	) as tbl
 	group by taskname'),
-	(2, 'TASK_SUMMARY_SP', 'This will produce task list', '{"en": ["Loction Name", "Location Category", "Completed Tasks", "Delayed Tasks"]}', '1','select 
+	(2, 'TASK_SUMMARY_SP', 'This will produce task list', '{"en": ["Service Point", "Service Point Category", "Completed Tasks", "Delayed Tasks"]}', '1','select 
 sp.sp_name,
 spc.spc_name,
 count(serv_in_txn.`status`) as completed,
@@ -25,7 +25,7 @@ inner join spl_node_sp_category_tbl spc on sp.spc_id_fk = spc.id
 inner join spl_node_service_instance_tbl serv_conf_in on serv_conf_in.sp_id_fk = sp.sp_id_fk
 inner join spl_node_service_in_txn_tbl serv_in_txn on serv_conf_in.id = serv_in_txn.serv_in_id_fk 
 where sp.cpm_id_fk = $WhereCpmIdValue$ and sp.sp_id_fk = ? and txn_date between ? and ?'),
-	(3, 'TASK_LIST_ALL', 'This will produce task list', '{"en": ["Location Name", "Task Name", "On Time", "Delayed", "Total"]}', '1','select locationname, taskname, count(ontime) as ontime, count(delay) as \'delayed\', count(total) as total from (
+	(3, 'TASK_LIST_ALL', 'This will produce task list', '{"en": ["Service Point", "Task Name", "On Time", "Delayed", "Total"]}', '1','select locationname, taskname, count(ontime) as ontime, count(delay) as \'delayed\', count(total) as total from (
 	select
 	sp.sp_name as locationname,
 	txn_data ->> \'$.taskname\' as taskname,
