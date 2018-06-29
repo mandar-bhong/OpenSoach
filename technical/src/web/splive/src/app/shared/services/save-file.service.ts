761ab93e06241d7cb6b943417bed784f75e763e6
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-06-29 14:56:23 +0000

    Description : Fixed bug where report was not downloading on Mozilla firefox. Changelog : NA. Bug ID: NA Reviewed By: Self

--- technical/src/web/splive/src/app/shared/services/save-file.service.ts
+++ technical/src/web/splive/src/app/shared/services/save-file.service.ts
@@ -7,11 +7,11 @@ export class SaveFileService {
     saveFile(data: Blob, filename: string) {
         const blob = new Blob([data]);
         const url = window.URL.createObjectURL(blob);
-        console.log('url dowload');
-        console.log(url);
         const anchor = document.createElement('a');
         anchor.download = filename;
         anchor.href = url;
+        document.body.appendChild(anchor);   // added into dom
         anchor.click();
+        anchor.remove();   // removed from dom
     }
 }
