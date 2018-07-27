b0f652fcc0f5c04ba2ecb0b8d420860478a23927
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-07-27 13:46:01 +0000

    Removed sme version

--- technical/src/apps/user/hospital/app/src/main/java/com/opensoach/hospital/DAL/IDBOutputFormatter.java
+++
@@ -1,9 +0,0 @@
-package com.opensoach.hospital.DAL;
-
-/**
- * Created by Mandar on 10/2/2017.
- */
-
-public interface IDBOutputFormatter {
-    String OrderBy(String orderFiter);
-}
--- technical/src/apps/user/hospital/app/src/main/java/com/opensoach/hospital/Model/Communication/PacketPayloadModel.java
+++
@@ -1,8 +0,0 @@
-package com.opensoach.hospital.Model.Communication;
-
-/**
- * Created by Mandar on 8/26/2017.
- */
-
-public class PacketPayloadModel {
-}
