f27d418d80c853e5bfc34def13335473ef68f6d1
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-07-30 07:39:48 +0000

    Description: Added schema for 'spl_hpft_master', 'spl_hpft_node' database Reviewed By: Mandar Change Log: Bug ID:

---
+++ trunk/technical/src/database/spl_hpft_master/schema/schema_1.0.0.sql
@@ -0,0 +1,45 @@
+-- =============================================================================
+-- Filename   :  schema_1.0.0.sql
+
+-- Version    :  1.0
+
+-- Created on :  22-Feb-2018
+
+-- Author     :  sanjay.sawant@opensoach.com
+
+-- Description:
+-- - db schema script to create database for spl_hpft_master. a master database applicable for all HPFT node databases.
+
+-- ==========================================================================
+drop database if exists spl_hpft_master;
+create database spl_hpft_master DEFAULT CHARACTER SET utf8;
+use spl_hpft_master;
+
+--
+-- Table structure for table `spl_prod_sp_category_tbl`
+--
+
+CREATE TABLE `spl_prod_master_sp_category_tbl` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `spc_name` varchar(50) NOT NULL,
+  `short_desc` varchar(250) DEFAULT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `spc_name_UNIQUE` (`spc_name`)
+) ENGINE=InnoDB COMMENT='Short Name for Table: spc';
+
+
+--
+-- Table structure for table `spl_prod_master_serv_conf_type_tbl`
+--
+
+CREATE TABLE `spl_prod_master_serv_conf_type_tbl` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `conf_type_code` varchar(20) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `conf_type_code_UNIQUE` (`conf_type_code`)
+) ENGINE=InnoDB COMMENT='Short Name for Table: serv_conf_type';
+
