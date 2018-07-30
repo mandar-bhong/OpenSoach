c54c02a0347d5234ea4bc996bd3aeecb256b9e32
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-07-30 13:24:24 +0000

    Added framework elements

---
+++ technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/Views/Fragment/IUIUpdateEvent.java
@@ -0,0 +1,12 @@
+package com.opensoach.hpft.Views.Fragment;
+
+import com.opensoach.hpft.Model.AppNotificationModelBase;
+
+/**
+ * Created by Mandar on 25-11-2017.
+ */
+
+public interface IUIUpdateEvent {
+
+   void OnUIUpdateEvent(final AppNotificationModelBase model);
+}
