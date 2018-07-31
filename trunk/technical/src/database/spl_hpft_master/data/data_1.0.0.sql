de1e2080d945e541fc95a1841dfaf6f2b1630be4
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-07-31 06:57:04 +0000

    Description: Added schema,data for 'spl_hpft_master', 'spl_hpft_node' database Reviewed By: Mandar Change Log: Bug ID:

---
+++ trunk/technical/src/database/spl_hpft_master/data/data_1.0.0.sql
@@ -0,0 +1,16 @@
+--
+-- Dumping data for table `spl_prod_master_config`
+--
+
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Web.Service.Address',':90');
+
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Host','localhost');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Port','6379');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Password','');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.DB','2');
+
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Host','localhost');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Port','6379');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Password','');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.DB','2');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Spl.Prod.Base.Url','http://172.105.232.148');
