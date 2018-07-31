87ab95c5ec5fcfac37acbdd7ab8d32068b0c8864
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-07-31 10:52:17 +0000

    Description: Added Option to override text for the shared screens. Changelog : NA. Bug ID: NA Reviewed By: self

---
+++ trunk/technical/src/web/splive/src/app/hpft/translations.ts
@@ -0,0 +1,8 @@
+import { TranslationValue } from "../shared/pipes/translate/translations";
+
+export const APP_TRANSLATIONS: TranslationValue[] = [
+    {
+        key: 'OperatorListColumOperatorName',
+        en: 'Medical Attendant'
+    }
+];
