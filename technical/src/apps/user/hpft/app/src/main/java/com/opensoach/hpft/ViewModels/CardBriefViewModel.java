12f55ade7dd8c09646aae1d0a26f4cfa39fa5104
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-01 12:08:15 +0000

    Patient data binding completed

--- technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/ViewModels/CardBriefViewModel.java
+++ technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/ViewModels/CardBriefViewModel.java
@@ -1,8 +1,42 @@
 package com.opensoach.hpft.ViewModels;
 
+import com.opensoach.hpft.Views.Fragment.MedicalDetailsFragment;
+
 /**
  * Created by Mandar on 30-07-2018.
  */
 
 public class CardBriefViewModel extends BaseViewModel {
+
+    private PatientDetailsViewModel patientDetails;
+    private MedicalDetailsViewModel medicalDetails;
+    private TaskDetailsViewModel taskDetails;
+
+
+    public PatientDetailsViewModel getPatientDetails() {
+        return patientDetails;
+    }
+
+    public void setPatientDetails(PatientDetailsViewModel patientDetails) {
+        this.patientDetails = patientDetails;
+        this.patientDetails.ContextActivity = ContextActivity;
+    }
+
+    public MedicalDetailsViewModel getMedicalDetails() {
+        return medicalDetails;
+    }
+
+    public void setMedicalDetails(MedicalDetailsViewModel medicalDetails) {
+        this.medicalDetails = medicalDetails;
+        this.medicalDetails.ContextActivity = ContextActivity;
+    }
+
+    public TaskDetailsViewModel getTaskDetails() {
+        return taskDetails;
+    }
+
+    public void setTaskDetails(TaskDetailsViewModel taskDetails) {
+        this.taskDetails = taskDetails;
+        this.taskDetails.ContextActivity = ContextActivity;
+    }
 }
--- technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/Views/Miscellaneous/TabPagerAdapter.java
+++ technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/Views/Miscellaneous/TabPagerAdapter.java
@@ -1,13 +1,26 @@
 package com.opensoach.hpft.Views.Miscellaneous;
 
 
+import android.databinding.DataBindingUtil;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.MainThread;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import com.opensoach.hpft.R;
+import com.opensoach.hpft.ViewModels.CardBriefViewModel;
+import com.opensoach.hpft.Views.Activity.CardListActivity;
+import com.opensoach.hpft.Views.ClickHandler.CardItemClickHandler;
 import com.opensoach.hpft.Views.Fragment.MedicalDetailsFragment;
 import com.opensoach.hpft.Views.Fragment.PatientDetailsFragment;
 import com.opensoach.hpft.Views.Fragment.TaskListFragment;
+import com.opensoach.hpft.Views.Notifier.NotifyPropChangeOnUIThread;
+import com.opensoach.hpft.databinding.FragmentPatientDetailsBinding;
 
 /**
  * Created by Mandar on 31-07-2018.
@@ -16,10 +29,14 @@ import com.opensoach.hpft.Views.Fragment.TaskListFragment;
 public class TabPagerAdapter extends FragmentStatePagerAdapter {
 
     int mNumOfTabs;
+    private CardBriefViewModel cardBrief;
 
-    public TabPagerAdapter(FragmentManager fm, int NumOfTabs) {
+
+    public TabPagerAdapter(FragmentManager fm, int NumOfTabs,
+                           CardBriefViewModel vm) {
         super(fm);
         this.mNumOfTabs = NumOfTabs;
+        cardBrief = vm;
     }
 
     @Override
@@ -27,14 +44,15 @@ public class TabPagerAdapter extends FragmentStatePagerAdapter {
 
         switch (position) {
             case 0:
-                PatientDetailsFragment tab1 = new PatientDetailsFragment();
-                return tab1;
+                PatientDetailsFragment patientDetailsFragment = new PatientDetailsFragment();
+                patientDetailsFragment.DataContext = cardBrief.getPatientDetails();
+                return patientDetailsFragment;
             case 1:
-                MedicalDetailsFragment tab2 = new MedicalDetailsFragment();
-                return tab2;
+                MedicalDetailsFragment medicalDetailsFragment = new MedicalDetailsFragment();
+                return medicalDetailsFragment;
             case 2:
-                TaskListFragment tab3 = new TaskListFragment();
-                return tab3;
+                TaskListFragment taskListFragment = new TaskListFragment();
+                return taskListFragment;
             default:
                 return null;
         }
@@ -44,4 +62,5 @@ public class TabPagerAdapter extends FragmentStatePagerAdapter {
     public int getCount() {
         return mNumOfTabs;
     }
+
 }
