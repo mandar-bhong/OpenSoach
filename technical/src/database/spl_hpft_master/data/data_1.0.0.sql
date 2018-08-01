4ceb034b2e6f7b5e3e94a1ec1c836b8e93e347d9
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-01 05:04:11 +0000

    Description: added master sp category data Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/database/spl_hpft_master/data/data_1.0.0.sql
+++ technical/src/database/spl_hpft_master/data/data_1.0.0.sql
@@ -14,3 +14,13 @@ INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Prod
 INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Password','');
 INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.DB','2');
 INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Spl.Prod.Base.Url','http://172.105.232.148');
+
+
+--
+-- Dumping data for table `spl_prod_master_sp_category_tbl`
+--
+
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (1,'General Ward');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (2,'Private Room');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (3,'Semi Private');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (4,'ICU');
