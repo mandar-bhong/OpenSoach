f7e44d788418376c3720ce896618e3831145339c
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-01 11:34:20 +0000

    Description: added systemctl service for hpft Reviewed By: Mandar Change Log: Bug ID:

--- technical/tools/scripts/installbuild.sh
+++ technical/tools/scripts/installbuild.sh
@@ -13,7 +13,8 @@ chmod  +777 $builddir/Build/spl
 #service hkt stop
 systemctl stop splservice
 systemctl stop splserverservice
-systemctl stop hktservice 
+systemctl stop hktservice
+systemctl stop hpftservice
 
 systemctl daemon-reload
 
@@ -45,11 +46,28 @@ mysql -u root -pwelcome -Bse "create database spl_hkt_node_0001;"
 #session will be terminiated hence executing all the commands in same session
 mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hkt_node/data/data_1.0.0.sql;"
 
+
+#executing command from shell prompt 
+mysql -u root -pwelcome -Bse "drop database spl_hpft_master;"
+mysql -u root -pwelcome -Bse "create database spl_hpft_master;"
+#session will be terminiated hence executing all the commands in same session
+mysql -u root -pwelcome -Bse "use spl_hpft_master;source /opt/build/spl/Build/database/spl_hpft_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hpft_master/data/data_1.0.0.sql;"
+
+
+#executing command from shell prompt 
+mysql -u root -pwelcome -Bse "drop database spl_hpft_node_0001;"
+mysql -u root -pwelcome -Bse "create database spl_hpft_node_0001;"
+#session will be terminiated hence executing all the commands in same session
+mysql -u root -pwelcome -Bse "use spl_hpft_node_0001;source /opt/build/spl/Build/database/spl_hpft_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hpft_node/data/data_1.0.0.sql;"
+
+
 echo "ExecTestData Value:" $1
 if [ "$1" == "true" ]; then
 	mysql -u root -pwelcome -Bse "use spl_master;source /opt/build/spl/Build/database/spl_master/testdata/testdata_1.0.0.sql;"
   mysql -u root -pwelcome -Bse "use spl_hkt_master;source /opt/build/spl/Build/database/spl_hkt_master/testdata/testdata_1.0.0.sql;"
   mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/testdata/testdata_1.0.0.sql;"
+  mysql -u root -pwelcome -Bse "use spl_hpft_master;source /opt/build/spl/Build/database/spl_hpft_master/testdata/testdata_1.0.0.sql;"
+  mysql -u root -pwelcome -Bse "use spl_hpft_node_0001;source /opt/build/spl/Build/database/spl_hpft_node/testdata/testdata_1.0.0.sql;"
 fi
 
 
@@ -62,4 +80,5 @@ sleep 5
 systemctl start splservice
 systemctl start splserverservice
 systemctl start hktservice
+systemctl start hpftservice
 
