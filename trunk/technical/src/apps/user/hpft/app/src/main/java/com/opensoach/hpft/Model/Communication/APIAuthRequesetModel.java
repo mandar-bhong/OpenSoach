f840207d288b1013057410ca84165b886ff3ed21
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-06 08:05:58 +0000

    Added ProdCode while sending auth command

--- trunk/technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/Model/Communication/APIAuthRequesetModel.java
+++ trunk/technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/Model/Communication/APIAuthRequesetModel.java
@@ -5,4 +5,7 @@ import com.google.gson.annotations.SerializedName;
 public class APIAuthRequesetModel {
     @SerializedName("serialno")
     public String SerialNumber;
+
+    @SerializedName("prodcode")
+    public String ProdCode;
 }
