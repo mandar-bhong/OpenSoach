bd436d394bb3e498f221a022acf26ce601f4f19c
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-06 11:53:40 +0000

    Added missing model

---
+++ trunk/technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/Views/ClickHandler/HeaderUploadClickHandler.java
@@ -0,0 +1,19 @@
+package com.opensoach.hpft.Views.ClickHandler;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.opensoach.hpft.AppRepo.AppRepo;
+import com.opensoach.hpft.ViewModels.CardBriefViewModel;
+import com.opensoach.hpft.ViewModels.HeaderViewModel;
+import com.opensoach.hpft.Views.Activity.CardDetailsActivity;
+
+/**
+ * Created by Mandar on 06-08-2018.
+ */
+
+public class HeaderUploadClickHandler {
+    public void onClick(View view, HeaderViewModel vm) {
+
+    }
+}
