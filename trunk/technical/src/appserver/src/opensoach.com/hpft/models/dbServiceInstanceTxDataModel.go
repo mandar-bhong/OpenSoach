b7cf7d4e27103778ad31f798f441899571e19bd0
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-07 14:09:38 +0000

    Description: modified get txn list api - added fopname field in response model Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hpft/models/dbServiceInstanceTxDataModel.go
+++ trunk/technical/src/appserver/src/opensoach.com/hpft/models/dbServiceInstanceTxDataModel.go
@@ -14,6 +14,7 @@ type DBServiceInstanceTxBriefDataModel struct {
 	ServiceInstanceTxnID int64     `db:"id" json:"servintxnid"`
 	ServiceInstanceID    int64     `db:"serv_in_id_fk" json:"servinid"`
 	FOPCode              string    `db:"fopcode" json:"fopcode"`
+	FopName              *string   `db:"fop_name" json:"fopname"`
 	Status               int       `db:"status" json:"status"`
 	TransactionData      string    `db:"txn_data" json:"txndata"`
 	TransactionDate      time.Time `db:"txn_date" json:"txndate"`
