23a8f504018f28d15dcdad3914195f9bcdeb228d
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-08 13:17:41 +0000

    Description: modified patient tbl schema - added column discharge_on, made respective changes in apis Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/hpft/constants/dbquery/patient.go
+++ technical/src/appserver/src/opensoach.com/hpft/constants/dbquery/patient.go
@@ -1,6 +1,6 @@
 package dbquery
 
-const QUERY_GET_PATIENT_LIST = `select id,patient_details,medical_details,patient_file_template,status,patient.sp_id_fk,sp.sp_name
+const QUERY_GET_PATIENT_LIST = `select id,patient_details,medical_details,patient_file_template,status,discharged_on,patient.sp_id_fk,sp.sp_name
 from spl_hpft_patient_master_tbl patient
 inner join spl_node_sp_tbl sp on sp.sp_id_fk = patient.sp_id_fk
 where patient.cpm_id_fk=?`
