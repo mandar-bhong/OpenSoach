d6d00adfdf816e369dbed5a1385269f6e86d5248
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-08 09:21:56 +0000

    According to time, time item selected

--- trunk/technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/ViewModels/TaskTimeItemViewModel.java
+++ trunk/technical/src/apps/user/hpft/app/src/main/java/com/opensoach/hpft/ViewModels/TaskTimeItemViewModel.java
@@ -4,6 +4,7 @@ import com.opensoach.hpft.Model.View.TaskTimeItemDataModel;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Mandar on 07-08-2018.
@@ -26,6 +27,22 @@ public class TaskTimeItemViewModel extends  BaseViewModel {
         String slotDisplayText = formatter.format(taskTimeDataModel.getStartTime());
         return slotDisplayText;
     }
+
+    public boolean getIsSelectedTime(){
+
+        Date d = new Date();
+        if (taskTimeDataModel.getStartTime().getTime() < d.getTime() &&
+                taskTimeDataModel.getEndTime().getTime() >    d.getTime() ){
+
+            ((TaskDetailsViewModel) ((TaskTimeDataViewModel)this.Parent).Parent).setSelectedItem(this);
+
+            return  true;
+        }else{
+            return false;
+        }
+    }
+
+
 
 
 }
