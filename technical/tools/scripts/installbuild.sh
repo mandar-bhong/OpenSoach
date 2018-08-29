cbf9e45e952119de38dbf59b6c7baf3211a4f381
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-29 13:37:35 +0000

    Description: added vst and hpft service,modified installbuild script respectively Reviewed By: Mandar Change Log: Bug ID:

--- technical/tools/scripts/installbuild.sh
+++ technical/tools/scripts/installbuild.sh
@@ -15,6 +15,7 @@ systemctl stop splservice
 systemctl stop splserverservice
 systemctl stop hktservice
 systemctl stop hpftservice
+systemctl stop vstservice
 
 systemctl daemon-reload
 
@@ -60,6 +61,19 @@ mysql -u root -pwelcome -Bse "create database spl_hpft_node_0001;"
 #session will be terminiated hence executing all the commands in same session
 mysql -u root -pwelcome -Bse "use spl_hpft_node_0001;source /opt/build/spl/Build/database/spl_hpft_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_hpft_node/data/data_1.0.0.sql;"
 
+#executing command from shell prompt 
+mysql -u root -pwelcome -Bse "drop database spl_vst_master;"
+mysql -u root -pwelcome -Bse "create database spl_vst_master;"
+#session will be terminiated hence executing all the commands in same session
+mysql -u root -pwelcome -Bse "use spl_vst_master;source /opt/build/spl/Build/database/spl_vst_master/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_vst_master/data/data_1.0.0.sql;"
+
+
+#executing command from shell prompt 
+mysql -u root -pwelcome -Bse "drop database spl_vst_node_0001;"
+mysql -u root -pwelcome -Bse "create database spl_vst_node_0001;"
+#session will be terminiated hence executing all the commands in same session
+mysql -u root -pwelcome -Bse "use spl_vst_node_0001;source /opt/build/spl/Build/database/spl_vst_node/schema/schema_1.0.0.sql;source /opt/build/spl/Build/database/spl_vst_node/data/data_1.0.0.sql;"
+
 
 echo "ExecTestData Value:" $1
 if [ "$1" == "true" ]; then
@@ -68,6 +82,7 @@ if [ "$1" == "true" ]; then
   mysql -u root -pwelcome -Bse "use spl_hkt_node_0001;source /opt/build/spl/Build/database/spl_hkt_node/testdata/testdata_1.0.0.sql;"
   mysql -u root -pwelcome -Bse "use spl_hpft_master;source /opt/build/spl/Build/database/spl_hpft_master/testdata/testdata_1.0.0.sql;"
   mysql -u root -pwelcome -Bse "use spl_hpft_node_0001;source /opt/build/spl/Build/database/spl_hpft_node/testdata/testdata_1.0.0.sql;"
+  mysql -u root -pwelcome -Bse "use spl_vst_node_0001;source /opt/build/spl/Build/database/spl_vst_node/testdata/testdata_1.0.0.sql;"
 fi
 
 
@@ -81,4 +96,4 @@ systemctl start splservice
 systemctl start splserverservice
 systemctl start hktservice
 systemctl start hpftservice
-
+systemctl start vstservice
