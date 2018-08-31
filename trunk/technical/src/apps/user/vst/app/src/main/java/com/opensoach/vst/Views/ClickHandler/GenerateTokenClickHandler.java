8dfb2eed4617c7791f73c4fb67030b72813c4409
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-08-31 12:18:56 +0000

    Added Job Selection activity

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/GenerateTokenClickHandler.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/GenerateTokenClickHandler.java
@@ -1,6 +1,7 @@
 package com.opensoach.vst.Views.ClickHandler;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 
 import com.opensoach.vst.AppRepo.AppRepo;
@@ -11,6 +12,7 @@ public class GenerateTokenClickHandler {
 
     public void onClick(View view) {
         Intent i = new Intent(view.getContext(), CreateTokenActivity.class);
+        i.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
         view.getContext().startActivity(i);
     }
 }
