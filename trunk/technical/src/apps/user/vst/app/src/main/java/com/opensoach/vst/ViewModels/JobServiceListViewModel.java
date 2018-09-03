2a1aa65de9128cf30eea812c533e76f1e3be2a2d
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-03 10:35:08 +0000

    Description:Add JobServiceListViewModel . Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/ViewModels/JobServiceListViewModel.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/ViewModels/JobServiceListViewModel.java
@@ -1,4 +1,28 @@
 package com.opensoach.vst.ViewModels;
 
+import com.opensoach.vst.Views.Adapter.JobServiceDataAdapter;
+
+import java.util.List;
+
 public class JobServiceListViewModel extends BaseViewModel{
+
+    private JobServiceDataAdapter jobServiceDataAdapter;
+    private List<JobServiceItemViewModel> data;
+    private JobServiceItemViewModel selectedJob;
+
+    public JobServiceListViewModel() {
+        this.jobServiceDataAdapter = new JobServiceDataAdapter();
+    }
+
+    public JobServiceDataAdapter getJobServiceDataAdapter() {
+        return jobServiceDataAdapter;
+    }
+
+    public List<JobServiceItemViewModel> getData() {
+        return data;
+    }
+
+    public void setData(List<JobServiceItemViewModel> data) {
+        this.data = data;
+    }
 }
