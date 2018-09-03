bb4913fe706d74e4583bc6b7ae161b16bd7e7199
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-03 08:57:42 +0000

    Description :Description : VST:- Dashboard design changes Changelog : NA. Bug ID: NA Reviewed By: Sanjay

--- trunk/technical/src/web/splive/src/app/vst/app/modules/dashboard/vehicle-weekly-trend/vehicle-weekly-trend.component.ts
+++ trunk/technical/src/web/splive/src/app/vst/app/modules/dashboard/vehicle-weekly-trend/vehicle-weekly-trend.component.ts
@@ -26,23 +26,43 @@ export class VehicleWeeklyTrendComponent implements OnInit {
   // legendTitle = 'Task Status';
   customColors = [
     {
-      name: this.ontimeLabel,
-      value: '#28a745'
+      name: 'SUN',
+      value: '#245AAE'
     },
-    // {
-    //   name: this.delayedLabel,
-    //   value: '#ffc107'
-    // },
+    {
+      name: 'MON',
+      value: '#E466C9'
+    },
+    {
+      name: 'TUE',
+      value: '#956EE8'
+    },
+    {
+      name: 'WED',
+      value: '#00B1EA'
+    },
+    {
+      name: 'THU',
+      value: '#00DDC6'
+    },
+    {
+      name: 'FRI',
+      value: '#FFB467'
+    },
+    {
+      name: 'SAT',
+      value: '#FF6859'
+    }
     // {
     //   name: this.missedLabel,
     //   value: '#ff5252'
     // },
   ];
-data:any;
+  data: any;
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
-   this.data= [
+    this.data = [
       {
         "name": "MON",
         "value": 82
@@ -72,83 +92,83 @@ data:any;
         "value": 100
       }
     ];
-   // this.getTaskTrend();
+    // this.getTaskTrend();
   }
 
-//   getTaskTrend() {
-//     const currentDate = new Date();
-//     this.request.enddate = new Date(Date.UTC(
-//       currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), currentDate.getHours(), currentDate.getMinutes()));
+  //   getTaskTrend() {
+  //     const currentDate = new Date();
+  //     this.request.enddate = new Date(Date.UTC(
+  //       currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), currentDate.getHours(), currentDate.getMinutes()));
 
-//     const ticks = Date.UTC(this.request.enddate.getUTCFullYear(), this.request.enddate.getUTCMonth() - 11, 1);
-//     this.request.startdate = new Date(ticks);
+  //     const ticks = Date.UTC(this.request.enddate.getUTCFullYear(), this.request.enddate.getUTCMonth() - 11, 1);
+  //     this.request.startdate = new Date(ticks);
 
-// //     this.dashboardService.getTaskTrend(this.request).subscribe(payloadResponse => {
-// //       if (payloadResponse && payloadResponse.issuccess) {
-// //         payloadResponse.data.forEach(item => {
-// //           const trendModel = new TaskTrendModel();
-// //           trendModel.copyFrom(item);
-// //           this.tasktrenddata.push(trendModel);
-// //         });
-// // console.log('data received');
-// //         this.generateSeriesTimeline();
-// //         this.generateRatingChartData();
-// //       }
-// //     });
+  // //     this.dashboardService.getTaskTrend(this.request).subscribe(payloadResponse => {
+  // //       if (payloadResponse && payloadResponse.issuccess) {
+  // //         payloadResponse.data.forEach(item => {
+  // //           const trendModel = new TaskTrendModel();
+  // //           trendModel.copyFrom(item);
+  // //           this.tasktrenddata.push(trendModel);
+  // //         });
+  // // console.log('data received');
+  // //         this.generateSeriesTimeline();
+  // //         this.generateRatingChartData();
+  // //       }
+  // //     });
 
-//     this.generateSeriesTimeline();
-//         this.generateRatingChartData();
-//   }
+  //     this.generateSeriesTimeline();
+  //         this.generateRatingChartData();
+  //   }
 
 
 
-//   generateRatingChartData() {
-//     const ontimeData = { name: this.ontimeLabel, series: [] };
-//     this.tasktrendchartdata.push(ontimeData);
-//     const delayedData = { name: this.delayedLabel, series: [] };
-//     this.tasktrendchartdata.push(delayedData);
-//     const missedData = { name: this.missedLabel, series: [] };
-//     this.tasktrendchartdata.push(missedData);
+  //   generateRatingChartData() {
+  //     const ontimeData = { name: this.ontimeLabel, series: [] };
+  //     this.tasktrendchartdata.push(ontimeData);
+  //     const delayedData = { name: this.delayedLabel, series: [] };
+  //     this.tasktrendchartdata.push(delayedData);
+  //     const missedData = { name: this.missedLabel, series: [] };
+  //     this.tasktrendchartdata.push(missedData);
 
-//     let dummyOnTime =1000;
-//     let dummyDelayed =205;
-//     let dummyMissed =50;
-//     this.timeline.forEach(item => {
-//       const xAxisDate = new Date(item.year, item.month).toUTCString();
-//       // const trendModel = this.tasktrenddata.find(rating => rating.year === item.year
-//       //   && rating.month-1 === item.month);
+  //     let dummyOnTime =1000;
+  //     let dummyDelayed =205;
+  //     let dummyMissed =50;
+  //     this.timeline.forEach(item => {
+  //       const xAxisDate = new Date(item.year, item.month).toUTCString();
+  //       // const trendModel = this.tasktrenddata.find(rating => rating.year === item.year
+  //       //   && rating.month-1 === item.month);
 
-//       // if (trendModel) {
-//       //   ontimeData.series.push({ name: xAxisDate, value: trendModel.ontime });
-//       //   delayedData.series.push({ name: xAxisDate, value: trendModel.delayed });
-//       //   missedData.series.push({ name: xAxisDate, value: trendModel.missed });
-//       // } else {
-//         ontimeData.series.push({ name: xAxisDate, value: dummyOnTime });
-//         delayedData.series.push({ name: xAxisDate, value: dummyDelayed });
-//         missedData.series.push({ name: xAxisDate, value: dummyMissed });
+  //       // if (trendModel) {
+  //       //   ontimeData.series.push({ name: xAxisDate, value: trendModel.ontime });
+  //       //   delayedData.series.push({ name: xAxisDate, value: trendModel.delayed });
+  //       //   missedData.series.push({ name: xAxisDate, value: trendModel.missed });
+  //       // } else {
+  //         ontimeData.series.push({ name: xAxisDate, value: dummyOnTime });
+  //         delayedData.series.push({ name: xAxisDate, value: dummyDelayed });
+  //         missedData.series.push({ name: xAxisDate, value: dummyMissed });
 
-//         dummyOnTime=dummyOnTime+12;
-//         dummyDelayed=dummyDelayed-13;
-//         dummyMissed=dummyMissed-3;
-//       //}
-//     });
-//   }
+  //         dummyOnTime=dummyOnTime+12;
+  //         dummyDelayed=dummyDelayed-13;
+  //         dummyMissed=dummyMissed-3;
+  //       //}
+  //     });
+  //   }
 
-//   generateSeriesTimeline() {
-//     let month = this.request.startdate.getMonth();
-//     let year = this.request.startdate.getFullYear();
-//     for (let i = 0; i < 12; i++) {
-//       this.timeline.push({ year: year, month: month });
-//       month = month + 1;
-//       if (month > 11) {
-//         month = 0;
-//         year = year + 1;
-//       }
-//     }
-//   }
+  //   generateSeriesTimeline() {
+  //     let month = this.request.startdate.getMonth();
+  //     let year = this.request.startdate.getFullYear();
+  //     for (let i = 0; i < 12; i++) {
+  //       this.timeline.push({ year: year, month: month });
+  //       month = month + 1;
+  //       if (month > 11) {
+  //         month = 0;
+  //         year = year + 1;
+  //       }
+  //     }
+  //   }
 
-//   formatXAxis(value: string) {
-//     const date = new Date(value);
-//     return date.toLocaleDateString('en-US', { month: 'short' });
-//   }
+  //   formatXAxis(value: string) {
+  //     const date = new Date(value);
+  //     return date.toLocaleDateString('en-US', { month: 'short' });
+  //   }
 }
