feb0247e8afb6f5c72f1688068d365313b0dc6c5
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-03 12:18:51 +0000

    Description:add getter setter method . Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/ViewModels/JobServiceItemViewModel.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/ViewModels/JobServiceItemViewModel.java
@@ -1,4 +1,20 @@
 package com.opensoach.vst.ViewModels;
 
+import android.databinding.Bindable;
+
+import com.opensoach.vst.Model.View.JobServiceItemDataModel;
+
 public class JobServiceItemViewModel extends BaseViewModel {
+
+    private String fname;
+
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
 }
