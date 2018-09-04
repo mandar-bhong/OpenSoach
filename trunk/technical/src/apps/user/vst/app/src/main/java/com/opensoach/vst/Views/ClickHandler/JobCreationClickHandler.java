7130f4aabb9ca486c605e58ab8482e64d1008628
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-04 13:37:08 +0000

    Description:Create ClickHandler  . Reviewed By: Mandar Change Log: Bug ID:

---
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobCreationClickHandler.java
@@ -0,0 +1,20 @@
+package com.opensoach.vst.Views.ClickHandler;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.opensoach.vst.ViewModels.JobServiceCreationViewModel;
+import com.opensoach.vst.ViewModels.JobServiceListViewModel;
+import com.opensoach.vst.ViewModels.MainViewModel;
+import com.opensoach.vst.Views.Activity.JobCreationActivity;
+import com.opensoach.vst.Views.Activity.JobServiceCreationActivity;
+import com.opensoach.vst.Views.Activity.JobServiceListActivity;
+
+public class JobCreationClickHandler {
+
+    public void  onClick (View view, JobServiceCreationViewModel vm){
+
+        Intent i = new Intent(MainViewModel.getInstance().ContextActivity, JobServiceListActivity.class);
+        MainViewModel.getInstance().ContextActivity.startActivity(i);
+    }
+}
