6b1f5144e3bb355ce4422610754a2ffdd0cbc668
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-05 09:36:20 +0000

    Description:add finish().  . Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TaskCreateCompleteClickHandler.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TaskCreateCompleteClickHandler.java
@@ -1,16 +1,24 @@
 package com.opensoach.vst.Views.ClickHandler;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 
 import com.opensoach.vst.AppRepo.AppRepo;
 import com.opensoach.vst.ViewModels.JobServiceItemViewModel;
+import com.opensoach.vst.ViewModels.MainViewModel;
 import com.opensoach.vst.ViewModels.TaskCreationViewModel;
 import com.opensoach.vst.ViewModels.TaskItemViewModel;
+import com.opensoach.vst.Views.Activity.JobCreationActivity;
+import com.opensoach.vst.Views.Activity.JobServiceCreationActivity;
+import com.opensoach.vst.Views.Activity.JobServiceListActivity;
 
 public class TaskCreateCompleteClickHandler {
 
     public void onClick(View view, JobServiceItemViewModel vm) {
         AppRepo.getInstance().getJobServiceViewModel().getJobServiceListViewModel().getJobServiceDataAdapter().addItem(vm);
-        vm.ContextActivity.finish();
+
+        ((Activity)view.getContext()).finish();
+
     }
 }
