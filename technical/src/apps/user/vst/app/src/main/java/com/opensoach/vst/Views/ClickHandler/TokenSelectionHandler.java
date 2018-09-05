5e54787ff64131a3a4299248ed0a4e32d669b138
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-05 10:35:15 +0000

    Set VM when token selection is completed

--- technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TokenSelectionHandler.java
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TokenSelectionHandler.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.view.View;
 
 import com.opensoach.vst.AppRepo.AppRepo;
+import com.opensoach.vst.ViewModels.JobDetailsViewModel;
+import com.opensoach.vst.ViewModels.JobServiceItemViewModel;
+import com.opensoach.vst.ViewModels.JobServiceListViewModel;
 import com.opensoach.vst.ViewModels.JobServiceViewModel;
 import com.opensoach.vst.ViewModels.TokenItemViewModel;
 import com.opensoach.vst.ViewModels.TokenListViewModel;
@@ -11,15 +14,30 @@ import com.opensoach.vst.ViewModels.TokenSelectionViewModel;
 import com.opensoach.vst.Views.Activity.JobCreationActivity;
 import com.opensoach.vst.Views.Activity.TaskDetailsActivity;
 
+import java.util.ArrayList;
+
 public class TokenSelectionHandler {
 
     public void onClick(View view, TokenSelectionViewModel vm) {
 
-        AppRepo.getInstance().setJobServiceViewModel(new JobServiceViewModel());
-        AppRepo.getInstance().getJobServiceViewModel().ContextActivity = vm.ContextActivity;
-        AppRepo.getInstance().getJobServiceViewModel().Parent = vm;
+        JobDetailsViewModel jobDetailsViewModel = new JobDetailsViewModel();
+        jobDetailsViewModel.Parent = vm;
+        jobDetailsViewModel.ContextActivity = vm.ContextActivity;
 
-        AppRepo.getInstance().getJobServiceViewModel().setTokenSelectionViewModel(vm);
+        JobServiceListViewModel jobServiceListViewModel = new  JobServiceListViewModel();
+        jobServiceListViewModel.Parent = jobDetailsViewModel;
+        jobServiceListViewModel.ContextActivity = vm.ContextActivity;
+        jobServiceListViewModel.setData(new ArrayList<JobServiceItemViewModel>());
+
+        JobServiceViewModel  jobServiceViewModel = new JobServiceViewModel();
+        jobServiceViewModel.Parent = vm;
+        jobServiceViewModel.ContextActivity = vm.ContextActivity;
+        jobServiceViewModel.setJobDetailsViewModel(jobDetailsViewModel);
+        jobServiceViewModel.setJobServiceListViewModel(jobServiceListViewModel);
+        jobServiceViewModel.setTokenSelectionViewModel(vm);
+
+
+        AppRepo.getInstance().setJobServiceViewModel(jobServiceViewModel);
 
         Intent i = new Intent(view.getContext(), JobCreationActivity.class);
         view.getContext().startActivity(i);
