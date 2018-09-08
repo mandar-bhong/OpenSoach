a9647d6fe665fbb6ff02eccc34f89bbf17b5ec35
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-08 13:19:20 +0000

    Description: create new JobExeDetailsActivity.java activity Change Log: Bug ID:

---
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/Activity/JobExeDetailsActivity.java
@@ -0,0 +1,32 @@
+package com.opensoach.vst.Views.Activity;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.opensoach.vst.R;
+import com.opensoach.vst.ViewModels.MainViewModel;
+import com.opensoach.vst.Views.Fragment.HeaderFragment;
+import com.opensoach.vst.Views.Fragment.TokenItemFragment;
+
+public class JobExeDetailsActivity extends AppCompatActivity
+        implements TokenItemFragment.OnFragmentInteractionListener,
+        HeaderFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_exe_details);
+
+        MainViewModel.getInstance().ContextActivity = this;
+
+        //TODO: This step is importent for adding fragment into activity
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.headerPlace, HeaderFragment.newInstance("","")).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
