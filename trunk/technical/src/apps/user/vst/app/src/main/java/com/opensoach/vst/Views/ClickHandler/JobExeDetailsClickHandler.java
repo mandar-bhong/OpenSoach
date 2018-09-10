caa68f57b9cfc66b0c74ea7360f591d523f5d3e3
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-10 10:50:09 +0000

    Description: Add jobExDetailsViewModel property in AppRepo.java.                set TokentItemViewMode And tokenSelectionViewModel in JobExeDetailsViewModel for get viehiclNo and TokenNo .               setBinding in JobExeDetailsActivity.java.               tokenNo and VehicleNo bind in activity_job_exe_details.xml.               add handler in fragment_job_service_item.xml Change Log: Bug ID:

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobExeDetailsClickHandler.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobExeDetailsClickHandler.java
@@ -1,4 +1,19 @@
 package com.opensoach.vst.Views.ClickHandler;
 
-public class JobExeDetailsClickHandler {
+import android.content.Intent;
+import android.view.View;
+
+import com.opensoach.vst.AppRepo.AppRepo;
+import com.opensoach.vst.ViewModels.JobServiceItemViewModel;
+import com.opensoach.vst.ViewModels.JobServiceListViewModel;
+import com.opensoach.vst.ViewModels.MainViewModel;
+import com.opensoach.vst.Views.Activity.JobExeDetailsActivity;
+
+public class JobExeDetailsClickHandler  {
+
+    public void onClick(View view, JobServiceItemViewModel vm) {
+
+        Intent i = new Intent(view.getContext(), JobExeDetailsActivity.class);
+        view.getContext().startActivity(i);
+    }
 }
