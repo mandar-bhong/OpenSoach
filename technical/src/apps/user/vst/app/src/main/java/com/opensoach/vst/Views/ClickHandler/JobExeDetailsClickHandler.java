4d6bddc38df548cf5402a79c0612f4c4d93322fb
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-11 09:51:05 +0000

    Description:set vm for get taskname in JobExeDetailsClickHandler.java Change Log: Bug ID:

--- technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobExeDetailsClickHandler.java
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobExeDetailsClickHandler.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.view.View;
 
 import com.opensoach.vst.AppRepo.AppRepo;
+import com.opensoach.vst.ViewModels.JobExeDetailsViewModel;
 import com.opensoach.vst.ViewModels.JobServiceItemViewModel;
 import com.opensoach.vst.ViewModels.JobServiceListViewModel;
 import com.opensoach.vst.ViewModels.MainViewModel;
@@ -12,7 +13,7 @@ import com.opensoach.vst.Views.Activity.JobExeDetailsActivity;
 public class JobExeDetailsClickHandler  {
 
     public void onClick(View view, JobServiceItemViewModel vm) {
-
+      AppRepo.getInstance().getJobServiceViewModel().setJobServiceItemViewModel(vm);
         Intent i = new Intent(view.getContext(), JobExeDetailsActivity.class);
         view.getContext().startActivity(i);
     }
