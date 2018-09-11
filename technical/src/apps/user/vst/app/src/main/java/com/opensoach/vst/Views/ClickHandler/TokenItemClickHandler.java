e8c7f9214723de5ed0716f90e992becf8daffb47
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-11 10:13:03 +0000

    When same item is selected then no action is performed

--- technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TokenItemClickHandler.java
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TokenItemClickHandler.java
@@ -11,9 +11,12 @@ import com.opensoach.vst.Views.Adapter.TokensDataAdapter;
 public class TokenItemClickHandler {
 
     public void onClick(View view, TokenItemViewModel vm) {
+
+        if (((TokenListViewModel)vm.Parent).getSelectedToken() == vm)
+            return;
+
         ((TokenListViewModel)vm.Parent).getTokensDataAdapter().SelectedIndexChange(vm.position);
         ((TokenListViewModel)vm.Parent).setSelectedToken(vm);
-
 
     }
 }
