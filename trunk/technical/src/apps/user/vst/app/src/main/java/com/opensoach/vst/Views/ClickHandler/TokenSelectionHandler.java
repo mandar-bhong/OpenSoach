026cf9dec3516d616a7f4847c8439b4cc8595e98
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-14 07:49:07 +0000

    Switch case is implemented

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TokenSelectionHandler.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/TokenSelectionHandler.java
@@ -24,81 +24,79 @@ public class TokenSelectionHandler {
 
     public void onClick(View view, TokenSelectionViewModel vm) {
 
+        switch (AppRepo.getInstance().getCurrentRunningMode()) {
+            case Token: {
+
+            }
+            break;
+            case JobCreation: {
+                JobServiceDetailsViewModel jobDetailsViewModel = new JobServiceDetailsViewModel();
+                jobDetailsViewModel.Parent = vm;
+                jobDetailsViewModel.ContextActivity = vm.ContextActivity;
+
+                jobDetailsViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
+                AppRepo.getInstance().setJobServiceDetailsViewModel(jobDetailsViewModel);
+
+                JobServiceListViewModel jobServiceListViewModel = new JobServiceListViewModel();
+                jobServiceListViewModel.Parent = jobDetailsViewModel;
+                jobServiceListViewModel.ContextActivity = vm.ContextActivity;
+                jobServiceListViewModel.setData(new ArrayList<JobServiceItemViewModel>());
+
+                JobServiceViewModel jobServiceViewModel = new JobServiceViewModel();
+                jobServiceViewModel.Parent = vm;
+                jobServiceViewModel.ContextActivity = vm.ContextActivity;
+
+                jobServiceViewModel.setJobServiceDetailsViewModel(jobDetailsViewModel);
+                jobServiceViewModel.setJobServiceListViewModel(jobServiceListViewModel);
+                jobServiceViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
+                AppRepo.getInstance().setJobServiceViewModel(jobServiceViewModel);
 
 
-        if (  AppRepo.getInstance().getCurrentRunningMode() == ApplicationConstants.AppRunningMode.Token)
-        {
+                Intent i = new Intent(view.getContext(), JobCreationActivity.class);
+                view.getContext().startActivity(i);
 
 
+
+            }
+            break;
+            case JobExecution: {
+
+                JobServiceDetailsViewModel jobDetailsViewModel = new JobServiceDetailsViewModel();
+                jobDetailsViewModel.Parent = vm;
+                jobDetailsViewModel.ContextActivity = vm.ContextActivity;
+
+                jobDetailsViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
+                AppRepo.getInstance().setJobServiceDetailsViewModel(jobDetailsViewModel);
+
+                JobServiceListViewModel jobServiceListViewModel = new JobServiceListViewModel();
+                jobServiceListViewModel.Parent = jobDetailsViewModel;
+                jobServiceListViewModel.ContextActivity = vm.ContextActivity;
+                jobServiceListViewModel.setData(new ArrayList<JobServiceItemViewModel>());
+
+                JobServiceViewModel jobServiceViewModel = new JobServiceViewModel();
+                jobServiceViewModel.Parent = vm;
+                jobServiceViewModel.ContextActivity = vm.ContextActivity;
+
+                jobServiceViewModel.setJobServiceDetailsViewModel(jobDetailsViewModel);
+                jobServiceViewModel.setJobServiceListViewModel(jobServiceListViewModel);
+                jobServiceViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
+
+
+                AppRepo.getInstance().setJobServiceViewModel(jobServiceViewModel);
+
+                JobExeDetailsViewModel jobExeDetailsViewModel = new JobExeDetailsViewModel();
+                jobExeDetailsViewModel.Parent = vm;
+                jobExeDetailsViewModel.ContextActivity = vm.ContextActivity;
+
+                jobExeDetailsViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
+                AppRepo.getInstance().setJobExeDetailsViewModel(jobExeDetailsViewModel);
+
+
+                Intent i = new Intent(view.getContext(), JobServiceListActivity.class);
+                view.getContext().startActivity(i);
+                break;
+            }
         }
-        else if( AppRepo.getInstance().getCurrentRunningMode() == ApplicationConstants.AppRunningMode.JobExecution)
-        {
-            JobServiceDetailsViewModel jobDetailsViewModel = new JobServiceDetailsViewModel();
-            jobDetailsViewModel.Parent = vm;
-            jobDetailsViewModel.ContextActivity = vm.ContextActivity;
-
-            jobDetailsViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
-            AppRepo.getInstance().setJobServiceDetailsViewModel(jobDetailsViewModel);
-
-            JobServiceListViewModel jobServiceListViewModel = new  JobServiceListViewModel();
-            jobServiceListViewModel.Parent = jobDetailsViewModel;
-            jobServiceListViewModel.ContextActivity = vm.ContextActivity;
-            jobServiceListViewModel.setData(new ArrayList<JobServiceItemViewModel>());
-
-            JobServiceViewModel  jobServiceViewModel = new JobServiceViewModel();
-            jobServiceViewModel.Parent = vm;
-            jobServiceViewModel.ContextActivity = vm.ContextActivity;
-
-            jobServiceViewModel.setJobServiceDetailsViewModel(jobDetailsViewModel);
-            jobServiceViewModel.setJobServiceListViewModel(jobServiceListViewModel);
-            jobServiceViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
-
-
-            AppRepo.getInstance().setJobServiceViewModel(jobServiceViewModel);
-
-            JobExeDetailsViewModel jobExeDetailsViewModel =new JobExeDetailsViewModel();
-            jobExeDetailsViewModel.Parent = vm;
-            jobExeDetailsViewModel.ContextActivity =vm.ContextActivity;
-
-            jobExeDetailsViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
-            AppRepo.getInstance().setJobExeDetailsViewModel(jobExeDetailsViewModel);
-
-
-
-            Intent i = new Intent(view.getContext(), JobServiceListActivity.class);
-            view.getContext().startActivity(i);
-
-        }
-        else if( AppRepo.getInstance().getCurrentRunningMode() == ApplicationConstants.AppRunningMode.JobCreation)
-        {
-
-            JobServiceDetailsViewModel jobDetailsViewModel = new JobServiceDetailsViewModel();
-            jobDetailsViewModel.Parent = vm;
-            jobDetailsViewModel.ContextActivity = vm.ContextActivity;
-
-            jobDetailsViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
-            AppRepo.getInstance().setJobServiceDetailsViewModel(jobDetailsViewModel);
-
-            JobServiceListViewModel jobServiceListViewModel = new  JobServiceListViewModel();
-            jobServiceListViewModel.Parent = jobDetailsViewModel;
-            jobServiceListViewModel.ContextActivity = vm.ContextActivity;
-            jobServiceListViewModel.setData(new ArrayList<JobServiceItemViewModel>());
-
-            JobServiceViewModel  jobServiceViewModel = new JobServiceViewModel();
-            jobServiceViewModel.Parent = vm;
-            jobServiceViewModel.ContextActivity = vm.ContextActivity;
-
-            jobServiceViewModel.setJobServiceDetailsViewModel(jobDetailsViewModel);
-            jobServiceViewModel.setJobServiceListViewModel(jobServiceListViewModel);
-            jobServiceViewModel.setTokenItemViewModel(vm.getTokenListViewModel().getSelectedToken());
-            AppRepo.getInstance().setJobServiceViewModel(jobServiceViewModel);
-
-
-            Intent i = new Intent(view.getContext(), JobCreationActivity.class);
-            view.getContext().startActivity(i);
-
-
-        } else {}
 
 
     }
