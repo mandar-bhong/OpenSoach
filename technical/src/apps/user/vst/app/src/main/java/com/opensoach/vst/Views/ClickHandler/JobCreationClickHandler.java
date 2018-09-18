efb81647432780f0903da2b012d097e8b8bdd8ce
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-18 09:26:36 +0000

    Renamed files

--- technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobCreationClickHandler.java
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobCreationClickHandler.java
@@ -1,15 +1,5 @@
 package com.opensoach.vst.Views.ClickHandler;
 
-import android.content.Intent;
-import android.view.View;
-
-import com.opensoach.vst.AppRepo.AppRepo;
-import com.opensoach.vst.ViewModels.JobServiceListViewModel;
-import com.opensoach.vst.ViewModels.MainViewModel;
-import com.opensoach.vst.Views.Activity.JobCreationActivity;
-import com.opensoach.vst.Views.Activity.JobServiceCreationActivity;
-import com.opensoach.vst.Views.Activity.JobServiceListActivity;
-
 public class JobCreationClickHandler {
 
 //    public void  onClick (View view, JobServiceCreationViewModel vm){
--- technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobServiceTaskListCreateHandler.java
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/ClickHandler/JobServiceTaskListCreateHandler.java
@@ -3,23 +3,14 @@ package com.opensoach.vst.Views.ClickHandler;
 import android.content.Intent;
 import android.view.View;
 
-import com.opensoach.vst.AppRepo.AppRepo;
-import com.opensoach.vst.Helper.AppAction;
-import com.opensoach.vst.Manager.SendPacketManager;
-import com.opensoach.vst.ViewModels.CreateTokenViewModel;
 import com.opensoach.vst.ViewModels.JobServiceListViewModel;
-import com.opensoach.vst.ViewModels.JobServiceViewModel;
-import com.opensoach.vst.ViewModels.MainViewModel;
-import com.opensoach.vst.Views.Activity.JobCreationActivity;
-import com.opensoach.vst.Views.Activity.JobServiceCreationActivity;
-import com.opensoach.vst.Views.Activity.JobServiceListActivity;
-import com.opensoach.vst.Views.Activity.TaskDetailsActivity;
+import com.opensoach.vst.Views.Activity.JobServiceTaskCreationActivity;
 
 public class JobServiceTaskListCreateHandler {
 
     public void onClick(View view, JobServiceListViewModel vm) {
 
-        Intent i = new Intent(view.getContext(), JobServiceCreationActivity.class);
+        Intent i = new Intent(view.getContext(), JobServiceTaskCreationActivity.class);
         view.getContext().startActivity(i);
     }
 
