47b96e7eed15a542144c83b6d64333d1933efda8
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-18 09:26:36 +0000

    Renamed files

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/Activity/JobServiceTaskCreationActivity.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Views/Activity/JobServiceTaskCreationActivity.java
@@ -4,10 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.EditText;
 
 import com.opensoach.vst.AppRepo.AppRepo;
@@ -15,16 +11,12 @@ import com.opensoach.vst.Constants.ApplicationConstants;
 import com.opensoach.vst.R;
 import com.opensoach.vst.ViewModels.JobServiceItemViewModel;
 import com.opensoach.vst.ViewModels.MainViewModel;
-import com.opensoach.vst.Views.ClickHandler.GenerateTokenClickHandler;
 import com.opensoach.vst.Views.ClickHandler.TaskCreateCompleteClickHandler;
 import com.opensoach.vst.Views.Fragment.HeaderFragment;
 import com.opensoach.vst.Views.Fragment.TokenItemFragment;
 import com.opensoach.vst.databinding.ActivityJobServiceCreationBinding;
-import com.opensoach.vst.databinding.ActivityTokenListBinding;
 
-import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
-
-public class JobServiceCreationActivity extends AppCompatActivity
+public class JobServiceTaskCreationActivity extends AppCompatActivity
         implements TokenItemFragment.OnFragmentInteractionListener,
         HeaderFragment.OnFragmentInteractionListener{
 
@@ -33,7 +25,7 @@ public class JobServiceCreationActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job_service_creation);
+        setContentView(R.layout.activity_job_service_task_creation);
 
         MainViewModel.getInstance().ContextActivity = this;
 
@@ -47,7 +39,7 @@ public class JobServiceCreationActivity extends AppCompatActivity
 
 
     void setBinding(){
-        ActivityJobServiceCreationBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_job_service_creation);
+        ActivityJobServiceCreationBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_job_service_task_creation);
 
         JobServiceItemViewModel jobServiceItemViewModel = new JobServiceItemViewModel();
         jobServiceItemViewModel.Parent = AppRepo.getInstance().getJobServiceViewModel().getJobServiceListViewModel();
