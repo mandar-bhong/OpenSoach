2e836100126db4b567ef0e444f4af808af863db6
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-09-19 10:20:57 +0000

    Added tokenid in transactional data

--- trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Model/Communication/PacketVehicleDetailsModel.java
+++ trunk/technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/Model/Communication/PacketVehicleDetailsModel.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 
 public class PacketVehicleDetailsModel {
 
+    @SerializedName("tokenid")
+    public Integer TokenID;
+
     @SerializedName("customerdetails")
     public PacketServiceCustomerDetailsDataModel CustomerDetails;
 
