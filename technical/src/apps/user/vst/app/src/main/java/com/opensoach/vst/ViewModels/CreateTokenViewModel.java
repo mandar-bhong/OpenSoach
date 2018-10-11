81fc66e2abf874246d7d64d1cd40c9aaa85041bd
rahul.mogal <rahul.mogal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-10-11 07:19:30 +0000

    Description:add constant value (MH 12) Change Log: Bug ID:

--- technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/ViewModels/CreateTokenViewModel.java
+++ technical/src/apps/user/vst/app/src/main/java/com/opensoach/vst/ViewModels/CreateTokenViewModel.java
@@ -37,27 +37,27 @@ public class CreateTokenViewModel extends BaseViewModel  {
     }
 
     public String getVehicleNumber() {
-        return vehicleNo1 + vehicleNo2 +" " + vehicleNo3 + vehicleNo4;
+        return "MH 12 " + vehicleNo3 + " " + vehicleNo4;
     }
 
-    public String getVehicleNo1() {
-        return vehicleNo1;
-    }
-
-    @Bindable
-    public void setVehicleNo1(String vehicleNo1) {
-        this.vehicleNo1 = vehicleNo1;
-    }
-
-
-    public String getVehicleNo2() {
-        return vehicleNo2;
-    }
-
-    @Bindable
-    public void setVehicleNo2(String vehicleNo2) {
-        this.vehicleNo2 = vehicleNo2;
-    }
+//    public String getVehicleNo1() {
+//        return vehicleNo1;
+//    }
+//
+//    @Bindable
+//    public void setVehicleNo1(String vehicleNo1) {
+//        this.vehicleNo1 = vehicleNo1;
+//    }
+//
+//
+//    public String getVehicleNo2() {
+//        return vehicleNo2;
+//    }
+//
+//    @Bindable
+//    public void setVehicleNo2(String vehicleNo2) {
+//        this.vehicleNo2 = vehicleNo2;
+//    }
 
 
     public String getVehicleNo3() {
