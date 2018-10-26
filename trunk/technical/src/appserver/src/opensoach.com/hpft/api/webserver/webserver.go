221b7fd6784d3235d2db470870ce2a8a771ec10d
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-10-26 07:15:22 +0000

    Added profiling handler

--- trunk/technical/src/appserver/src/opensoach.com/hpft/api/webserver/webserver.go
+++ trunk/technical/src/appserver/src/opensoach.com/hpft/api/webserver/webserver.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/gin-contrib/pprof"
+	"github.com/gin-contrib/pprof"	
 	"github.com/gin-gonic/gin"
 	"github.com/itsjamie/gin-cors"
 	"opensoach.com/hpft/api/constants"
@@ -30,7 +30,7 @@ import (
 func Init(configSetting *gmodels.ConfigSettings) error {
 
 	ginEngine := gin.Default()
-	pprof.Register(ginEngine, &pprof.Options{RoutePrefix: "debug/pprof"})
+	pprof.Register(ginEngine, "debug/pprof")
 
 	enableCrossDomain(ginEngine)
 
