2d1ac4b9315a0f2b0fbf157d32c0eb8d084ab8ac
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-11-02 13:16:57 +0000

    Description : local notification Changelog : NA. Bug ID: NA Reviewed By: Sanjay

--- trunk/technical/src/apps/user/patientcare/src/app/modules/list/list.component.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/modules/list/list.component.ts
@@ -3,9 +3,11 @@ import { RouterExtensions } from "nativescript-angular/router";
 import { SearchBar } from "tns-core-modules/ui/search-bar";
 import { isAndroid } from "platform";
 import * as app from "application";
-// import { RadSideDrawerComponent, SideDrawerType } from "nativescript-ui-sidedrawer/angular";
-// import { ViewChild } from "@angular/core";
-// import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Page } from "ui/page";
+
+import { alert } from "tns-core-modules/ui/dialogs";
+import { LocalNotifications } from "nativescript-local-notifications";
+
 @Component({
 	moduleId: module.id,
 	selector: 'list',
@@ -17,17 +19,16 @@ export class ListComponent implements OnInit {
 	data = [];
 	searchshow = false;
 	searchiocn = true;
-	// @ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
-	constructor(private routerExtensions: RouterExtensions) { }
+	constructor(private routerExtensions: RouterExtensions,
+		private page: Page) { }
 
 	ngOnInit() {
-		// this.data.push({ text: "Bulbasaur", src: "" });
 		this.data.push({ ward: "3A/312", name: "Sumeet karande", mobile: "9878978980" });
 		this.data.push({ ward: "3B/323", name: "Amol Patil", mobile: "32423432432" });
-		// this.data.push({ ward: "2A/643", name: "Sanjay Mohan", mobile: "453545352"});
-		// this.data.push({ ward: "4A/515", name: "Sanjay Sawant", mobile: "9878978980"});
-		// this.data.push({ ward: "5A/616", name: "Parveen", mobile: "5645645665"});
-		// this.data.push({ ward: "6A/897", name: "Mandar bhong", mobile: "98789909090"});
+		this.data.push({ ward: "2A/643", name: "Sanjay Mohan", mobile: "453545352"});
+		this.data.push({ ward: "4A/515", name: "Sanjay Sawant", mobile: "9878978980"});
+		this.data.push({ ward: "5A/616", name: "Parveen", mobile: "5645645665"});
+		this.data.push({ ward: "6A/897", name: "Mandar bhong", mobile: "98789909090"});
 		// this.data.push({ ward: "7A/244", name: "Om", mobile: "323434355445"});
 		// this.data.push({ ward: "3B/324", name: "Ahubham", mobile: "9809878679"});
 		// this.data.push({ ward: "2A/454", name: "Suraj", mobile: "76568768778"});
@@ -54,18 +55,13 @@ export class ListComponent implements OnInit {
 	details() {
 		this.routerExtensions.navigate(["/list/details"], { clearHistory: true });
 	}
-	// onOpenDrawerTap() {
-    //     this.drawerComponent.sideDrawer.showDrawer();
-    // }
-    // onCloseDrawerTap() {
-    //     this.drawerComponent.sideDrawer.closeDrawer();
-    // }
 
 	searchTab() {
 		this.searchshow = true;
 		this.searchiocn =  false;
+		// this.page.actionBarHidden = true;
 	}
-	searchTabClose(){
+	searchTabClose() {
 		this.searchshow = false;
 		this.searchiocn =  true;
 	}
@@ -78,4 +74,39 @@ export class ListComponent implements OnInit {
 	onTextChange() {
 
 	}
+
+
+	 showWithSound(): void {
+        LocalNotifications.schedule([{
+          id: 1,
+          title: 'Sound & Badge',
+          body: 'Who needs a push service anyway?',
+          badge: 1,
+          at: new Date(new Date().getTime() + (5 * 1000)) // 5 seconds from now
+        }]);
+    
+        // adding a handler, so we can do something with the received notification.. in this case an alert
+        LocalNotifications.addOnMessageReceivedCallback(data => {
+          alert({
+            title: "Local Notification received",
+            message: `id: '${data.id}', title: '${data.title}'.`,
+            okButtonText: "Roger that"
+          });
+        });
+      }
+    
+      conti(): void {
+        LocalNotifications.schedule([{
+          id: 2,
+          title: 'Cancel me, quickly!',
+          body: 'Who thought this would be a good idea!?',
+          interval: 'minute',
+          sound: null,
+          at: new Date(new Date().getTime() + (5 * 1000)), // 5 seconds from now
+        }]);
+      }
+    
+      cancelAll(): void {
+        LocalNotifications.cancelAll();
+      }
 }
