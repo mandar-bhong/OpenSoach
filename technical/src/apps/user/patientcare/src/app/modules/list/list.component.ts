897b548c893f873c6b5700edcb441b04cbdf524a
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-11-29 14:49:16 +0000

    Used nativescript-ui-listview RadListView control instead of ListView. Added web socket test integration

--- technical/src/apps/user/patientcare/src/app/modules/list/list.component.ts
+++ technical/src/apps/user/patientcare/src/app/modules/list/list.component.ts
@@ -7,6 +7,14 @@ import { Page } from "ui/page";
 
 import { alert } from "tns-core-modules/ui/dialogs";
 import { LocalNotifications } from "nativescript-local-notifications";
+import { ObservableArray } from "tns-core-modules/data/observable-array";
+
+export class DataItem {
+	public name: string;
+	public ward: string;
+	public mobile: string;
+
+}
 
 @Component({
 	moduleId: module.id,
@@ -15,20 +23,38 @@ import { LocalNotifications } from "nativescript-local-notifications";
 	styleUrls: ['./list.component.css']
 })
 
+
 export class ListComponent implements OnInit {
-	data = [];
+	private _dataItems: ObservableArray<DataItem>;
+	data = new Array<DataItem>();
 	searchshow = false;
 	searchiocn = true;
 	constructor(private routerExtensions: RouterExtensions,
 		private page: Page) { }
 
+		get dataItems(): ObservableArray<DataItem> {
+			return this._dataItems;
+	}
 	ngOnInit() {
-		this.data.push({ ward: "3A/312", name: "Sumeet karande", mobile: "9878978980" });
-		this.data.push({ ward: "3B/323", name: "Amol Patil", mobile: "32423432432" });
-		this.data.push({ ward: "2A/643", name: "Sanjay Mohan", mobile: "453545352"});
-		this.data.push({ ward: "4A/515", name: "Sanjay Sawant", mobile: "9878978980"});
-		this.data.push({ ward: "5A/616", name: "Parveen", mobile: "5645645665"});
-		this.data.push({ ward: "6A/897", name: "Mandar bhong", mobile: "98789909090"});
+
+		const tempdata = new Array<DataItem>();
+		tempdata.push({ ward: "3A/312", name: "Sumeet karande", mobile: "9878978980" });
+		tempdata.push({ ward: "3B/323", name: "Amol Patil", mobile: "32423432432" });
+		tempdata.push({ ward: "2A/643", name: "Sanjay Mohan", mobile: "453545352"});
+		tempdata.push({ ward: "4A/515", name: "Sanjay Sawant", mobile: "9878978980"});
+		tempdata.push({ ward: "5A/616", name: "Parveen", mobile: "5645645665"});
+		
+
+		
+
+		for(let i=0;i<20;i++)
+		{
+			tempdata.forEach(item=>{
+				this.data.push(item);
+			})
+		}
+
+		this._dataItems = new ObservableArray(this.data);
 		// this.data.push({ ward: "7A/244", name: "Om", mobile: "323434355445"});
 		// this.data.push({ ward: "3B/324", name: "Ahubham", mobile: "9809878679"});
 		// this.data.push({ ward: "2A/454", name: "Suraj", mobile: "76568768778"});
@@ -108,5 +134,7 @@ export class ListComponent implements OnInit {
     
       cancelAll(): void {
         LocalNotifications.cancelAll();
-      }
+			}
+			
+	
 }
