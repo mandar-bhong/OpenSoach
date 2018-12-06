fe6305f260b0d7c269ffa95baa98c9f683cb858e
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-12-06 11:18:38 +0000

    Description : network status check online and offline Bug ID: NA Reviewed By: Sanjay

---
+++ trunk/technical/src/apps/user/patientcare/src/app/network-status.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterExtensions } from "nativescript-angular/router";
+
+
+import { InternetConnectionService } from '~/app/services/internet-status/internet-connection.service';
+
+@Component({
+    moduleId: module.id,
+    selector: "network-status",
+    template: `
+    <Label  class="homeicon mdi" text="{{(connectionStatus) ? '&#xf12f;' : '&#xf12f;'}}" [style.color]="connectionStatus ? 'green' : 'red'"></Label>
+    `,
+    styles: [`   ` ]
+
+})
+export class NetworkStatusComponent implements OnInit, OnDestroy  {
+    connectionStatus: boolean = true;
+    connection$;
+    constructor(private routerExtensions: RouterExtensions,
+        private _internetConnection: InternetConnectionService) {
+    }
+    ngOnInit() {
+		this.connection$ = this._internetConnection.connectionStatus$.subscribe(data => {
+            this.connectionStatus = data.valueOf();
+        });
+    }
+    ngOnDestroy(): void {
+        if (this.connection$)
+            this.connection$.unsubscribe();
+    }
+}
