7790cc6b69ac025a15c44be298f42c47be75e8dd
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-12-06 11:22:20 +0000

    Description : home list lazy loading code comment, search bar filter record Bug ID: NA Reviewed By: Sanjay

--- technical/src/apps/user/patientcare/src/app/modules/list/list.module.ts
+++ technical/src/apps/user/patientcare/src/app/modules/list/list.module.ts
@@ -6,6 +6,13 @@ import { ListRoutingModule } from "./list-routing.module";
 import { ListComponent } from "./list.component";
 import { DetailsComponent } from "./details/details.component";
 import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
+import { NetworkStatusComponent } from "~/app/network-status.component";
+import { FloatBtnComponent } from "~/app/float-btn.component";
+import { CamerasComponent } from "~/app/modules/list/cameras/cameras.component";
+import { PatientDetailsComponent } from "~/app/modules/list/patient-details/patient-details.component";
+import { MonitorComponent } from "~/app/modules/list/monitor/monitor.component";
+import { ActionComponent } from "~/app/modules/list/action/action.component";
+import { ChartsComponent } from "~/app/modules/list/charts/charts.component";
 
 @NgModule({
     imports: [
@@ -16,7 +23,14 @@ import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
     ],
     declarations: [
         ListComponent,
-        DetailsComponent
+        DetailsComponent,
+        NetworkStatusComponent,
+        FloatBtnComponent,
+        CamerasComponent,
+        PatientDetailsComponent,
+        MonitorComponent,
+        ActionComponent,
+        ChartsComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
