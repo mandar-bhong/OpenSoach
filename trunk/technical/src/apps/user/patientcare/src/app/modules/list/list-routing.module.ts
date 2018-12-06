b815033fba306875ccdc61ab20472b833ed03621
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-12-06 11:22:20 +0000

    Description : home list lazy loading code comment, search bar filter record Bug ID: NA Reviewed By: Sanjay

--- trunk/technical/src/apps/user/patientcare/src/app/modules/list/list-routing.module.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/modules/list/list-routing.module.ts
@@ -4,11 +4,35 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { ListComponent } from "./list.component";
 import { DetailsComponent } from "./details/details.component";
+import { CamerasComponent } from "~/app/modules/list/cameras/cameras.component";
+import { PatientDetailsComponent } from "~/app/modules/list/patient-details/patient-details.component";
+import { MonitorComponent } from "~/app/modules/list/monitor/monitor.component";
+import { ActionComponent } from "~/app/modules/list/action/action.component";
+import { ChartsComponent } from "~/app/modules/list/charts/charts.component";
 
 
 const routes: Routes = [
-    { path: "", component: ListComponent },
-    { path: "details", component: DetailsComponent }
+    { 
+        path: "", component: ListComponent 
+    },
+    { 
+        path: "details", component: DetailsComponent 
+    },
+    { 
+        path: "cameras", component: CamerasComponent
+    },
+    { 
+        path: "patient", component: PatientDetailsComponent
+    },
+    { 
+        path: "monitor", component: MonitorComponent
+    },
+    { 
+        path: "action", component: ActionComponent
+    },
+    { 
+        path: "charts", component: ChartsComponent
+    },
 ];
 
 @NgModule({
