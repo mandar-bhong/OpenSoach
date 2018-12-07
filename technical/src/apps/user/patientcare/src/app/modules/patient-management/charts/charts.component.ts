1e2a240174490f486dd7e8e0a7beb355a32542fb
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-12-07 12:50:05 +0000

    Description : list component move to patient management component Bug ID: NA Reviewed By: Sanjay

---
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/charts/charts.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'charts',
+	templateUrl: './charts.component.html',
+	styleUrls: ['./charts.component.css']
+})
+
+export class ChartsComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit() {
+		// alert('charts');
+	 }
+}
---
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-mgnt-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { PatientMgntComponent } from "~/app/modules/patient-management/patient-mgnt.component";
+import { DetailsComponent } from "~/app/modules/patient-management/details/details.component";
+import { CamerasComponent } from "~/app/modules/patient-management/cameras/cameras.component";
+import { PatientDetailsComponent } from "~/app/modules/patient-management/patient-details/patient-details.component";
+import { MonitorComponent } from "~/app/modules/patient-management/monitor/monitor.component";
+import { ActionComponent } from "~/app/modules/patient-management/action/action.component";
+import { ChartsComponent } from "~/app/modules/patient-management/charts/charts.component";
+
+
+const routes: Routes = [
+    { 
+        path: "", component: PatientMgntComponent 
+    },
+    { 
+        path: "details", component: DetailsComponent 
+    },
+    { 
+        path: "cameras", component: CamerasComponent
+    },
+    { 
+        path: "patient", component: PatientDetailsComponent
+    },
+    { 
+        path: "monitor", component: MonitorComponent
+    },
+    { 
+        path: "action", component: ActionComponent
+    },
+    { 
+        path: "charts", component: ChartsComponent
+    },
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forChild(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class PatientMgntRoutingModule { }
