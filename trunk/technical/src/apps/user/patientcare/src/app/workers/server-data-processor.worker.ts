c7c6984cfc721b9b5c4f37b1ebe477198287b3e6
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2018-12-18 06:32:35 +0000

    Added package ts_loader required for workers. added worker service to initialize workers. added worker with dummy code for server data processor

---
+++ trunk/technical/src/apps/user/patientcare/src/app/workers/server-data-processor.worker.ts
@@ -0,0 +1,11 @@
+require('globals');
+
+const context: Worker = self as any;
+
+context.onmessage = msg => {
+    setTimeout(() => {
+        console.log("Inside TS worker...");
+        console.log(msg);
+        (<any>global).postMessage("TS Worker");
+    }, 500)
+};
