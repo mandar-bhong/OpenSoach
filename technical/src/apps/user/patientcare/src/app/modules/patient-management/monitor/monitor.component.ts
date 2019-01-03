d77f5a0fd0764a2bc42f3eb90c30981915190d37
sarjerao.ghadage <sarjerao.ghadage@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-03 13:03:35 +0000

    Temperature and blood pressure chart updated with multiline charts

--- technical/src/apps/user/patientcare/src/app/modules/patient-management/monitor/monitor.component.ts
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/monitor/monitor.component.ts
@@ -8,22 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MonitorComponent implements OnInit {
-	categoricalSource: { Country: string, Amount: any }[] = [
-		{ Country: "08:00 AM", Amount: 98 },
-		{ Country: "12:00 AM", Amount: 98.3 },
-		{ Country: "04:00 PM", Amount: 99 },
-		{ Country: "08:00 PM", Amount: 101 },
-		{ Country: "12:00 PM", Amount: 97 },
-		{ Country: "04:00 AM", Amount: 100 }
+	// categoricalSource: { Country: string, Amount: any }[] = [
+	// 	{ Country: "08:00 AM", Amount: 98 },
+	// 	{ Country: "12:00 AM", Amount: 98.3 },
+	// 	{ Country: "04:00 PM", Amount: 99 },
+	// 	{ Country: "08:00 PM", Amount: 101 },
+	// 	{ Country: "12:00 PM", Amount: 97 },
+	// 	{ Country: "04:00 AM", Amount: 100 }
+	// ];
+	categoricalSourcehigh: { Country: string, Amount1: number }[] = [
+        { Country: "08:00 AM", Amount1: 20 },
+        { Country: "12:00 AM", Amount1: 20 },
+        { Country: "04:00 PM", Amount1: 20 },
+        { Country: "12:00 PM", Amount1: 20 },
+        { Country: "04:00 AM", Amount1: 20 },
+
+    ];
+    categoricalSourcelow: { Country: string, Amount1: number }[] = [
+        { Country: "08:00 AM", Amount1: 5 },
+        { Country: "12:00 AM", Amount1: 5 },
+        { Country: "04:00 PM", Amount1: 5 },
+        { Country: "12:00 PM", Amount1:5 },
+        { Country: "04:00 AM", Amount1: 5 },
+
+    ];
+    categoricalSource: { Country: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
+        { Country: "08:00 AM", Amount: 15, SecondVal: 14, ThirdVal: 24 },
+        { Country: "12:00 AM", Amount: 13, SecondVal: 23, ThirdVal: 25 },
+        { Country: "04:00 PM", Amount: 24, SecondVal: 17, ThirdVal: 23 },
+        { Country: "12:00 PM", Amount: 11, SecondVal: 19, ThirdVal: 24 },
+        { Country: "04:00 AM", Amount: 18, SecondVal: 8, ThirdVal: 21 }
 	];
-	bloodSource: { Country: string, Amount: any }[] = [
-		{ Country: "08:00 AM", Amount: 70 },
-		{ Country: "12:00 AM", Amount: 90 },
-		{ Country: "04:00 PM", Amount: 75 },
-		{ Country: "08:00 PM", Amount: 110 },
-		{ Country: "12:00 PM", Amount: 80 },
-		{ Country: "04:00 AM", Amount: 130 }
+	
+	bloodSourcecategoricalSourcehigh: { Country: string, Amount1: number }[] = [
+        { Country: "08:00 AM", Amount1: 80 },
+        { Country: "12:00 AM", Amount1: 80 },
+        { Country: "04:00 PM", Amount1: 80 },
+        { Country: "12:00 PM", Amount1: 80 },
+        { Country: "04:00 AM", Amount1: 80 },
+
+    ];
+    bloodSourcecategoricalSourcelow: { Country: string, Amount1: number }[] = [
+        { Country: "08:00 AM", Amount1: 25 },
+        { Country: "12:00 AM", Amount1: 25 },
+        { Country: "04:00 PM", Amount1: 25 },
+        { Country: "12:00 PM", Amount1: 25 },
+        { Country: "04:00 AM", Amount1: 25 },
+
+    ];
+    bloodSourcecategoricalSource: { Country: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
+        { Country: "08:00 AM", Amount: 40, SecondVal: 70, ThirdVal: 24 },
+        { Country: "12:00 AM", Amount: 50, SecondVal: 23, ThirdVal: 25 },
+        { Country: "04:00 PM", Amount: 40, SecondVal: 17, ThirdVal: 23 },
+        { Country: "12:00 PM", Amount: 80, SecondVal: 19, ThirdVal: 24 },
+        { Country: "04:00 AM", Amount: 70, SecondVal: 8, ThirdVal: 21 }
 	];
+	
+
+	// bloodSource: { Country: string, Amount: any }[] = [
+	// 	{ Country: "08:00 AM", Amount: 70 },
+	// 	{ Country: "12:00 AM", Amount: 90 },
+	// 	{ Country: "04:00 PM", Amount: 75 },
+	// 	{ Country: "08:00 PM", Amount: 110 },
+	// 	{ Country: "12:00 PM", Amount: 80 },
+	// 	{ Country: "04:00 AM", Amount: 130 }
+	// ];
 	constructor() { }
 
 	ngOnInit() {
