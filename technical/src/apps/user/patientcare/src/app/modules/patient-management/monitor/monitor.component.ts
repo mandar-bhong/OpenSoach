817af4f056743aee90821b97eb8afc2deb67463f
sarjerao.ghadage <sarjerao.ghadage@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-04 11:00:04 +0000

    Separate action bar component created for patient details and monitor chart updated

--- technical/src/apps/user/patientcare/src/app/modules/patient-management/monitor/monitor.component.ts
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/monitor/monitor.component.ts
@@ -1,82 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	moduleId: module.id,
-	selector: 'monitor',
-	templateUrl: './monitor.component.html',
-	styleUrls: ['./monitor.component.css']
+    moduleId: module.id,
+    selector: 'monitor',
+    templateUrl: './monitor.component.html',
+    styleUrls: ['./monitor.component.css']
 })
 
 export class MonitorComponent implements OnInit {
-	// categoricalSource: { Country: string, Amount: any }[] = [
-	// 	{ Country: "08:00 AM", Amount: 98 },
-	// 	{ Country: "12:00 AM", Amount: 98.3 },
-	// 	{ Country: "04:00 PM", Amount: 99 },
-	// 	{ Country: "08:00 PM", Amount: 101 },
-	// 	{ Country: "12:00 PM", Amount: 97 },
-	// 	{ Country: "04:00 AM", Amount: 100 }
-	// ];
-	categoricalSourcehigh: { Country: string, Amount1: number }[] = [
-        { Country: "08:00 AM", Amount1: 20 },
-        { Country: "12:00 AM", Amount1: 20 },
-        { Country: "04:00 PM", Amount1: 20 },
-        { Country: "12:00 PM", Amount1: 20 },
-        { Country: "04:00 AM", Amount1: 20 },
+    // categoricalSource: { Country: string, Amount: any }[] = [
+    // 	{ Country: "08:00 AM", Amount: 98 },
+    // 	{ Country: "12:00 AM", Amount: 98.3 },
+    // 	{ Country: "04:00 PM", Amount: 99 },
+    // 	{ Country: "08:00 PM", Amount: 101 },
+    // 	{ Country: "12:00 PM", Amount: 97 },
+    // 	{ Country: "04:00 AM", Amount: 100 }
+    // ];
+    categoricalSourcehigh: { Country: string, Amount1: number }[] = [
+        { Country: "12:00 PM", Amount1:99.5 },
+        { Country: "04:00 PM", Amount1: 99.5 },
+        { Country: "08:00 PM", Amount1: 99.5 },
+        { Country: "12:00 AM", Amount1: 99.5 },
+        { Country: "04:00 AM", Amount1: 99.5 },
 
     ];
     categoricalSourcelow: { Country: string, Amount1: number }[] = [
-        { Country: "08:00 AM", Amount1: 5 },
-        { Country: "12:00 AM", Amount1: 5 },
-        { Country: "04:00 PM", Amount1: 5 },
-        { Country: "12:00 PM", Amount1:5 },
-        { Country: "04:00 AM", Amount1: 5 },
+        { Country: "12:00 PM", Amount1:  97.7 },
+        { Country: "04:00 PM", Amount1:  97.7 },
+        { Country: "08:00 PM", Amount1:  97.7 },
+        { Country: "12:00 AM", Amount1:  97.7 },
+        { Country: "04:00 AM", Amount1:  97.7 },
 
     ];
-    categoricalSource: { Country: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
-        { Country: "08:00 AM", Amount: 15, SecondVal: 14, ThirdVal: 24 },
-        { Country: "12:00 AM", Amount: 13, SecondVal: 23, ThirdVal: 25 },
-        { Country: "04:00 PM", Amount: 24, SecondVal: 17, ThirdVal: 23 },
-        { Country: "12:00 PM", Amount: 11, SecondVal: 19, ThirdVal: 24 },
-        { Country: "04:00 AM", Amount: 18, SecondVal: 8, ThirdVal: 21 }
-	];
-	
-	bloodSourcecategoricalSourcehigh: { Country: string, Amount1: number }[] = [
-        { Country: "08:00 AM", Amount1: 80 },
-        { Country: "12:00 AM", Amount1: 80 },
-        { Country: "04:00 PM", Amount1: 80 },
-        { Country: "12:00 PM", Amount1: 80 },
-        { Country: "04:00 AM", Amount1: 80 },
+    categoricalSource: { Country: string, Amount: number }[] = [
+        { Country: "12:00 PM", Amount: 98.6 },
+        { Country: "04:00 PM", Amount: 97.3 },
+        { Country: "08:00 PM", Amount: 99.0},
+        { Country: "12:00 AM", Amount: 98.0 },
+        { Country: "04:00 AM", Amount: 99.8 }
+    ];
+
+    bloodSourcecategoricalSourcehigh: { Country: string, Amount1: number }[] = [
+        { Country: "12:00 PM", Amount1: 140 },
+        { Country: "04:00 PM", Amount1: 140 },
+        { Country: "08:00 PM", Amount1: 140 },
+        { Country: "12:00 AM", Amount1: 140 },
+        { Country: "04:00 AM", Amount1: 140 },
 
     ];
     bloodSourcecategoricalSourcelow: { Country: string, Amount1: number }[] = [
-        { Country: "08:00 AM", Amount1: 25 },
-        { Country: "12:00 AM", Amount1: 25 },
-        { Country: "04:00 PM", Amount1: 25 },
-        { Country: "12:00 PM", Amount1: 25 },
-        { Country: "04:00 AM", Amount1: 25 },
+        { Country: "12:00 PM", Amount1: 90 },
+        { Country: "04:00 PM", Amount1: 90 },
+        { Country: "08:00 PM", Amount1: 90 },
+        { Country: "12:00 AM", Amount1: 90 },
+        { Country: "04:00 AM", Amount1: 89 },
 
     ];
-    bloodSourcecategoricalSource: { Country: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
-        { Country: "08:00 AM", Amount: 40, SecondVal: 70, ThirdVal: 24 },
-        { Country: "12:00 AM", Amount: 50, SecondVal: 23, ThirdVal: 25 },
-        { Country: "04:00 PM", Amount: 40, SecondVal: 17, ThirdVal: 23 },
-        { Country: "12:00 PM", Amount: 80, SecondVal: 19, ThirdVal: 24 },
-        { Country: "04:00 AM", Amount: 70, SecondVal: 8, ThirdVal: 21 }
-	];
-	
+    bloodSourcecategoricalSource: { Country: string, Amount: number }[] = [
+        { Country: "12:00 PM", Amount: 100 },
+        { Country: "04:00 PM", Amount: 150 },
+        { Country: "08:00 PM", Amount: 130},
+        { Country: "12:00 AM", Amount: 87 },
+        { Country: "04:00 AM", Amount: 110 }
+    ];
 
-	// bloodSource: { Country: string, Amount: any }[] = [
-	// 	{ Country: "08:00 AM", Amount: 70 },
-	// 	{ Country: "12:00 AM", Amount: 90 },
-	// 	{ Country: "04:00 PM", Amount: 75 },
-	// 	{ Country: "08:00 PM", Amount: 110 },
-	// 	{ Country: "12:00 PM", Amount: 80 },
-	// 	{ Country: "04:00 AM", Amount: 130 }
-	// ];
-	constructor() { }
 
-	ngOnInit() {
-		// alert('monitor');
-	}
+    // bloodSource: { Country: string, Amount: any }[] = [
+    // 	{ Country: "08:00 AM", Amount: 70 },
+    // 	{ Country: "12:00 AM", Amount: 90 },
+    // 	{ Country: "04:00 PM", Amount: 75 },
+    // 	{ Country: "08:00 PM", Amount: 110 },
+    // 	{ Country: "12:00 PM", Amount: 80 },
+    // 	{ Country: "04:00 AM", Amount: 130 }
+    // ];
+    constructor() { }
+
+    ngOnInit() {
+        // alert('monitor');
+    }
 
 }
---
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-action-bar/patient-action-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
+
+@Component({
+	moduleId: module.id,
+	selector: 'patient-action-bar',
+	templateUrl: './patient-action-bar.component.html',
+	styleUrls: ['./patient-action-bar.component.css']
+})
+
+export class PatientActionBarComponent implements OnInit {
+	//  process variable 
+	patientname: string;
+
+	constructor(private routerExtensions: RouterExtensions) {
+
+	}
+	@Input() patientName: string;
+	ngOnInit() {
+		this.patientname = this.patientName;
+	}
+
+	goBackPage() {
+		this.routerExtensions.navigate(["/home"], { clearHistory: true });
+	}
+	
+	patientdetail(){
+		this.routerExtensions.navigate(['patientmgnt', 'patient'], { clearHistory: true });
+	}
+}
