e5ba014ed09232184d2d9b0c25d2a6ebb0c85499
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-07 14:01:12 +0000

    Description: 1. added validations - monitor,medicine,intake forms 2. moved models to ui model Reviewed By: Mandar Change Log: Bug ID:

--- technical/src/apps/user/patientcare/src/app/modules/patient-management/charts/intake-chart/intake-chart.component.ts
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/charts/intake-chart/intake-chart.component.ts
@@ -2,137 +2,178 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterExtensions } from "nativescript-angular/router";
 import { dateProperty } from 'tns-core-modules/ui/date-picker/date-picker';
 import { SegmentedBar, SegmentedBarItem } from "tns-core-modules/ui/segmented-bar";
-import {DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Switch } from "tns-core-modules/ui/switch";
-import  { ChartModel } from "~/app/models/ui/chart-models";
+import { ChartDBModel, IntakeChartModel } from "~/app/models/ui/chart-models";
 import { ChartService } from "~/app/services/chart/chart.service";
-
-
-export class NewDataItem{
-    public intakeName:string;
-    public quantity: string;
-    public frequency:number;
-    public duration:number;
-    public startDate:Date;
-    public intervalHrs: number;
-    public startTime:Date;
-    public specificTime:Date;
-    public specificTimes:Array<any>;
-    public desc:string="";
-}
-
-export class ChartIntakeConfModel{
-    public name:string;
-    public quantity: string;
-    public frequency:number;
-    public duration:number;
-    public startDate:string;
-    public intervalHrs: number;
-    public startTime:string;
-    public specificTime:string;
-    public specificTimes:Array<any>;    
-    public desc:string;
-}
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-	moduleId: module.id,
-	selector: 'intake-chart',
-	templateUrl: './intake-chart.component.html',
-	styleUrls: ['./intake-chart.component.css']
+    moduleId: module.id,
+    selector: 'intake-chart',
+    templateUrl: './intake-chart.component.html',
+    styleUrls: ['./intake-chart.component.css']
 })
 
 export class IntakeChartComponent implements OnInit {
-    
-    formData : NewDataItem;
-    chartConfModel : ChartIntakeConfModel;
-    chartDbModel : ChartModel
 
-    public frequencyItems: Array<SegmentedBarItem>;
-    public freqSelectedIndex = 0;
+    // proccess variables
+    intakeForm: FormGroup;
 
-    constructor(private routerExtensions: RouterExtensions, private datePipe: DatePipe,private chartservice: ChartService){
+    intakeNameIsValid:boolean;
+    quantityIsValid:boolean;
+    intervalHrsIsValid: boolean;
+    durationIsValid: boolean;
 
-        this.formData = new NewDataItem();
+    specifictimes: Array<string>;
+
+    formData: IntakeChartModel;
+    chartConfModel: IntakeChartModel;
+    chartDbModel: ChartDBModel
+
+    frequencyItems: Array<SegmentedBarItem>;
+    freqSelectedIndex = 0;
+    // end of proccess variables
+
+    constructor(private routerExtensions: RouterExtensions, private datePipe: DatePipe, private chartservice: ChartService) {
+
+        this.formData = new IntakeChartModel();
         this.formData.specificTimes = [];
-        this.chartConfModel = new ChartIntakeConfModel();
+        this.specifictimes = [];
+        this.chartConfModel = new IntakeChartModel();
         this.chartConfModel.specificTimes = [];
-        this.chartDbModel = new ChartModel();
+        this.chartDbModel = new ChartDBModel();
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
+
+        // creating form control
+        this.createFormControls();
+
         this.frequencyItems = [];
         const freqItem1 = new SegmentedBarItem();
         freqItem1.title = "Every 'X' hours";
         this.frequencyItems.push(freqItem1);
-        const freqItem2= new SegmentedBarItem();
+        const freqItem2 = new SegmentedBarItem();
         freqItem2.title = "Specific time";
         this.frequencyItems.push(freqItem2);
-        
+
     }
 
+    // << func for navigating previous page
     goBackPage() {
-		this.routerExtensions.navigate(['patientmgnt', 'details'], { clearHistory: true });
+        this.routerExtensions.navigate(['patientmgnt', 'details'], { clearHistory: true });
     }
+    // >> func for navigating previous page
 
     onPageLoaded(args) {
         console.log("intake form page loaded");
     }
 
-    public onFrequencySelectedIndexChange(args) {
+    onFrequencySelectedIndexChange(args) {
         let segmetedBar = <SegmentedBar>args.object;
         this.freqSelectedIndex = segmetedBar.selectedIndex;
+
+        if (this.freqSelectedIndex == 0) {
+            this.intakeForm.controls['intervalHrs'].setValidators([Validators.required]);
+            this.intakeForm.controls['intervalHrs'].updateValueAndValidity();
+        } else {
+            this.intakeForm.controls['intervalHrs'].clearValidators();
+            this.intakeForm.controls['intervalHrs'].updateValueAndValidity();
+            this.intervalHrsIsValid = false;
+        }
+
     }
 
-    public onSubmit(){
+    // << func for submit form data
+    onSubmit() {
 
+        this.intakeNameIsValid = this.intakeForm.controls['name'].hasError('required');
+        this.quantityIsValid = this.intakeForm.controls['quantity'].hasError('required');
+        this.intervalHrsIsValid = this.intakeForm.controls['intervalHrs'].hasError('required');
+        this.durationIsValid = this.intakeForm.controls['duration'].hasError('required');
+
+        if (this.intakeForm.invalid) {
+            console.log("validation error");
+            return;
+        }
+
+        // assign form data to model
+        this.formData = Object.assign({}, this.intakeForm.value);
+        this.formData.specificTimes = this.specifictimes;
+
+        // insert form data to sqlite db
         this.insertData(this.formData);
 
     }
+    // >> func for submit form data
 
-    public insertData(data: NewDataItem){
+    // << func for inserting form data to sqlite db
+    insertData(data: IntakeChartModel) {
 
-        if (data.frequency==0){
+        //set chart conf model
+        if (data.frequency == 0) {
             this.chartConfModel.intervalHrs = data.intervalHrs;
-            this.chartConfModel.startTime = this.datePipe.transform(data.startTime,"h:mm a");
+            this.chartConfModel.startTime = this.datePipe.transform(data.startTime, "h:mm a");
         }
-        if (data.frequency==1){
-
-            for(var i = 0;i<data.specificTimes.length;i++){
-                this.chartConfModel.specificTimes.push(this.datePipe.transform(data.specificTimes[i],"h:mm a"));
+        if (data.frequency == 1) {
+            for (var i = 0; i < data.specificTimes.length; i++) {
+                this.chartConfModel.specificTimes.push(this.datePipe.transform(data.specificTimes[i], "h:mm a"));
             }
         }
 
-        this.chartConfModel.name = data.intakeName;
+        this.chartConfModel.name = data.name;
         this.chartConfModel.quantity = data.quantity;
         this.chartConfModel.frequency = data.frequency;
         this.chartConfModel.duration = data.duration;
-        this.chartConfModel.startDate = this.datePipe.transform(data.startDate,"yyyy-MM-dd");
+        this.chartConfModel.startDate = this.datePipe.transform(data.startDate, "yyyy-MM-dd");
 
-        if(data.desc!=""){
+        if (data.desc != null) {
             this.chartConfModel.desc = data.quantity + "\n" + data.desc;
-        }else{
+        } else {
             this.chartConfModel.desc = data.quantity
         }
 
-        
+
         let confString = JSON.stringify(this.chartConfModel);
 
-        this.chartDbModel.admissionid = 2 ;
+        // set db model
+        this.chartDbModel.admissionid = 2;
         this.chartDbModel.conf = confString;
         this.chartDbModel.conf_type_code = "Intake";
 
+        // insert chart db model to sqlite db
         this.chartservice.insertChartItem(this.chartDbModel);
 
+        // get chart data from sqlite db
         this.chartservice.getChartList()
 
         this.goBackPage();
 
     }
+    // >> func for inserting form data to sqlite db
 
-    public addSpecificTime(){
-        this.formData.specificTimes.push(this.formData.specificTime);
+    // << func for specific timings
+    addSpecificTime() {
+        this.specifictimes.push(this.intakeForm.controls['specificTime'].value);
     }
+    // >> func for specific timings
+
+    // << func for creating form controls
+    createFormControls(): void {
+        this.intakeForm = new FormGroup({
+            name: new FormControl('', [Validators.required]),
+            quantity: new FormControl('', [Validators.required]),
+            frequency: new FormControl(),
+            duration: new FormControl('', [Validators.required]),
+            startDate: new FormControl(),
+            intervalHrs: new FormControl(),
+            startTime: new FormControl(),
+            specificTime: new FormControl(),
+            desc: new FormControl()
+        });
+    }
+    // >> func for creating form controls
 
 }
--- technical/src/apps/user/patientcare/src/app/modules/patient-management/charts/monitor-chart/monitor-chart.component.ts
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/charts/monitor-chart/monitor-chart.component.ts
@@ -2,200 +2,245 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterExtensions } from "nativescript-angular/router";
 import { dateProperty } from 'tns-core-modules/ui/date-picker/date-picker';
 import { SegmentedBar, SegmentedBarItem } from "tns-core-modules/ui/segmented-bar";
-import {DatePipe} from '@angular/common';
-import  { ChartModel } from "~/app/models/ui/chart-models";
+import { DatePipe } from '@angular/common';
+import { ChartDBModel, MonitorChartModel } from "~/app/models/ui/chart-models";
 import { ChartService } from "~/app/services/chart/chart.service";
 import { Observable } from 'tns-core-modules/ui/page/page';
 import { ObservableArray } from 'tns-core-modules/data/observable-array/observable-array';
-import { ConfListViewModel, ConfModel } from '~/app/models/ui/conf-models';
+import { ConfListViewModel } from '~/app/models/ui/conf-models';
 import { ConfService } from '~/app/services/conf/conf.service';
 import { ListPicker } from "tns-core-modules/ui/list-picker";
-
-
-export class NewDataItem{
-    public monitorName:string;
-    public instruction:number;
-    public frequency:number;
-    public duration:number;
-    public startDate:Date;
-    public intervalHrs: number;
-    public startTime:Date;
-    public endTime:Date;
-    public specificTime:Date;
-    public specificTimes:Array<any>;
-    public desc:string="";
-}
-
-export class ChartMonitorConfModel{
-    public name:string;
-    public foodInst:number;
-    public frequency:number;
-    public duration:number;
-    public startDate:string;
-    public intervalHrs: number;
-    public startTime:string;
-    public endTime:string;
-    public specificTimes:Array<any>;
-    public desc:string;
-}
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-	moduleId: module.id,
-	selector: 'monitor-chart',
-	templateUrl: './monitor-chart.component.html',
-	styleUrls: ['./monitor-chart.component.css']
+    moduleId: module.id,
+    selector: 'monitor-chart',
+    templateUrl: './monitor-chart.component.html',
+    styleUrls: ['./monitor-chart.component.css']
 })
 
 export class MonitorChartComponent implements OnInit {
-    
-    formData : NewDataItem;
-    chartConfModel : ChartMonitorConfModel;
-    chartDbModel : ChartModel
 
-    public myItems: Array<SegmentedBarItem>;
-    public frequencyItems: Array<SegmentedBarItem>;
-    public instSelectedIndex = 0;
-    public freqSelectedIndex = 0;
+    // proccess variables
+    monitorForm: FormGroup;
 
-    private monitorConfListItems = new ObservableArray<ConfModel>();
-    public monitorConf: ConfModel;
+    intervalHrsIsValid: boolean;
+    durationIsValid: boolean;
 
-    constructor(private routerExtensions: RouterExtensions, private datePipe: DatePipe,private chartservice: ChartService, private confService:ConfService){
+    monitorName: string;
+    specifictimes: Array<string>;
 
-        this.formData = new NewDataItem();
+    formData: MonitorChartModel;
+    chartConfModel: MonitorChartModel;
+    chartDbModel: ChartDBModel
+
+    foodInstItems: Array<SegmentedBarItem>;
+    frequencyItems: Array<SegmentedBarItem>;
+    foodInstSelectedIndex = 0;
+    freqSelectedIndex = 0;
+
+    monitorConfListItems = new ObservableArray<ConfListViewModel>();
+    monitorConf: ConfListViewModel;
+    // end of proccess variables
+
+    constructor(private routerExtensions: RouterExtensions, private datePipe: DatePipe, private chartservice: ChartService, private confService: ConfService) {
+
+        this.formData = new MonitorChartModel();
         this.formData.specificTimes = [];
-        this.chartConfModel = new ChartMonitorConfModel();
-        this.chartConfModel.specificTimes = [];
-        this.chartDbModel = new ChartModel();
+        this.specifictimes = [];
+        this.chartConfModel = new MonitorChartModel();
+        this.chartDbModel = new ChartDBModel();
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
 
+        // creating form control
+        this.createFormControls();
+
+        // get montior conf data for list picker
         this.getMonitorConfListData();
 
-        this.myItems = [];
+        this.foodInstItems = [];
         this.frequencyItems = [];
 
-        const item1 = new SegmentedBarItem();
-        item1.title = "Before Meal";
-        this.myItems.push(item1);
-        const item2 = new SegmentedBarItem();
-        item2.title = "After Meal";
-        this.myItems.push(item2);
+        // set food instruction segmented bar items
+        const foodInstItem1 = new SegmentedBarItem();
+        foodInstItem1.title = "Before Meal";
+        this.foodInstItems.push(foodInstItem1);
+        const foodInstItem2 = new SegmentedBarItem();
+        foodInstItem2.title = "After Meal";
+        this.foodInstItems.push(foodInstItem2);
 
+        // set frequency segmented bar items
         const freqItem1 = new SegmentedBarItem();
         freqItem1.title = "Every 'X' hours";
         this.frequencyItems.push(freqItem1);
-        const freqItem2= new SegmentedBarItem();
+        const freqItem2 = new SegmentedBarItem();
         freqItem2.title = "Specific time";
         this.frequencyItems.push(freqItem2);
-        
+
+        // load default form data
+        // this.monitorForm.get('startDate').setValue(Date.now());
+
     }
 
+    // << func for navigating previous page
     goBackPage() {
-		this.routerExtensions.navigate(['patientmgnt', 'details'], { clearHistory: true });
+        this.routerExtensions.navigate(['patientmgnt', 'details'], { clearHistory: true });
     }
+    // >> func for navigating previous page
 
     onPageLoaded(args) {
         console.log("monitor form page loaded");
     }
 
-    public onInstructionSelectedIndexChange(args) {
+    onFoodInstSelectedIndexChange(args) {
         let segmetedBar = <SegmentedBar>args.object;
-        this.instSelectedIndex = segmetedBar.selectedIndex;
+        this.foodInstSelectedIndex = segmetedBar.selectedIndex;
     }
 
-    public onFrequencySelectedIndexChange(args) {
+    onFrequencySelectedIndexChange(args) {
         let segmetedBar = <SegmentedBar>args.object;
         this.freqSelectedIndex = segmetedBar.selectedIndex;
+
+        if (this.freqSelectedIndex == 0) {
+            this.monitorForm.controls['intervalHrs'].setValidators([Validators.required]);
+            this.monitorForm.controls['intervalHrs'].updateValueAndValidity();
+        } else {
+            this.monitorForm.controls['intervalHrs'].clearValidators();
+            this.monitorForm.controls['intervalHrs'].updateValueAndValidity();
+            this.intervalHrsIsValid = false;
+        }
+
     }
 
-    public onSubmit(){
+    // << func for submit form data
+    onSubmit() {
 
+        this.intervalHrsIsValid = this.monitorForm.controls['intervalHrs'].hasError('required');
+        this.durationIsValid = this.monitorForm.controls['duration'].hasError('required');
+
+        if (this.monitorForm.invalid) {
+            console.log("validation error");
+            return;
+        }
+
+        // assign form data to model
+        this.formData = Object.assign({}, this.monitorForm.value);
+        this.formData.name = this.monitorName;
+        this.formData.specificTimes = this.specifictimes;
+
+        // insert form data to sqlite db
         this.insertData(this.formData);
 
     }
+    // >> func for submit form data
 
-    public insertData(data: NewDataItem){
+    // << func for inserting form data to sqlite db
+    insertData(data: MonitorChartModel) {
 
-        if (data.frequency==0){
+        //set chart conf model
+        if (data.frequency == 0) {
             this.chartConfModel.intervalHrs = data.intervalHrs;
-            this.chartConfModel.startTime = this.datePipe.transform(data.startTime,"h:mm a");
-            this.chartConfModel.endTime = this.datePipe.transform(data.endTime,"h:mm a");
-            
-        if (data.desc!=""){
-            this.chartConfModel.desc = "Monitor every " + data.intervalHrs + " hours.\n" + data.desc ;
-        }else{
-            this.chartConfModel.desc = "Monitor every " + data.intervalHrs + " hours.";
-        }
-        }
 
-        if (data.frequency==1){
-            for(var i = 0;i<data.specificTimes.length;i++){
-                this.chartConfModel.specificTimes.push(this.datePipe.transform(data.specificTimes[i],"h:mm a"));
-            }
-            if (data.desc!=""){
-                this.chartConfModel.desc = "Monitor as per specific timings " + ".\n" + data.desc ;
-            }else{
-                this.chartConfModel.desc = "Monitor as per specific timings ";
+            this.chartConfModel.startTime = this.datePipe.transform(data.startTime, "h:mm a");
+            this.chartConfModel.endTime = this.datePipe.transform(data.endTime, "h:mm a");
+
+            if (data.desc != null) {
+                this.chartConfModel.desc = "Monitor every " + data.intervalHrs + " hours.\n" + data.desc;
+            } else {
+                this.chartConfModel.desc = "Monitor every " + data.intervalHrs + " hours.";
             }
         }
 
-        this.chartConfModel.name = data.monitorName;
+        if (data.frequency == 1) {
+            this.chartConfModel.specificTimes = [];
+            for (var i = 0; i < data.specificTimes.length; i++) {
+                this.chartConfModel.specificTimes.push(this.datePipe.transform(data.specificTimes[i], "h:mm a"));
+            }
+            if (data.desc !=null) {
+                this.chartConfModel.desc = "Monitor as per specific timings." + "\n" + data.desc;
+            } else {
+                this.chartConfModel.desc = "Monitor as per specific timings.";
+            }
+        }
+
+        this.chartConfModel.name = data.name;
         this.chartConfModel.frequency = data.frequency;
         this.chartConfModel.duration = data.duration;
-        this.chartConfModel.startDate = this.datePipe.transform(data.startDate,"yyyy-MM-dd");
-        this.chartConfModel.foodInst = data.instruction;       
-        
+        this.chartConfModel.startDate = this.datePipe.transform(data.startDate, "yyyy-MM-dd");
+        this.chartConfModel.foodInst = data.foodInst;
+
         let confString = JSON.stringify(this.chartConfModel);
 
-        this.chartDbModel.admissionid = 2 ;
+        // set db model
+        this.chartDbModel.admissionid = 2;
         this.chartDbModel.conf = confString;
         this.chartDbModel.conf_type_code = "Monitor";
 
+        // insert chart db model to sqlite db
         this.chartservice.insertChartItem(this.chartDbModel);
 
+        // get chart data from sqlite db
         this.chartservice.getChartList()
 
         this.goBackPage();
-
+        
     }
+    // >> func for inserting form data to sqlite db
 
-    public addSpecificTime(){
-        this.formData.specificTimes.push(this.formData.specificTime);
+    // << func for specific timings
+    addSpecificTime() {
+        this.specifictimes.push(this.monitorForm.controls['specificTime'].value);
     }
+    // >> func for specific timings
 
-    public getMonitorConfListData(){
-		this.confService.getMonitorConf().then(
-			(val)=>{
-				val.forEach(item => {
-					let monitorConfListItem = new ConfModel();
-                    monitorConfListItem = item;
-                    monitorConfListItem.conf =  JSON.parse(item.conf);
-					this.monitorConfListItems.push(monitorConfListItem);
+    // << func for getting monitor conf data
+    getMonitorConfListData() {
+        this.confService.getMonitorConf().then(
+            (val) => {
+                val.forEach(item => {
+                    let monitorConfListItem = new ConfListViewModel();
+                    monitorConfListItem.dbmodel = item;
+                    monitorConfListItem.dbmodel.conf = JSON.parse(item.conf);
+                    this.monitorConfListItems.push(monitorConfListItem);
                 });
                 this.monitorConf = this.monitorConfListItems.getItem(0);
-			},
-			(error)=>{
-				console.log("confListService error:",error);
-			}
+            },
+            (error) => {
+                console.log("confListService error:", error);
+            }
 
-		);
+        );
 
     }
-    
-    public selectedIndexChanged(args) {        
+    // >> func for getting monitor list data
+
+    // << func for selecting monitor name
+    selectedIndexChanged(args) {
         let picker = <ListPicker>args.object;
-        let picked :any;
-
-        // console.log("picker selection: " + picker.selectedIndex);
-        // console.log("picker selection value: " + (<any>picker).selectedValue);
-
-        picked = this.monitorConf.conf.tasks[picker.selectedIndex];
-        this.formData.monitorName = picked.name;
-
+        let picked: any;
+        picked = this.monitorConf.dbmodel.conf.tasks[picker.selectedIndex];
+        this.formData.name = picked.name;
+        this.monitorName = picked.name;
     }
+    //  >> func for selecting monitor name
+
+    // << func for creating form controls
+    createFormControls(): void {
+        this.monitorForm = new FormGroup({
+            foodInst: new FormControl(),
+            frequency: new FormControl(),
+            duration: new FormControl('', [Validators.required]),
+            startDate: new FormControl(),
+            intervalHrs: new FormControl('', [Validators.required]),
+            startTime: new FormControl(),
+            endTime: new FormControl(),
+            specificTime: new FormControl(),
+            desc: new FormControl()
+        });
+    }
+    // >> func for creating form controls
 
 }
