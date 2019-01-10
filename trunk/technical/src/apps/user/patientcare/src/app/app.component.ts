620e5a872dffc4c6bcfae24341d57bd8884147de
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-10 08:34:44 +0000

    Comitted web socket inside worker.

--- trunk/technical/src/apps/user/patientcare/src/app/app.component.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, OnDestroy, NgZone } from "@angular/core";
 import { DatabaseSchemaService } from "./services/offline-store/database-schema.service";
 import { server } from './environments/environment';
 import { WorkerService } from "./services/worker.service";
+import { InternetConnectionService } from "./services/internet-status/internet-connection.service";
+import { Subscription } from "rxjs";
+import { ServerDataProcessorMessageModel } from "./models/api/server-data-processor-message-model";
+import { SERVER_WORKER_MSG_TYPE } from "~/app/app-constants";
 var WS = require('nativescript-websockets');
 
 @Component({
@@ -14,62 +18,76 @@ export class AppComponent implements OnInit, OnDestroy {
     private socket: any;
     public messages: Array<any>;
     public chatBox: string;
+    private internetConnectionSubscription:Subscription;
     constructor(private databaseSchemaService: DatabaseSchemaService,
         private zone: NgZone,
-    private workerService:WorkerService) {
-        this.databaseSchemaService.setOfflineDB();
-        console.log('server', server);
-        this.socket = new WS("ws://echo.websocket.org", []);
-        console.log('socket created', this.socket);
+    private workerService:WorkerService, private internetConnectionService:InternetConnectionService) {
+       // this.databaseSchemaService.setOfflineDB();
+        // console.log('server', server);
+        // this.socket = new WS("ws://echo.websocket.org", []);
+        // console.log('socket created', this.socket);
         this.messages = [];
         this.chatBox = "";
+
+        // Initialize the worker here
+       this.workerService.initServerDataProcessorWorker();  
+
+        // this.internetConnectionSubscription=   this.internetConnectionService.connectionStatus$.subscribe(status=>{
+        //  if(status)
+        //  {
+        //     // TODO if the status is connected, open webscocket connection again
+        //  }
+        // });
     }
 
     ngOnInit() {
 
-        // Initialize the worker here
-        this.workerService.initServerDataProcessorWorker();  
-        
         // TODO: Dummy code for testing 
+        console.log('in app component init');
         this.workerService.ServerDataProcessorWorker.onmessage=m=>this.workerOnMessage(m);
-        this.workerService.ServerDataProcessorWorker.postMessage('message posted to worker');
+         const initModel= new ServerDataProcessorMessageModel();
+         initModel.msgtype=SERVER_WORKER_MSG_TYPE.INIT_SERVER_INTERFACE;
+        this.workerService.ServerDataProcessorWorker.postMessage(initModel);
         
         // console.log('socketIO', this.socketIO);
         // this.socketIO.connect();
-        this.socket.on('open', socket => {
-            this.zone.run(() => {
-                this.messages.push("Welcome to the chat!");
-                console.log('messages', this.messages);
-                this.chatBox = "test message";
-                this.send();
-            });
-        });
-        this.socket.on('message', (socket, message) => {
-            this.zone.run(() => {
-                console.log("on message", message);
-                this.messages.push(message);
-                console.log('messages', this.messages);
-            });
-        });
+        // this.socket.on('open', socket => {
+        //     this.zone.run(() => {
+        //         this.messages.push("Welcome to the chat!");
+        //         console.log('messages', this.messages);
+        //         this.chatBox = "test message";
+        //         this.send();
+        //     });
+        // });
+        // this.socket.on('message', (socket, message) => {
+        //     this.zone.run(() => {
+        //         console.log("on message", message);
+        //         this.messages.push(message);
+        //         console.log('messages', this.messages);
+        //     });
+        // });
         //this.socket.on('message', function (socket, message) { console.log("Got a message", message); });
 
-        this.socket.on('close', (socket, code, reason) => {
-            this.zone.run(() => {
-                this.messages.push({ content: "You have been disconnected" });
-                console.log('messages', this.messages);
-            });
-        });
-        this.socket.on('error', (socket, error) => {
-            console.log("The socket had an error", error);
-        });
+        // this.socket.on('close', (socket, code, reason) => {
+        //     this.zone.run(() => {
+        //         this.messages.push({ content: "You have been disconnected" });
+        //         console.log('messages', this.messages);
+        //     });
+        // });
+        // this.socket.on('error', (socket, error) => {
+        //     console.log("The socket had an error", error);
+        // });
 
-        this.socket.open();
+        // this.socket.open();
     }
 
     ngOnDestroy() {
         //   this.socketIO.disconnect();
-        this.socket.close();
-        this.workerService.ServerDataProcessorWorker.terminate();
+        // this.socket.close();
+        // TODO: Send command to worker to disconnect the websocket before terminating server
+         this.workerService.ServerDataProcessorWorker.terminate();
+
+         this.internetConnectionSubscription.unsubscribe();
     }
 
     public send() {
