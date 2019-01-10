2e9fbeeb3147dfb65a7fbfce801a0afb1785f53a
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-10 08:34:44 +0000

    Comitted web socket inside worker.

--- technical/src/apps/user/patientcare/references.d.ts
+++ technical/src/apps/user/patientcare/references.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
 /// <reference path="./typings/custom.d.ts" /> Workerloader
+/// <reference path="./src/app/app-constants.ts" /> AppConstants
--- technical/src/apps/user/patientcare/src/app/workers/server-data-processor.worker.ts
+++ technical/src/apps/user/patientcare/src/app/workers/server-data-processor.worker.ts
@@ -1,11 +1,67 @@
+ import { ServerDataProcessorMessageModel } from "~/app/models/api/server-data-processor-message-model";
+ import { SERVER_WORKER_MSG_TYPE } from "~/app/app-constants.js";
+ const timerModule = require("tns-core-modules/timer");
+ //require('~/app/app-constants')
+
 require('globals');
+var WS = require('nativescript-websockets');
 
 const context: Worker = self as any;
 
 context.onmessage = msg => {
-    setTimeout(() => {
+    // setTimeout(() => {
         console.log("Inside TS worker...");
-        console.log(msg);
-        (<any>global).postMessage("TS Worker");
-    }, 500)
+        WorkerTasks.processMessage(msg.data);                                    
+    // }, 500)    
+
 };
+
+export class WorkerTasks
+{
+    private static socket: any;
+    public static processMessage(msg:any)
+    {
+        
+        console.log(msg);
+        switch(msg.msgtype)
+        {
+            case SERVER_WORKER_MSG_TYPE.INIT_SERVER_INTERFACE:
+            this.initWebSocket();
+            break;
+            case SERVER_WORKER_MSG_TYPE.SEND_MESSAGE:
+            this.socket.send();
+            break;
+        }
+       // this.initWebSocket();
+        //(<any>global).postMessage("TS Worker");        
+    }
+
+    private static initWebSocket()
+    {
+        this.socket = new WS("ws://172.105.232.148:8090/ws", []);
+        console.log('socket created', this.socket);
+
+        this.socket.on('open', socket => {
+                console.log('messages', "WebSocket opened");
+                this.socket.send("test message");          
+        });
+        this.socket.on('message', (socket, message) => {
+                console.log("websocket message recieved", message);            
+        });
+        
+        this.socket.on('close', (socket, code, reason) => {
+               console.log('messages', "Websocket disconnected");            
+        });
+        this.socket.on('error', (socket, error) => {
+            console.log("The socket had an error", error);
+        });
+
+        this.socket.open();
+
+       const timer = timerModule.setInterval(() => {
+            const randNumber = Math.floor(Math.random());
+            this.socket.send("message"+randNumber);
+        }, 10000);
+    }
+}
+
