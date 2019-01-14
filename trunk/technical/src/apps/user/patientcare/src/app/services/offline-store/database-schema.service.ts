ab8a8cab5429b35891ca3ede428cedceb44ad9bd
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-14 09:39:07 +0000

    Description: updated db Reviewed By: Mandar Change Log: Bug ID:

--- trunk/technical/src/apps/user/patientcare/src/app/services/offline-store/database-schema.service.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/services/offline-store/database-schema.service.ts
@@ -87,9 +87,9 @@ export class DatabaseSchemaService {
         `INSERT INTO patient_conf_tbl (id , conf_type_code , conf , created_on , updated_on ) 
             VALUES (1, 'Monitor' ,'{"tasks":[{"name":"Temperature"},{"name":"Blood Pressure"},{"name":"Pulse Rate"},{"name":"Respiration Rate"}]}' , '2018-12-04 14:37:53', '2018-12-04 14:37:53')`,
 
-        `INSERT INTO service_point_tbl (sp_id,spc_id,sp_name,short_desc,sp_state,sp_state_since,creadted_on,update_on ) values (1,1,"General Ward 1",1,'2018-12-04 14:37:53','2018-12-04 14:37:53','2018-12-04 14:37:53')`,
-        `INSERT INTO service_point_tbl (sp_id,spc_id,sp_name,short_desc,sp_state,sp_state_since,creadted_on,update_on ) values (2,1,"General Ward 2",1,'2018-12-04 14:37:53','2018-12-04 14:37:53','2018-12-04 14:37:53')`,
-        `INSERT INTO service_point_tbl (sp_id,spc_id,sp_name,short_desc,sp_state,sp_state_since,creadted_on,update_on ) values (3,1,"General Ward 3",1,'2018-12-04 14:37:53','2018-12-04 14:37:53','2018-12-04 14:37:53')`,
+        `INSERT INTO service_point_tbl (sp_id,spc_id,sp_name,short_desc,sp_state,sp_state_since,created_on,updated_on ) values (1,1,"General Ward 1","",1,'2018-12-04 14:37:53','2018-12-04 14:37:53','2018-12-04 14:37:53')`,
+        `INSERT INTO service_point_tbl (sp_id,spc_id,sp_name,short_desc,sp_state,sp_state_since,created_on,updated_on ) values (2,1,"General Ward 2","",1,'2018-12-04 14:37:53','2018-12-04 14:37:53','2018-12-04 14:37:53')`,
+        `INSERT INTO service_point_tbl (sp_id,spc_id,sp_name,short_desc,sp_state,sp_state_since,created_on,updated_on ) values (3,1,"General Ward 3","",1,'2018-12-04 14:37:53','2018-12-04 14:37:53','2018-12-04 14:37:53')`,
     ]
     
 
