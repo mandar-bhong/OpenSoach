ec0c72220f4704bb2b8f5e08831e5ea14167747b
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-15 12:10:06 +0000

    Added platform helper.

---
+++ technical/src/apps/user/patientcare/src/app/helpers/platform-helper-api.ts
@@ -0,0 +1,4 @@
+export interface PlatformHelperAPI
+{
+    getRandomUUID():string;
+}
---
+++ technical/src/apps/user/patientcare/src/app/helpers/platform-ios-helper.ts
@@ -0,0 +1,7 @@
+import { PlatformHelperAPI } from "./platform-helper-api";
+
+export class PlatformiOSHelper implements PlatformHelperAPI {
+    getRandomUUID(): string {
+        return "";
+    }
+}
