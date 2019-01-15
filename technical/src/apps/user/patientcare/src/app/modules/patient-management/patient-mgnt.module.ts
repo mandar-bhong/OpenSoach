e55316eb6bc6d872c78e0a3db10defaed17ed268
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-15 05:36:04 +0000

    Description :  AutoComplete module import Bug ID: NA Reviewed By: Sanjay

--- technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-mgnt.module.ts
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-mgnt.module.ts
@@ -6,33 +6,35 @@ import { DetailsComponent } from "./details/details.component";
 import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
 import { NetworkStatusComponent } from "~/app/network-status.component";
 import { FloatBtnComponent } from "~/app/float-btn.component";
-import { PatientMgntRoutingModule } from "~/app/modules/patient-management/patient-mgnt-routing.module";
-import { PatientMgntComponent } from "~/app/modules/patient-management/patient-mgnt.component";
-import { CamerasComponent } from "~/app/modules/patient-management/cameras/cameras.component";
-import { PatientDetailsComponent } from "~/app/modules/patient-management/patient-details/patient-details.component";
-import { MonitorComponent } from "~/app/modules/patient-management/monitor/monitor.component";
-import { ActionComponent } from "~/app/modules/patient-management/action/action.component";
-import { ChartsComponent } from "~/app/modules/patient-management/charts/charts.component";
-import { MedicineChartComponent } from "~/app/modules/patient-management/charts/medicine-chart/medicine-chart.component";
+import { PatientMgntRoutingModule } from "./patient-mgnt-routing.module";
+import { PatientMgntComponent } from "./patient-mgnt.component";
+import { CamerasComponent } from "./cameras/cameras.component";
+import { PatientDetailsComponent } from "./patient-details/patient-details.component";
+import { MonitorComponent } from "./monitor/monitor.component";
+import { ActionComponent } from "./action/action.component";
+import { ChartsComponent } from "./charts/charts.component";
+import { MedicineChartComponent } from "./charts/medicine-chart/medicine-chart.component";
 import { NativeScriptUIChartModule } from "nativescript-ui-chart/angular";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { DatePipe } from "@angular/common";
 import { ChartService } from "~/app/services/chart/chart.service";
-import { IntakeChartComponent } from "~/app/modules/patient-management/charts/intake-chart/intake-chart.component";
-import { MonitorChartComponent } from "~/app/modules/patient-management/charts/monitor-chart/monitor-chart.component";
-import { MedicalDetailsComponent } from "~/app/modules/patient-management/patient-details/medical-details/medical-details.component";
-import { PatientInfoComponent } from "~/app/modules/patient-management/patient-details/patient-info/patient-info.component";
+import { IntakeChartComponent } from "./charts/intake-chart/intake-chart.component";
+import { MonitorChartComponent } from "./charts/monitor-chart/monitor-chart.component";
+import { MedicalDetailsComponent } from "./patient-details/medical-details/medical-details.component";
+import { PatientInfoComponent } from "./patient-details/patient-info/patient-info.component";
 import { ConfService } from "~/app/services/conf/conf.service";
-import { ReportsComponent } from "~/app/modules/patient-management/reports/reports.component";
-import { SectionOneComponent } from "~/app/modules/patient-management/reports/section-one/section-one.component";
+import { ReportsComponent } from "./reports/reports.component";
+import { SectionOneComponent } from "./reports/section-one/section-one.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { sharedModule } from "~/app/modules/shared-mudule";
-import { PatientActionBarComponent } from "~/app/modules/patient-management/patient-action-bar/patient-action-bar.component";
+import { PatientActionBarComponent } from "./patient-action-bar/patient-action-bar.component";
 import { Action } from "rxjs/internal/scheduler/Action";
 import { ActionService } from "~/app/services/action/action.service";
-import { ShowCameraImageComponent } from "~/app/modules/patient-management/reports/section-one/show-camera-image/show-camera-image.component";
-import { ShowUploadedImageComponent } from "~/app/modules/patient-management/reports/section-one/show-uploaded-image/show-uploaded-image.component";
+import { ShowCameraImageComponent } from "./reports/section-one/show-camera-image/show-camera-image.component";
+import { ShowUploadedImageComponent } from "./reports/section-one/show-uploaded-image/show-uploaded-image.component";
 import { GetUUIDService } from "~/app/services/get-UUID.service";
+import { NativeScriptUIAutoCompleteTextViewModule } from "nativescript-ui-autocomplete/angular"; 
+
 
 @NgModule({
     imports: [
@@ -44,7 +46,8 @@ import { GetUUIDService } from "~/app/services/get-UUID.service";
         NativeScriptFormsModule,
         ReactiveFormsModule,
         sharedModule,
-        NativeScriptFormsModule
+        NativeScriptFormsModule,
+        NativeScriptUIAutoCompleteTextViewModule
     ],
     declarations: [
         PatientMgntComponent,
