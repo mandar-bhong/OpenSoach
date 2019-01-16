0d59b100c724ec729c1f6310498fcadeb24e4c92
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-16 07:03:36 +0000

    Posted a dummy message from the worker and triggered the subject on the web service.

--- technical/src/apps/user/patientcare/src/app/services/worker.service.ts
+++ technical/src/apps/user/patientcare/src/app/services/worker.service.ts
@@ -2,21 +2,36 @@ import { Injectable } from "@angular/core";
 
 // add if building with webpack
 import * as ServerDataProcessorWorker from "nativescript-worker-loader!../workers/server-data-processor.worker";
+import { Subject } from "rxjs";
+import { SERVER_WORKER_EVENT_MSG_TYPE } from "~/app/app-constants";
 
 @Injectable()
 export class WorkerService {
     public ServerDataProcessorWorker: Worker;
+    public ServerNotificationSubject = new Subject<any>();
     constructor() {
     }
 
-    initServerDataProcessorWorker()
-    {        
+    initServerDataProcessorWorker() {
         if (global["TNS_WEBPACK"]) {
             console.log('build with web pack');
-            this.ServerDataProcessorWorker=new ServerDataProcessorWorker();
+            this.ServerDataProcessorWorker = new ServerDataProcessorWorker();            
         } else {
             console.log('build without web pack');
             this.ServerDataProcessorWorker = new Worker("../workers/server-data-processor.worker");
         }
+
+        this.ServerDataProcessorWorker.onmessage = m => this.serverWorkerMessageRecieved(m);
+    }
+
+    serverWorkerMessageRecieved(message: MessageEvent) {
+        console.log('worker message recieved', message);        
+        switch(message.data.msgtype)
+        {
+            case SERVER_WORKER_EVENT_MSG_TYPE.DATA_RECEIVED:
+            console.log('subject triggered');
+            this.ServerNotificationSubject.next(message.data);
+            break;
+        }        
     }
 }
