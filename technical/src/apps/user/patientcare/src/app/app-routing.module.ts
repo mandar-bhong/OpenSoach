f3c9ba33cb9fbfeed5154cbe1150f62f017c4cc6
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-16 07:01:17 +0000

    Changed the way routes are defined to support build with release mode.

--- technical/src/apps/user/patientcare/src/app/app-routing.module.ts
+++ technical/src/apps/user/patientcare/src/app/app-routing.module.ts
@@ -1,29 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { LoginModule } from "~/app/login/login.module";
-import { HomeModule } from "~/app/home/home.module";
-import { PatientMgntModule } from "~/app/modules/patient-management/patient-mgnt.module";
 
 const routes: Routes = [
     // { path: "", redirectTo: "/home", pathMatch: "full" },
     {
         path: "", redirectTo: "/login", pathMatch: "full"
     },
-    // {
-    //     path: "login", loadChildren: "~/app/login/login.module#LoginModule"
-    // },
-    // {
-    //     path: "home", loadChildren: "~/app/home/home.module#HomeModule"
-    // },
-    // {
-    //     path: "patientmgnt", loadChildren: "~/app/modules/patient-management/patient-mgnt.module#PatientMgntModule"
-    // },
-
-
-    {path:'login' ,loadChildren: ()=>LoginModule},
-    {path:'home' ,loadChildren: ()=>HomeModule},
-    {path:'patientmgnt' ,loadChildren: ()=>PatientMgntModule},
+    {
+        path: "login", loadChildren: "~/app/login/login.module#LoginModule"
+    },
+    {
+        path: "home", loadChildren: "~/app/home/home.module#HomeModule"
+    },
+    {
+        path: "patientmgnt", loadChildren: "~/app/modules/patient-management/patient-mgnt.module#PatientMgntModule"
+    },
 ];
 
 @NgModule({
