7f650010ed7747dd663ae8b82cfc74528af9a28b
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-17 09:03:05 +0000

    Description: added sync store constants,deleted conf service Change Log: Bug ID:

--- trunk/technical/src/apps/user/patientcare/src/app/app-constants.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/app-constants.ts
@@ -10,12 +10,35 @@ export enum SERVER_WORKER_EVENT_MSG_TYPE {
     NONE = 0,
     SERVER_CONNECTED = 1,
     SERVER_DISCONNECTED = 2,
-    DATA_RECEIVED=3,
-    NOTIFICATION_DATA_RECEIVED=4
+    DATA_RECEIVED = 3,
+    NOTIFICATION_DATA_RECEIVED = 4
 }
 
 export enum APP_MODE {
-    NONE=0,
-    SHARED_DEVICE=1,
-    USER_DEVICE=2
+    NONE = 0,
+    SHARED_DEVICE = 1,
+    USER_DEVICE = 2
+}
+
+export enum SYNC_STORE {
+    SERVICE_POINT = 'service_point_tbl',
+    CONF = 'conf_tbl',
+    PATIENT_MASTER = 'patient_master_tbl',
+    SCHEDULE = 'schedule_tbl',
+    PATIENT_ADMISSION = 'patient_admission_tbl',
+    PERSONAL_DETAILS = 'patient_personal_details_tbl',
+    MEDICAL_DETAILS = 'patient_medical_details_tbl',
+    ACTION_TXN = 'action_txn_tbl'
+}
+
+export enum SYNC_TYPE {
+    NONE = 0,
+    SYNC_FROM_SERVER = 1,
+    SYNC_TO_SERVER = 2,
+    SYNC_TO_AND_FROM_SERVER = 3
+}
+
+export enum SYNC_STATE {
+    IN_SYNC = 0,
+    NOT_IN_SYNC = 1
 }
