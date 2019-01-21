5ecd59db18a7d7b6141dfa887d46a558a02e2227
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-21 11:10:44 +0000

    Description: modified get sync - get by store name Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hpft/server/constants/dbconstants.go
+++ trunk/technical/src/appserver/src/opensoach.com/hpft/server/constants/dbconstants.go
@@ -5,5 +5,7 @@ const DB_SPL_HKT_SP_COMPLAINT_TBL = "spl_node_sp_complaint_tbl"
 const DB_SPL_NODE_FEEDBACK_TBL = "spl_node_feedback_tbl"
 const DB_SPL_NODE_DEV_STATUS_TBL = "spl_node_dev_status_tbl"
 
-const DB_PATIENT_MASTER_TBL_STORE_ID = 1
-const DB_PATIENT_ADMISSION_TBL_STORE_ID = 2
+const DB_PATIENT_MASTER_TBL_STORE_NAME = "patient_master_tbl"
+const DB_PATIENT_ADMISSION_TBL_STORE_NAME = "patient_admission_tbl"
+
+const DB_SP_TBL_STORE_NAME = "service_point_tbl"
