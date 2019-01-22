5629f11f74a9e65428261bc1aaf04f64e2027973
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-22 11:45:06 +0000

    Modified data storage models.

--- technical/src/apps/user/patientcare/src/app/models/api/server-data-processor-message-model.ts
+++ technical/src/apps/user/patientcare/src/app/models/api/server-data-processor-message-model.ts
@@ -2,5 +2,5 @@ import { SERVER_WORKER_MSG_TYPE } from "../../app-constants";
 
 export class ServerDataProcessorMessageModel {
 	public msgtype: SERVER_WORKER_MSG_TYPE;
-	public msg: any;
+	public data: any;
 }
