086b7535637335269611e63965f6c3fdafdea446
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-23 14:25:29 +0000

    Description: added server cmd models Change Log: Bug ID:

---
+++ trunk/technical/src/apps/user/patientcare/src/app/models/api/server-cmd-model.ts
@@ -0,0 +1,25 @@
+export class CmdModel {
+    header: Header;
+    payload: any;
+}
+
+export class Header {
+    crc: string;
+    category: number;
+    commandid: number;
+    seqid: number;
+}
+
+export class GetSyncRequestModel {
+    storename: string;
+    updatedon: string;
+}
+
+export class ApplySyncRequestModel {
+    storename: string;
+    storedata: any[];
+}
+
+export class tokenModel {
+    token: string;
+}
