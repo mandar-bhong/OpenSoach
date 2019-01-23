fdf1facd35556e301b43fe5e41145ca910f9ff63
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-23 13:25:01 +0000

    Description: postmessageFn specified msg type Change Log: Bug ID:

--- trunk/technical/src/apps/user/patientcare/src/app/workers/server-worker-context.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/workers/server-worker-context.ts
@@ -1,4 +1,7 @@
 export class ServerWorkerContext
 {
     public static ContextVar1 : string;
+    public static authToken:string;
+    public static deviceSerialNo: string;
+    public static serverUrl: string;
 }
