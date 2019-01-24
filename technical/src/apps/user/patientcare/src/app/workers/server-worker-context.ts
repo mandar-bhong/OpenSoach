2685e5ac401a09966c331b947ed585c3f1afdad1
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-24 08:35:35 +0000

    Added sync variables

--- technical/src/apps/user/patientcare/src/app/workers/server-worker-context.ts
+++ technical/src/apps/user/patientcare/src/app/workers/server-worker-context.ts
@@ -1,7 +1,23 @@
-export class ServerWorkerContext
-{
-    public static ContextVar1 : string;
-    public static authToken:string;
+export class ServerWorkerContext {
+    public static ContextVar1: string;
+    public static authToken: string;
     public static deviceSerialNo: string;
     public static serverUrl: string;
+
+    // SYNC Engine Context
+    public static isSyncInprogress: boolean;
+    public static syncType: SYNC_TYPE;
+    public static syncDirection: SYNC_DIRECTION;
+}
+
+export enum SYNC_TYPE {
+    NONE = 0,
+    FULL = 1,
+    DIFFERENTIAL = 2
+}
+
+export enum SYNC_DIRECTION {
+    NONE = 0,
+    TO_SERVER = 1,
+    FROM_SERVER = 2
 }
