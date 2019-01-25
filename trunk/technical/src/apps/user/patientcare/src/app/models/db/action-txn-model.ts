42fa6490920f43dd2276270635f268f9218c50f2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-25 08:34:12 +0000

    Description: added admission_uuid in action_txn tbl, modified changes sync_state to sync_pending Change Log: Bug ID:

--- trunk/technical/src/apps/user/patientcare/src/app/models/db/action-txn-model.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/models/db/action-txn-model.ts
@@ -3,6 +3,7 @@ import { IDatastoreModel } from "./idatastore-model";
 export class ActionTxnDatastoreModel implements IDatastoreModel {
     uuid: string;
     schedule_uuid: string;
+    admission_uuid:string;
     txn_data: string;
     txn_date: Date;
     txn_state: number;
@@ -11,7 +12,7 @@ export class ActionTxnDatastoreModel implements IDatastoreModel {
     updated_on: Date;
     sync_pending: number;
     getModelValues(): any[] {
-        return [this.uuid, this.schedule_uuid, this.txn_data, this.txn_date, this.txn_state, this.conf_type_code,
+        return [this.uuid, this.admission_uuid, this.schedule_uuid, this.txn_data, this.txn_date, this.txn_state, this.conf_type_code,
         this.runtime_config_data, this.updated_on, this.sync_pending];
     }
 }
