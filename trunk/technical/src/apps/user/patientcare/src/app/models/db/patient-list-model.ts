2714eae35944a21dcf8adff56287e1377f70abc6
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-25 07:03:32 +0000

    Description : Create patient list Binding model Bug ID: NA Reviewed By: Sanjay

---
+++ trunk/technical/src/apps/user/patientcare/src/app/models/db/patient-list-model.ts
@@ -0,0 +1,11 @@
+export class PatientListBindData{
+    fname: string;
+    lanme: string;
+    bed_no: string;
+    mob_no: string;
+    status: number;
+    attended: Date;
+    sp_uuid: string;
+    patient_uuid: string;
+    admission_uuid: string;
+}
