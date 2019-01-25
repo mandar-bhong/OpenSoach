ebd8db7e212f0abfd391367b125be8450ddd17ce
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-01-25 06:44:28 +0000

    Description : patient list master and admission handler Bug ID: NA Reviewed By: Sanjay

--- trunk/technical/src/apps/user/patientcare/src/app/services/worker.service.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/services/worker.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 // add if building with webpack
 import * as ServerDataProcessorWorker from "nativescript-worker-loader!../workers/server-data-processor.worker";
 import { Subject } from "rxjs";
-import { SERVER_WORKER_EVENT_MSG_TYPE } from "~/app/app-constants";
+import { SERVER_WORKER_EVENT_MSG_TYPE, SYNC_STORE } from "~/app/app-constants";
 import { ServerDataProcessorMessageModel } from "~/app/models/api/server-data-processor-message-model";
 import { ServerWorkerEventDataModel } from "../models/api/server-worker-event-data-model";
 import { ServerDataStoreDataModel } from "../models/api/server-data-store-data-model";
@@ -11,7 +11,10 @@ import { ServerDataStoreDataModel } from "../models/api/server-data-store-data-m
 @Injectable()
 export class WorkerService {
     private ServerDataProcessorWorker: Worker;
-    public DataReceivedSubject = new Subject<ServerDataStoreDataModel>();
+    //public DataReceivedSubject = new Subject<ServerDataStoreDataModel>();
+    public patientMasterDataReceivedSubject = new Subject<string>();
+    public patientAdmissionDataReceivedSubject = new Subject<string>();
+
     public ServerConnectionSubject = new Subject<boolean>();
 
     constructor() {
@@ -38,10 +41,7 @@ export class WorkerService {
         const message: ServerWorkerEventDataModel = messageEvent.data;
         switch (message.msgtype) {
             case SERVER_WORKER_EVENT_MSG_TYPE.DATA_RECEIVED:
-                message.data.forEach(element => {
-                    console.log('subject triggered');
-                    this.DataReceivedSubject.next(element);
-                });
+                this.handleDataReceived(message.data);
                 break;
             case SERVER_WORKER_EVENT_MSG_TYPE.SERVER_CONNECTED:
                 this.ServerConnectionSubject.next(true);
@@ -54,5 +54,20 @@ export class WorkerService {
 
     closeServerDataProcessorWorker() {
         this.ServerDataProcessorWorker.terminate();
+    }
+
+    handleDataReceived(data: ServerDataStoreDataModel[]) {
+        data.forEach(item => {
+            console.log('subject triggered');
+
+            switch (item.datastore) {
+                case SYNC_STORE.PATIENT_MASTER:
+                    this.patientMasterDataReceivedSubject.next();
+                    break;
+                case SYNC_STORE.PATIENT_ADMISSION:
+                    this.patientAdmissionDataReceivedSubject.next();
+                    break;
+            }
+        });
     }
 }
