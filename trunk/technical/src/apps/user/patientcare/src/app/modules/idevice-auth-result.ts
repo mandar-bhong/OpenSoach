8e7eb64794ef41059b9db92e813f5639294b52bc
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-04 10:22:21 +0000

    exported interface for handling the auth result from the shared access code.

--- trunk/technical/src/apps/user/patientcare/src/app/modules/idevice-auth-result.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/modules/idevice-auth-result.ts
@@ -1,5 +1,5 @@
 export interface IDeviceAuthResult {
-    onDeviceAuthSuccess(userid: number);
-    onDeviceAuthError(error: any);
-    onSubmitDiscarded();
+    onDeviceAuthSuccess(userid: number): void;
+    onDeviceAuthError(error: any): void;
+    onSubmitDiscarded(): void;
 }
