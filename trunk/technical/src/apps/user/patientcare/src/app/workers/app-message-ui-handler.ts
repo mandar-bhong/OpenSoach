800848211f5822f00ccd3d7ff9a8b858735cf7b5
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-13 12:46:24 +0000

    Moved code to notify sync to app-message-handler.ts.

--- trunk/technical/src/apps/user/patientcare/src/app/workers/app-message-ui-handler.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/workers/app-message-ui-handler.ts
@@ -2,8 +2,6 @@ import { AppMessageHandler } from "./app-message-handler.js";
 import { ServerDataStoreDataModel } from "../models/api/server-data-store-data-model.js";
 import { ServerWorkerEventDataModel } from "../models/api/server-worker-event-data-model.js";
 import { IDatastoreModel } from "../models/db/idatastore-model.js";
-import { SyncStoreManager } from "./sync-store-manager.js";
-
 export class AppMessageUIHandler extends AppMessageHandler {
 
     constructor() {
@@ -15,7 +13,6 @@ export class AppMessageUIHandler extends AppMessageHandler {
         this.saveToDataStore().then(() => {
             this.notifyUI();
             this.notifySync();
-            SyncStoreManager.syncToServerChanged(msg.datastore);
         });
 
     }
