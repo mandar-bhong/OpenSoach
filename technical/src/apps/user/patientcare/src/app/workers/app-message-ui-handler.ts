ff1aa5e2d6d2ca014a7ca6373a960159355f73f0
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-14 07:55:10 +0000

    Reverted code from revision 1694. SyncStoreManager is not working inside app-message-handler.ts, it might be related to cross reference in server helper and syncstore manager.

--- technical/src/apps/user/patientcare/src/app/workers/app-message-ui-handler.ts
+++ technical/src/apps/user/patientcare/src/app/workers/app-message-ui-handler.ts
@@ -2,6 +2,8 @@ import { AppMessageHandler } from "./app-message-handler.js";
 import { ServerDataStoreDataModel } from "../models/api/server-data-store-data-model.js";
 import { ServerWorkerEventDataModel } from "../models/api/server-worker-event-data-model.js";
 import { IDatastoreModel } from "../models/db/idatastore-model.js";
+import { SyncStoreManager } from "./sync-store-manager.js";
+
 export class AppMessageUIHandler extends AppMessageHandler {
 
     constructor() {
@@ -13,6 +15,7 @@ export class AppMessageUIHandler extends AppMessageHandler {
         this.saveToDataStore().then(() => {
             this.notifyUI();
             this.notifySync();
+            SyncStoreManager.syncToServerChanged(msg.datastore);
         });
 
     }
