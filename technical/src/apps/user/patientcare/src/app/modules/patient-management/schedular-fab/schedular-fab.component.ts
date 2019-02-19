00bb6386ddb3472ac1501e286c049ca225711c55
sarjerao.ghadage <sarjerao.ghadage@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-19 09:59:37 +0000

    camera permission updated in doctor orders component

---
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/schedular-fab/schedular-fab.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalDialogParams } from 'nativescript-angular/modal-dialog';
+import { MedicineChartComponent } from '../charts/medicine-chart/medicine-chart.component';
+import { IntakeChartComponent } from '../charts/intake-chart/intake-chart.component';
+import { MonitorChartComponent } from '../charts/monitor-chart/monitor-chart.component';
+
+@Component({
+	moduleId: module.id,
+	selector: 'schedular-fab',
+	templateUrl: './schedular-fab.component.html',
+	styleUrls: ['./schedular-fab.component.css']
+})
+
+export class SchedularFabComponent implements OnInit {
+	constructor(private params: ModalDialogParams) { }
+	ngOnInit() { }
+	
+	medicineForm() {
+		this.params.closeCallback(MedicineChartComponent);
+	}
+	intakeForm() {
+		this.params.closeCallback(IntakeChartComponent);
+	}
+	monitorForm() {
+		this.params.closeCallback(MonitorChartComponent);
+	}
+	closeDialog() {
+		console.log('in close Dialog');
+		this.params.closeCallback();
+	}
+
+}
