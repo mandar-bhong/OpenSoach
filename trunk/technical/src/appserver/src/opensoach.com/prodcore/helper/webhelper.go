08741717634fc28dc5d696aeeb1c88655bc6b700
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-20 09:54:25 +0000

    Description: 1. implemented document store methods 2. added device session helper methods 3. added device prepare execution context methods Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/prodcore/helper/webhelper.go
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/helper/webhelper.go
@@ -90,6 +90,80 @@ func PrepareExecutionReqData(osContext *gcore.Context, ginContext *gin.Context, 
 
 }
 
+func PrepareDeviceExecutionData(osContext *gcore.Context, ginContext *gin.Context) (bool, interface{}) {
+	dataModel := &gmodels.DeviceExecutionContext{}
+
+	isSessionSuccess, deviceInfo := DeviceSessionGet(osContext, ginContext)
+
+	if !isSessionSuccess {
+		errorData := gmodels.APIResponseError{}
+		errorData.Code = gmodels.MOD_OPER_ERR_INPUT_CLIENT_DATA
+		return false, errorData
+	}
+
+	dataModel.DeviceSessionInfo = *deviceInfo
+
+	return true, dataModel
+
+}
+
+func PrepareDeviceExecutionReqData(osContext *gcore.Context, ginContext *gin.Context, pClientReq interface{}) (bool, interface{}) {
+
+	dataModel := &gmodels.DeviceExecutionContext{}
+
+	if ginContext.Request.Method == http.MethodGet {
+
+		jsonData := ginContext.Query("params")
+
+		if jsonData == "" { // Expected Data but no data received
+			errorData := gmodels.APIResponseError{}
+			errorData.Code = gmodels.MOD_OPER_ERR_INPUT_CLIENT_DATA
+			return false, errorData
+		}
+
+		jsonDecodeErr := json.Unmarshal([]byte(jsonData), pClientReq)
+
+		if jsonDecodeErr != nil {
+			errorData := gmodels.APIResponseError{}
+			errorData.Code = gmodels.MOD_OPER_ERR_INPUT_CLIENT_DATA
+			return false, errorData
+		}
+
+	} else {
+
+		err := ginContext.Bind(pClientReq)
+
+		if err != nil {
+			//logger.Log(MODULENAME, logger.ERROR, "Client data binding error: ", err.Error())
+			errorData := gmodels.APIResponseError{}
+			errorData.Code = gmodels.MOD_OPER_ERR_INPUT_CLIENT_DATA
+			return false, errorData
+		}
+	}
+
+	isSessionSuccess, deviceInfo := DeviceSessionGet(osContext, ginContext)
+
+	if !isSessionSuccess {
+		errorData := gmodels.APIResponseError{}
+		errorData.Code = gmodels.MOD_OPER_ERR_USER_SESSION_NOT_AVAILABLE
+		return false, errorData
+	}
+
+	isUpdateSuccess := SessionUpdate(osContext, ginContext)
+
+	if !isUpdateSuccess {
+		errorData := gmodels.APIResponseError{}
+		errorData.Code = gmodels.MOD_OPER_ERR_SERVER
+		return false, errorData
+	}
+
+	dataModel.DeviceSessionInfo = *deviceInfo
+	dataModel.SessionToken = ginContext.GetHeader(gmodels.SESSION_CLIENT_HEADER_KEY)
+
+	return true, dataModel
+
+}
+
 func CommonWebRequestHandler(pContext *gin.Context, requestHandlerFunc RequestHandler) {
 	var isSuccess bool
 	var successErrorData interface{}
