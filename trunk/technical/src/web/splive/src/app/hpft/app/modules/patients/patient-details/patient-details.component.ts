52cae1bf3a6b6ab29f951a829cef8d7dd7e17e3e
chandan.pal <chandan.pal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-21 07:51:36 +0000

    Description : patient-details, tabs are working Bug ID: NA Reviewed By: Sanjay

---
+++ trunk/technical/src/web/splive/src/app/hpft/app/modules/patients/patient-details/patient-details.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, Subject } from 'rxjs';
+import { TranslatePipe } from '../../../../../shared/pipes/translate/translate.pipe';
+import { EditRecordBase, EDITABLE_RECORD_STATE, FORM_MODE } from '../../../../../shared/views/edit-record-base';
+import { PatientDataModel } from '../../../models/ui/patient-models';
+import { PatientService } from '../../../services/patient.service';
+
+@Component({
+  selector: 'app-patient-details',
+  templateUrl: './patient-details.component.html',
+  styleUrls: ['./patient-details.component.css']
+})
+export class PatientDetailsComponent extends EditRecordBase implements OnInit, OnDestroy {
+
+  @Input()
+  editRecordBase: EditRecordBase;
+  @Output()
+  editClick = new EventEmitter<null>();
+  routeSubscription: Subscription;
+  dataModal = new PatientDataModel();
+  selectedIndex: 0;
+  disableTab: boolean;
+  addid: number;
+  admissionIdSubscription: Subscription;
+
+  constructor(
+    private patientService: PatientService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private translatePipe: TranslatePipe,
+  ) {
+    super();
+    this.iconCss = 'fa fa-user';
+    this.pageTitle = 'Personal Detail';
+  }
+
+  ngOnInit() {
+    this.routeSubscription = this.route.queryParams.subscribe(params => {
+      this.subTitle = this.translatePipe.transform('OPERATOR_ADD_MODE_TITLE');
+      this.callbackUrl = params['callbackurl'];
+    });
+    this.admissionIdSubscription = this.patientService.admissionIdReceived.subscribe((value) => {
+      this.addid = value;
+      this.HideTab();
+    });
+    if (this.patientService.selcetdIndex && this.patientService.selcetdIndex != null) {
+      this.changeTab(this.patientService.selcetdIndex);
+    }
+    this.route.queryParams.subscribe(params => {
+      const id = params['id'];
+      this.addid = params['addid'];
+      this.HideTab();
+    });
+  }
+
+  HideTab() {
+    if (this.addid != null) {
+      this.disableTab = true;
+    } else {
+      this.disableTab = false;
+    }
+  }
+
+
+  changeTab(value) {
+    this.selectedIndex = value;
+  }
+
+  close() {
+    this.router.navigate(['patients'], { queryParams: { callbackurl: 'patients' }, skipLocationChange: true });
+  }
+
+  closeForm() {
+    this.router.navigate([this.callbackUrl], { skipLocationChange: true });
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+    if (this.admissionIdSubscription) { this.admissionIdSubscription.unsubscribe(); }
+  }
+
+}
