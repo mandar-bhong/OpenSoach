724aeef097a59eb30aa26224fd01f498e2c8c9b2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-02-22 09:23:46 +0000

    Description: added treatment,pathology models Change Log: Bug ID:

---
+++ technical/src/apps/user/patientcare/src/app/models/db/pathology-record-doc-model.ts
@@ -0,0 +1,12 @@
+import { IDatastoreModel } from "./idatastore-model";
+
+export class PathologyRecordDocDatastoreModel implements IDatastoreModel {
+    uuid: string;
+    sync_pending: number;
+    client_updated_at: Date;
+    pathology_record_uuid:string;
+    document_uuid:string;
+    getModelValues(): any[] {
+        return [this.pathology_record_uuid,this.document_uuid];
+    }
+}
