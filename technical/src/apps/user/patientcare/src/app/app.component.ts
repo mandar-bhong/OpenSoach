ec7c57eb10ec9bb977ae18e5cc882be0f8a82de5
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-05 09:29:44 +0000

    Added startup-page. moved starup code to app-startup.service.ts.

--- technical/src/apps/user/patientcare/src/app/app.component.ts
+++ technical/src/apps/user/patientcare/src/app/app.component.ts
@@ -1,21 +1,11 @@
-import { Component, OnInit, OnDestroy, NgZone } from "@angular/core";
-import { DatabaseSchemaService } from "./services/offline-store/database-schema.service";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { WorkerService } from "./services/worker.service";
-import { Subscription } from "rxjs";
-import { ServerDataProcessorMessageModel } from "./models/api/server-data-processor-message-model";
-import { SERVER_WORKER_MSG_TYPE, API_SPL_BASE_URL } from "~/app/app-constants";
-import * as appSettings from "tns-core-modules/application-settings";
-import { APP_MODE } from "./app-constants";
-import { AppGlobalContext } from "./app-global-context";
-import { RouterExtensions } from "nativescript-angular/router";
-import { PlatformHelper } from "./helpers/platform-helper";
-import { PassDataService } from "./services/pass-data-service";
-import { ServerApiInterfaceService } from "./services/server-api-interface.service";
 //TODO: DONT Remove websocket require, if this line is removed. the worker doesnt get access to nativescript-websockets.
 // this is a temporary fix and need to be handled through webpack config.
 var WS = require('nativescript-websockets');
 import * as trace from 'trace';
 import { TraceCustomCategory } from "./helpers/trace-helper";
+import { AppStartupService } from "./services/app-startup.service";
 
 @Component({
     moduleId: module.id,
@@ -24,169 +14,17 @@ import { TraceCustomCategory } from "./helpers/trace-helper";
 })
 export class AppComponent implements OnInit, OnDestroy {
 
-    public chatBox: string;
-    private internetConnectionSubscription: Subscription;
-    constructor(private databaseSchemaService: DatabaseSchemaService,
-        private zone: NgZone,
-        private workerService: WorkerService,
-        private routerExtensions: RouterExtensions,
-        private passDataService: PassDataService,
-        private serverApiInterfaceService: ServerApiInterfaceService) {
-
-        // init PlatformHelper
-        PlatformHelper.init();
-        this.databaseSchemaService.setOfflineDB().then(() => {
-            this.onDbCreate();
-        });
-    }
-
-    onDbCreate() {
-        // Initialize the worker here
-        this.workerService.initServerDataProcessorWorker();
-        this.checkIfLoggedIn();
+    constructor(private appStartupService: AppStartupService,
+        private workerService: WorkerService) {
+        trace.write("in app component constructor", TraceCustomCategory.APP_START, trace.messageType.info);
+        this.appStartupService.init();
     }
 
     ngOnInit() {
-        console.log('in app component init');
+        trace.write("in app component ngOnInit", TraceCustomCategory.APP_START, trace.messageType.info);
     }
 
     ngOnDestroy() {
         this.workerService.closeServerDataProcessorWorker();
-
-        this.internetConnectionSubscription.unsubscribe();
     }
-
-    checkIfLoggedIn() {
-        // read appSetting to get AUTH_TOKEN && APP_MODE
-        // if APP_MODE doesnt exist call checkIfDeviceIsRegistered()
-        // if APP_MODE exists && AUTH_TOKEN exists
-        // check if token is still valid
-        // if valid call initAppStart()
-        // else call checkIfDeviceIsRegistered()
-
-        const appMode = appSettings.getNumber("APP_MODE", APP_MODE.NONE);
-        const token = appSettings.getString("AUTH_TOKEN");
-
-        console.log("AUTH_TOKEN", token);
-        console.log("appMode", appMode);
-
-        if (appMode == APP_MODE.NONE) {
-            this.checkIfDeviceIsRegistered();
-        } else {
-            if (appMode == APP_MODE.SHARED_DEVICE && token != null) {
-                this.serverApiInterfaceService.get(API_SPL_BASE_URL + "/v1/validateauthtoken",
-                    {
-                        token: token
-                    })
-                    .then((result) => {
-                        console.log("token validate success");
-                        // setting context if token validated
-                        AppGlobalContext.Token = token;
-                        console.log("AppGlobalContext.Token", AppGlobalContext.Token);
-
-                        // pass device token for any one access
-                        AppGlobalContext.WebsocketUrl = appSettings.getString("WEB_SOCKET_URL");
-                        trace.write(`AppGlobalContext.WebsocketUrl: ${AppGlobalContext.WebsocketUrl}`, TraceCustomCategory.APP_START, trace.messageType.info);
-                        this.initAppStart();
-                    }, (error) => {
-                        if (!error.handled) {
-                            trace.write(`token validate error: ${error}`, trace.categories.Error, trace.messageType.error);
-                            this.checkIfDeviceIsRegistered();
-                        }
-                    });
-            }
-
-        }
-
-
-    }
-
-    getSerialNumber(): string {
-        //TODO: Read the serial number
-        // Set the Serial Number in AppGlobalContext
-
-        const serialNumber = "1234567890123456";
-        return serialNumber;
-
-    }
-
-    checkIfDeviceIsRegistered() {
-        //TODO: Call HTTP API to check if device is registered. getSerialNumber()
-        // Handle the response        
-        //  Device is registered && is shared device
-        // save token in appSetting and AppGlobalContext
-        // call initAppStart();
-        //Else
-        // Navigate to login page
-
-
-        // const SerialNo = PlatformHelper.API.getSerialNumber();
-        const SerialNo = this.getSerialNumber();
-        AppGlobalContext.SerialNumber = SerialNo;
-
-        console.log("SerialNo:", SerialNo);
-
-        this.serverApiInterfaceService.post(API_SPL_BASE_URL + "/v1/endpoint/deviceauthorization",
-            {
-                'serialno': SerialNo,
-                'prodcode': 'SPL_HPFT'
-            })
-            .then(
-                res => {
-                    console.log("POST Request is successful ", res);
-                    this.handleDevAuthResponse(res);
-                }, (error) => {
-                    if (!error.handled) {
-                        console.error('deviceauthorization in error', error);
-                        this.routerExtensions.navigate(['login']);
-                    }
-                }
-            );
-
-    }
-
-    handleDevAuthResponse(resData) {
-        appSettings.setNumber("APP_MODE", APP_MODE.SHARED_DEVICE);
-        AppGlobalContext.AppMode = APP_MODE.SHARED_DEVICE;
-        appSettings.setString("AUTH_TOKEN", resData.token);
-        appSettings.setString("WEB_SOCKET_URL", resData.locationurl);
-        AppGlobalContext.Token = resData.token;
-        AppGlobalContext.WebsocketUrl = resData.locationurl;
-        console.log("AppGlobalContext.Token", AppGlobalContext.Token);
-        this.initAppStart();
-    }
-
-    initAppStart() {
-        // post message to worker to connect websocket
-        // navigate to patient listing page
-
-        console.log('in initappStart');
-        const initModel = new ServerDataProcessorMessageModel();
-        initModel.msgtype = SERVER_WORKER_MSG_TYPE.INIT_SERVER_INTERFACE;
-        initModel.data = {};
-        initModel.data.WebsocketUrl = AppGlobalContext.WebsocketUrl;
-        initModel.data.Token = AppGlobalContext.Token;
-        this.workerService.postMessageToServerDataProcessorWorker(initModel);
-        this.routerExtensions.navigate(['home']);
-    }
-
-    buildUrl(url, parameters) {
-        let qs = "";
-        for (const key in parameters) {
-            if (parameters.hasOwnProperty(key)) {
-                const value = parameters[key];
-                qs +=
-                    "{" + "\"" + key + "\"" + ":" + "\"" + value + "\"" + "&";
-            }
-        }
-        if (qs.length > 0) {
-            qs = qs.substring(0, qs.length - 1); //chop off last "&"
-            url = url + "?params=" + qs + "}";
-        }
-
-        console.log("url", url);
-
-        return url;
-    }
-
 }
