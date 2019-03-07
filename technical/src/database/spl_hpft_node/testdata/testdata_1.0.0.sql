e404aa9768e9ae64381547a99c76c22f8411b096
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-07 09:40:22 +0000

    Description: added patient personal,medical tbl test data Bug ID:

--- technical/src/database/spl_hpft_node/testdata/testdata_1.0.0.sql
+++ technical/src/database/spl_hpft_node/testdata/testdata_1.0.0.sql
@@ -88,18 +88,53 @@ VALUES
 
 INSERT INTO `spl_hpft_patient_admission_tbl` (`uuid`, `cpm_id_fk`, `patient_id_fk`, `patient_reg_no`, `bed_no`, `status`, `sp_id_fk`, `dr_incharge`, `admitted_on`, `updated_by`) 
 VALUES 
-('PA001', '3', 1, 'P12B12213', '3A/312', '1', '3', '8', '2018-12-04 14:37:53', 1),
-('PA002', '3', 2, 'P12B12214', '3B/323', '1', '3', '8', '2018-12-04 12:47:53', 1),
-('PA003', '3', 3, 'P12B12215', '2A/643', '1', '3', '8', '2018-12-04 09:17:53', 1),
-('PA004', '3', 4, 'P12B12216', '4A/415', '2', '3', '8', '2018-12-04 11:00:53', 1),
-('PA005', '3', 5, 'P12B12217', '5A/616', '3', '3', '8', '2018-12-04 01:11:53', 1),
-('PA006', '3', 6, 'P12B12218', '6A/317', '1', '3', '8', '2018-12-04 14:32:53', 1),
-('PA007', '3', 7, 'P12B12219', '7A/312', '2', '7', '8', '2018-12-04 16:44:53', 1),
-('PA008', '3', 8, 'P12B12210', '3A/319', '3', '7', '8', '2018-12-04 11:12:53', 1),
-('PA009', '3', 9, 'P12B12221', '8A/314', '2', '7', '8', '2018-12-04 04:54:53', 1),
-('PA010', '3', 10, 'P12B12222', '4A/309', '1', '7', '8', '2018-12-04 15:55:53', 1),
-('PA011', '3', 11, 'P12B12223', '2B/231', '4', '8', '8', '2018-12-04 21:35:53', 1),
-('PA012', '3', 12, 'P12B12224', '2B/232', '1', '8', '8', '2018-12-04 19:33:53', 1);
+('PA001', '3', 1, 'P12B12213', '3A/312', '1', '3', '8', '2018-12-04 14:37:53', 2),
+('PA002', '3', 2, 'P12B12214', '3B/323', '1', '3', '8', '2018-12-04 12:47:53', 2),
+('PA003', '3', 3, 'P12B12215', '2A/643', '1', '3', '8', '2018-12-04 09:17:53', 2),
+('PA004', '3', 4, 'P12B12216', '4A/415', '2', '3', '8', '2018-12-04 11:00:53', 2),
+('PA005', '3', 5, 'P12B12217', '5A/616', '2', '3', '8', '2018-12-04 01:11:53', 2),
+('PA006', '3', 6, 'P12B12218', '6A/317', '1', '3', '8', '2018-12-04 14:32:53', 2),
+('PA007', '3', 7, 'P12B12219', '7A/312', '3', '7', '8', '2018-12-04 16:44:53', 2);
+
+
+--
+-- Dumping data for table `spl_hpft_patient_personal_details_tbl`
+--
+
+INSERT INTO `spl_hpft_patient_personal_details_tbl` (`cpm_id_fk`, `patient_id`, `admission_id_fk`, `uuid`, `age`, `person_accompanying`, `updated_by`) 
+VALUES 
+('3', '1', '1', 'PPD001', '22', '{"name": "Ashish", "gender": 1, "contact": "9843xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "9923xxxxxx", "relationshipwithpatient": "cousin"}', '2'),
+('3', '2', '2', 'PPD002', '22', '{"name": "Sanket", "gender": 1, "contact": "9712xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "8123xxxxxx", "relationshipwithpatient": "cousin"}', '2'),
+('3', '3', '3', 'PPD003', '22', '{"name": "Ashish", "gender": 1, "contact": "9932xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "8993xxxxxx", "relationshipwithpatient": "cousin"}', '2'),
+('3', '4', '4', 'PPD004', '22', '{"name": "Rohit", "gender": 2, "contact": "8812xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "9901xxxxxx", "relationshipwithpatient": "cousin"}', '2'),
+('3', '5', '5', 'PPD005', '22', '{"name": "Priya", "gender": 2, "contact": "9453xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "9456xxxxxx", "relationshipwithpatient": "cousin"}', '2'),
+('3', '6', '6', 'PPD006', '22', '{"name": "Sonal", "gender": 2, "contact": "8663xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "8723xxxxxx", "relationshipwithpatient": "cousin"}', '2'),
+('3', '7', '7', 'PPD007', '22', '{"name": "Kaushik", "gender": 1, "contact": "9813xxxxxx", "pesrsonage": "23", "personaddress": "warje", "alternatecontact": "8884xxxxxx", "relationshipwithpatient": "cousin"}', '2');
+
+
+--
+-- Dumping data for table `spl_hpft_patient_medical_details_tbl`
+--
+
+ INSERT INTO `spl_hpft_patient_medical_details_tbl` (`uuid`, `cpm_id_fk`, `patient_id`, `admission_id_fk`, `present_complaints`, `reason_for_admission`, `history_present_illness`, `past_history`, `treatment_before_admission`, `investigation_before_admission`, `family_history`, `allergies`, `personal_history`, `updated_by`) 
+ VALUES 
+  ('PMD001', '3', '1', '1', 
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "throat infection"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "throat infection"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "history of present illness"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "past history"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "treatment"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "investigation"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "family history"}]}',
+  '{"data": [{"date": "2019-03-07T08:52:36.474Z", "text": "allergies"}]}',
+ 	'{"data":{"weight":{"weight":40,"weight_tendency":"Increasing"},"alcohol":{"applicable":true,"quantity":"30 ml","remarks":"drinks sometimes"},"smoking":{"applicable":false,"quantity":null,"remarks":null},"others":null}}',
+  	'2'),
+ ('PMD002', '3', '1', '1', null, null, null, null, null, null, null, null, null, '2'),
+ ('PMD003', '3', '1', '1', null, null, null, null, null, null, null, null, null, '2'),
+ ('PMD004', '3', '1', '1', null, null, null, null, null, null, null, null, null, '2'),
+ ('PMD005', '3', '1', '1', null, null, null, null, null, null, null, null, null, '2'),
+ ('PMD006', '3', '1', '1', null, null, null, null, null, null, null, null, null, '2'),
+ ('PMD007', '3', '1', '1', null, null, null, null, null, null, null, null, null, '2');
 
 
 
