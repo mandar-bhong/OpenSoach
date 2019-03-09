9867689cdcc259c52e7b8445774d976f4bc5af7e
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-09 07:43:18 +0000

    Description: notify apply sync to source device added Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/prodcore/services/packetbld.go
+++ trunk/technical/src/appserver/src/opensoach.com/prodcore/services/packetbld.go
@@ -27,30 +27,28 @@ func (r *PacketbldService) Handle(serctx *ServiceContext) error {
 
 	for _, token := range r.ServiceRuntime.Tokens {
 
-		if token != r.ServiceConfig.SourceToken {
+		isSuccess, deviceTokenModel, _ := pchelper.CacheGetDeviceInfo(r.Repo.Context.Master.Cache, token)
+		if isSuccess == false {
+			logger.Context().LogError(SUB_MODULE_NAME, logger.Normal, "Failed to get device token model from cache.", nil)
+			return errors.New("Failed to get device token model from cache.")
+		}
 
-			isSuccess, deviceTokenModel, _ := pchelper.CacheGetDeviceInfo(r.Repo.Context.Master.Cache, token)
-			if isSuccess == false {
-				logger.Context().LogError(SUB_MODULE_NAME, logger.Normal, "Failed to get device token model from cache.", nil)
-				return errors.New("Failed to get device token model from cache.")
-			}
+		val, ok := r.ServiceRuntime.DeviceLocationMap[deviceTokenModel.DevID]
+		if ok {
+			for _, each := range val {
+				devPacket := gmodels.DevicePacket{}
+				devPacket.Header = header
+				devPacket.Header.SPID = each
+				devPacket.Payload = r.ServiceConfig.DestinationData
+				_, jsonpack := ghelper.ConvertToJSON(devPacket)
 
-			val, ok := r.ServiceRuntime.DeviceLocationMap[deviceTokenModel.DevID]
-			if ok {
-				for _, each := range val {
-					devPacket := gmodels.DevicePacket{}
-					devPacket.Header = header
-					devPacket.Header.SPID = each
-					devPacket.Payload = r.ServiceConfig.DestinationData
-					_, jsonpack := ghelper.ConvertToJSON(devPacket)
+				packet.Token = token
+				packet.Packet = jsonpack
 
-					packet.Token = token
-					packet.Packet = jsonpack
-
-					r.ServiceResult.DestinationPackets = append(r.ServiceResult.DestinationPackets, packet)
-				}
+				r.ServiceResult.DestinationPackets = append(r.ServiceResult.DestinationPackets, packet)
 			}
 		}
+
 	}
 
 	if r.NextHandler != nil {
