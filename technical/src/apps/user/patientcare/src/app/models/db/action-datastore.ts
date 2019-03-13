45454d9c6bcecc2e9c20731dcb772ab0970bbfa2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-13 14:21:53 +0000

    Description: added action tbl sync code Change Log: Bug ID:

--- technical/src/apps/user/patientcare/src/app/models/db/action-datastore.ts
+++ technical/src/apps/user/patientcare/src/app/models/db/action-datastore.ts
@@ -7,10 +7,13 @@ export class ActionDataStoreModel implements IDatastoreModel {
     conf_type_code: string;
     schedule_uuid: string;
     scheduled_time: string;
+    is_deleted: number;
+    updated_by:number;
+    updated_on:Date;
     sync_pending: number;
     client_updated_at: Date;
     getModelValues(): any[] {
-        return [this.uuid, this.admission_uuid, this.conf_type_code, this.schedule_uuid, this.scheduled_time, this.sync_pending,this.client_updated_at];
+        return [this.uuid, this.admission_uuid, this.conf_type_code, this.schedule_uuid, this.scheduled_time, this.is_deleted,this.updated_by,this.updated_on, this.sync_pending,this.client_updated_at];
     }
 }
 
