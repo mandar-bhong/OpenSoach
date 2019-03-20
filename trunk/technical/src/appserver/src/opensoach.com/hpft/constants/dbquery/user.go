fd6b8260a53f12c829d2318c264e1547af231830
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-20 06:06:30 +0000

    Description: modified query - get doctor list Change Log: Bug ID:

--- trunk/technical/src/appserver/src/opensoach.com/hpft/constants/dbquery/user.go
+++ trunk/technical/src/appserver/src/opensoach.com/hpft/constants/dbquery/user.go
@@ -5,6 +5,6 @@ const QUERY_SELECT_DOCTOR_USERS = `select id,usr_name,fname,lname,urole_code,uro
 inner join spl_master_usr_cpm_tbl ucpm on usr.id = ucpm.user_id_fk
 inner join spl_master_usr_details_tbl usrd on usr.id = usrd.usr_id_fk
 inner join spl_master_user_role_tbl urole on urole.id = ucpm.urole_id_fk
-where urole.urole_code in ('EX_DOC','IN_DOC') and ucpm.cpm_id_fk = 3
+where urole.urole_code in ('EX_DOC','IN_DOC') and ucpm.cpm_id_fk = ?
 ) as tbl
 group by id`
