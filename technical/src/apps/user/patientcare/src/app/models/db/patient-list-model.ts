7fbb91bc76859989e62d06ba4bd6acedfaf965d4
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-26 12:55:45 +0000

    Removed unwanted column attended and all references from patient_admission_tbl.

--- technical/src/apps/user/patientcare/src/app/models/db/patient-list-model.ts
+++ technical/src/apps/user/patientcare/src/app/models/db/patient-list-model.ts
@@ -4,7 +4,6 @@ export class PatientListDBModel{
     bed_no: string;
     mob_no: string;
     status: number;
-    attended: Date;
     sp_uuid: string;
     patient_uuid: string;
     admission_uuid: string;
