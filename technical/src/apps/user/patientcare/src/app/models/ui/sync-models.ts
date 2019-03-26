2666ac0bac427c4a80be657ec994ade5adf10ca3
sanjay.sawant <sanjay.sawant@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-03-26 11:49:40 +0000

    changed sync_from_server_pending_time and sync_to_server_pending_time to string. set date to iso string

--- technical/src/apps/user/patientcare/src/app/models/ui/sync-models.ts
+++ technical/src/apps/user/patientcare/src/app/models/ui/sync-models.ts
@@ -8,7 +8,7 @@ export class SyncDataModel {
     last_synced: string;
     sync_type: number;
     sync_to_server_pending:number;
-    sync_to_server_pending_time:Date;
+    sync_to_server_pending_time:string;
     sync_from_server_pending:number;
-    sync_from_server_pending_time:Date;
+    sync_from_server_pending_time:string;
 }
