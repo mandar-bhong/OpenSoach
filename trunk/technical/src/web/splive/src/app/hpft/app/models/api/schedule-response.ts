742873c21452cc08ae504af9c89b2673dadd5826
chandan.pal <chandan.pal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-03 10:28:11 +0000

    Description : put startdate in models Bug ID: NA Reviewed By: Amol

--- trunk/technical/src/web/splive/src/app/hpft/app/models/api/schedule-response.ts
+++ trunk/technical/src/web/splive/src/app/hpft/app/models/api/schedule-response.ts
@@ -6,6 +6,7 @@ export class ScheduleDataResponse<T> {
     conftypecode: string;
     conf: T;
     enddate: string;
+    startdate: string;
     status: number;
 }
 export class SchedularConfigData {
