4c1fed06bff1a05fb5dd086fb2fc4c894969ba20
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-04 05:29:08 +0000

    Description: modified tratmentdoc tbl,pathology record doc tbl - made respective changes to models,queries Change Log: Bug ID:

--- technical/src/apps/user/patientcare/src/app/models/db/pathology-record-doc-model.ts
+++ technical/src/apps/user/patientcare/src/app/models/db/pathology-record-doc-model.ts
@@ -2,14 +2,14 @@ import { IDatastoreModel } from "./idatastore-model";
 
 export class PathologyRecordDocDatastoreModel implements IDatastoreModel {
     uuid: string;
-    sync_pending: number;
-    client_updated_at: string;
     pathology_record_uuid: string;
     document_uuid: string;
     document_name: string;
     doctype: string;
     updated_on: string;
+    sync_pending: number;
+    client_updated_at: string;
     getModelValues(): any[] {
-        return [this.pathology_record_uuid, this.document_uuid,this.document_name,this.doctype];
+        return [this.pathology_record_uuid, this.document_uuid,this.document_name,this.doctype,this.updated_on,this.sync_pending,this.client_updated_at];
     }
 }
--- technical/src/apps/user/patientcare/src/db-scripts/v1.sql
+++ technical/src/apps/user/patientcare/src/db-scripts/v1.sql
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS doctors_orders_tbl (uuid TEXT, admission_uuid TEXT, d
 CREATE TABLE IF NOT EXISTS usr_tbl (usr_id INTEGER, usr_name TEXT, urole_name Text, fname TEXT, lname TEXT ,updated_on TEXT, sync_pending INTEGER , client_updated_at TEXT);
 CREATE TABLE IF NOT EXISTS document_tbl (uuid TEXT, doc_path TEXT, doc_name TEXT, doc_type TEXT, datastore TEXT, updated_by INTEGER, updated_on TEXT, sync_pending INTEGER, client_updated_at TEXT );
 CREATE TABLE IF NOT EXISTS treatment_tbl (uuid TEXT, admission_uuid TEXT, treatment_done TEXT,treatment_performed_time TEXT, details TEXT, post_observation TEXT, updated_by INTEGER, updated_on TEXT, sync_pending INTEGER , client_updated_at TEXT);
-CREATE TABLE IF NOT EXISTS treatment_doc_tbl (treatment_uuid TEXT, document_uuid TEXT,document_name Text, doctype Text);
+CREATE TABLE IF NOT EXISTS treatment_doc_tbl (treatment_uuid TEXT, document_uuid TEXT,document_name Text, doctype Text,updated_on TEXT, sync_pending INTEGER, client_updated_at TEXT);
 CREATE TABLE IF NOT EXISTS pathology_record_tbl (uuid TEXT, admission_uuid TEXT, test_performed TEXT, test_performed_time TEXT, test_result TEXT, comments TEXT, updated_by INTEGER, updated_on TEXT, sync_pending INTEGER , client_updated_at TEXT);
-CREATE TABLE IF NOT EXISTS pathology_record_doc_tbl (pathology_record_uuid TEXT,document_uuid TEXT,document_name Text, doctype Text);
+CREATE TABLE IF NOT EXISTS pathology_record_doc_tbl (pathology_record_uuid TEXT,document_uuid TEXT,document_name Text, doctype Text,updated_on TEXT, sync_pending INTEGER, client_updated_at TEXT);
 INSERT INTO sync_tbl (store_name, sync_order, sync_type, sync_to_server_pending,sync_to_server_pending_time,sync_from_server_pending,sync_from_server_pending_time) VALUES ('mst_user_tbl', 100,1,0,'',0,'');
 INSERT INTO sync_tbl (store_name, sync_order, sync_type, sync_to_server_pending,sync_to_server_pending_time,sync_from_server_pending,sync_from_server_pending_time) VALUES ('document_tbl', 101,2,0,'',0,'');
 INSERT INTO sync_tbl (store_name, sync_order, sync_type, sync_to_server_pending,sync_to_server_pending_time,sync_from_server_pending,sync_from_server_pending_time) VALUES ('service_point_tbl', 200,1,0,'',0,'');
