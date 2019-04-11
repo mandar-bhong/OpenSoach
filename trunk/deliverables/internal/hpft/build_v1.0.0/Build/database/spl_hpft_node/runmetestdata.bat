888e621a330d3d59c37470fa687d409c2a09a087
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-11 06:54:19 +0000

    Added basic demo build of hpft

---
+++ trunk/deliverables/internal/hpft/build_v1.0.0/Build/database/spl_hpft_node/runmetestdata.bat
@@ -0,0 +1,17 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+echo Starting sql script executing
+
+::-------------Set sql parameter------------------
+SET Server=localhost
+SET User=root
+SET Password=welcome
+SET Database=spl_hpft_node_0001
+::----------------End ----------------------------
+
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -h%Server% -u%User% -p%Password% %Database% < %~dp0\testdata\testdata_1.0.0.sql
+
+
+SET /P uname=Please enter any key to exit:
+
+
---
+++ trunk/deliverables/internal/hpft/build_v1.0.0/Build/database/spl_vst_master/data/data_1.0.0.sql
@@ -0,0 +1,26 @@
+--
+-- Dumping data for table `spl_prod_master_sp_category_tbl`
+--
+
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (1,'Token Generation');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (2,'Job Creation');
+INSERT INTO `spl_prod_master_sp_category_tbl` (`id`,`spc_name`) VALUES (3,'Job Execution');
+
+--
+-- Dumping data for table `spl_prod_master_config`
+--
+
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Web.Service.Address',':92');
+
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Host','localhost');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Port','6379');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.Password','');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Cache.Address.DB','2');
+
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Host','localhost');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Port','6379');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.Password','');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Product.Que.Address.DB','2');
+INSERT INTO `spl_prod_master_config` (`config_key`,`config_value`) VALUES ('Spl.Prod.Base.Url','http://172.105.232.148');
+
+INSERT INTO `spl_prod_master_serv_conf_type_tbl` (`conf_type_code`) VALUES ('SERVICE_DAILY_CHART');
