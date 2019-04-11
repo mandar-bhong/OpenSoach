dca4654914c2440bee3137bbd01d37d12f3496e2
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-11 06:55:02 +0000

    Description: corrected prod url constants Change Log: Bug ID:

--- trunk/technical/src/apps/user/patientcare/src/app/app-constants.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/app-constants.ts
@@ -102,8 +102,8 @@ export enum ScheuldeStatus {
 // export const API_SPL_BASE_URL = "http://172.105.232.148/api";
 // export const API_APP_BASE_URL = "http://172.105.232.148:91/api";
 
-export const API_SPL_BASE_URL = "http://http://139.162.75.182:91/api";
-export const API_APP_BASE_URL = "http://http://139.162.75.182/api";
+export const API_SPL_BASE_URL = "http://139.162.75.182:91/api";
+export const API_APP_BASE_URL = "http://139.162.75.182/api";
 
 export const ACTION_MISSED_WINDOW = 3 * 60;
 export const ACTION_DELAYED_AFTER = 30;
