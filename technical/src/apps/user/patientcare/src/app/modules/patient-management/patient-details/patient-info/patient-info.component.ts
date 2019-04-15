2c295c643fbd546d7fda6b4eecf07b972f2cdf7c
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-15 11:40:01 +0000

    Description : patient details gender get id binding text Bug ID: NA Reviewed By: Sanjay

--- technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-details/patient-info/patient-info.component.ts
+++ technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-details/patient-info/patient-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PatientListService } from '~/app/services/patient-list/patient-list.service';
 import { PassDataService } from '~/app/services/pass-data-service';
 import { PatientDetailsModel } from '~/app/models/ui/patient-details';
+import { PERSON_ACCMPANYING_GENDER } from '~/app/app-constants';
 
 
 @Component({
@@ -30,9 +31,25 @@ export class PatientInfoComponent implements OnInit {
 					this.patientDetailsModel.fname = item.fname;
 					this.patientDetailsModel.lname = item.lname;
 					this.patientDetailsModel.blood_grp = item.blood_grp;
-					this.patientDetailsModel.gender = item.gender;
+					
 					this.patientDetailsModel.age = item.age;
 					this.patientDetailsModel.mob_no = item.mob_no;
+					let gender: any;
+					if (item.gender != null) {
+						switch( item.gender){
+						   case 0:
+						   gender = PERSON_ACCMPANYING_GENDER.GENDER_NOT_SELECTED;
+						   break;
+						   case 1:
+						   gender = PERSON_ACCMPANYING_GENDER.GENDER_MALE;
+						   break;
+						   case 2:
+						   gender= PERSON_ACCMPANYING_GENDER.GENDER_NOT_SELECTED;
+						   break;
+						}
+						this.patientDetailsModel.gender = gender;
+						
+					}
 				});
 			},
 			(error) => {
