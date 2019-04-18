dc7db02f895b39b0054240b74fe4edac6a2baae3
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-18 11:24:22 +0000

    Description: modified device sp deassociate model Change Log: Bug ID:

--- technical/src/appserver/src/opensoach.com/hpft/api/models/apiservicepoint.go
+++ technical/src/appserver/src/opensoach.com/hpft/api/models/apiservicepoint.go
@@ -18,5 +18,6 @@ type APIDevSpAsscociationRequest struct {
 
 type APIDevSpAsscociationRemoveRequest struct {
 	DevId int64 `db:"dev_id_fk" dbattr:"pri"  json:"devid"`
+	SpId  int64 `db:"sp_id_fk" dbattr:"pri"  json:"spid"`
 	CpmId int64 `db:"cpm_id_fk" json:"cpmid"`
 }
--- technical/src/appserver/src/opensoach.com/hpft/constants/dbquery/servicepoint.go
+++ technical/src/appserver/src/opensoach.com/hpft/constants/dbquery/servicepoint.go
@@ -2,7 +2,7 @@ package dbquery
 
 const QUERY_GET_SP_CATEGORY_SHORT_LIST = `select id,spc_name from spl_node_sp_category_tbl where cpm_id_fk=?`
 
-const QUERY_DELETE_DEV_SP_MAPPING_TABLE_ROW = `Delete From spl_node_dev_sp_mapping Where dev_id_fk = :dev_id_fk And cpm_id_fk = :cpm_id_fk`
+const QUERY_DELETE_DEV_SP_MAPPING_TABLE_ROW = `Delete From spl_node_dev_sp_mapping Where dev_id_fk = :dev_id_fk and sp_id_fk = :sp_id_fk  And cpm_id_fk = :cpm_id_fk`
 
 const QUERY_GET_NODE_SP_TABLE_TOTAL_FILTERED_COUNT = `Select count(*) as count from spl_node_sp_tbl sp 
 left join spl_node_dev_sp_mapping devsp  on devsp.sp_id_fk = sp.sp_id_fk
