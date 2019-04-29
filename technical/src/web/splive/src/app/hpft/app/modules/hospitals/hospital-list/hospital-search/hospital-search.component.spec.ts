e4c109d4b54fee5470ef2e7830adb2111ec3eb42
chandan.pal <chandan.pal@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-29 07:07:34 +0000

    Description :Complete laboratory functionality successfully Bug ID: NA Reviewed By: Mandar

---
+++ technical/src/web/splive/src/app/hpft/app/modules/hospitals/hospital-list/hospital-search/hospital-search.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HospitalSearchComponent } from './hospital-search.component';
+
+describe('HospitalSearchComponent', () => {
+  let component: HospitalSearchComponent;
+  let fixture: ComponentFixture<HospitalSearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HospitalSearchComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HospitalSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
