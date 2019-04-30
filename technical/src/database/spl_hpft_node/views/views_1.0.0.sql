172c435923b287723bc5d05410e0b2f53fd027f8
sumeet.karande <sumeet.karande@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-04-30 09:38:13 +0000

    Description: 1. modified sync config schema - removed column device type 2. modified sync config test data 3. modified monitored patient view Change Log: Bug ID:

--- technical/src/database/spl_hpft_node/views/views_1.0.0.sql
+++ technical/src/database/spl_hpft_node/views/views_1.0.0.sql
@@ -1,27 +1,3 @@
-
---
--- view_sync_select_patient_admission
---
-
-create view view_sync_select_patient_admission as
-
-select * from (
- 
-select padmsn.uuid,upmm.usr_id_fk,0 as patient_uuid,padmsn.patient_reg_no,padmsn.bed_no,padmsn.status,0 as sp_uuid,padmsn.dr_incharge,padmsn.admitted_on,padmsn.discharged_on,padmsn.updated_by,padmsn.updated_on, padmsn.client_updated_at from spl_hpft_patient_admission_tbl padmsn 
-inner join  spl_hpft_user_patient_monitor_mapping upmm on padmsn.sp_id_fk = upmm.sp_id_fk and padmsn.patient_id_fk = upmm.patient_id_fk
-
-union
-
-select padmsn.uuid,upmm.usr_id_fk,0 as patient_uuid,padmsn.patient_reg_no,padmsn.bed_no,padmsn.status,0 as sp_uuid,padmsn.dr_incharge,padmsn.admitted_on,padmsn.discharged_on,padmsn.updated_by,padmsn.updated_on, padmsn.client_updated_at from spl_hpft_patient_admission_tbl padmsn 
-inner join  spl_hpft_user_patient_monitor_mapping upmm on padmsn.sp_id_fk = upmm.sp_id_fk and  upmm.patient_id_fk is null
-
-union
-
-select padmsn.uuid,upmm.usr_id_fk,0 as patient_uuid,padmsn.patient_reg_no,padmsn.bed_no,padmsn.status,0 as sp_uuid,padmsn.dr_incharge,padmsn.admitted_on,padmsn.discharged_on,padmsn.updated_by,padmsn.updated_on, padmsn.client_updated_at from spl_hpft_patient_admission_tbl padmsn 
-inner join  spl_hpft_user_patient_monitor_mapping upmm on upmm.sp_id_fk is null and  upmm.patient_id_fk is null
-
-) t ;
-
 --
 -- view_get_monitor_patient
 --
@@ -48,6 +24,30 @@ select 1 as monitored, padmsn.id as admission_id,upmm.id as upmmid, patient.fnam
 inner join spl_hpft_patient_master_tbl patient on patient.id = padmsn.patient_id_fk 
 inner join  spl_hpft_user_patient_monitor_mapping upmm on upmm.sp_id_fk is null and  upmm.patient_id_fk is null
 inner join spl_node_sp_tbl sp on sp.sp_id_fk = padmsn.sp_id_fk
+
+) t ;
+
+
+--
+-- view_monitored_patients
+--
+
+create view view_monitored_patients as
+
+select * from (
+ 
+select padmsn.uuid,padmsn.id as admission_id,padmsn.patient_id_fk,padmsn.sp_id_fk,upmm.usr_id_fk   from spl_hpft_patient_admission_tbl padmsn 
+inner join  spl_hpft_user_patient_monitor_mapping upmm on padmsn.sp_id_fk = upmm.sp_id_fk and padmsn.patient_id_fk = upmm.patient_id_fk
+
+union
+
+select padmsn.uuid,padmsn.id as admission_id,padmsn.patient_id_fk,padmsn.sp_id_fk,upmm.usr_id_fk from spl_hpft_patient_admission_tbl padmsn 
+inner join  spl_hpft_user_patient_monitor_mapping upmm on padmsn.sp_id_fk = upmm.sp_id_fk and  upmm.patient_id_fk is null
+
+union
+
+select padmsn.uuid,padmsn.id as admission_id,padmsn.patient_id_fk,padmsn.sp_id_fk,upmm.usr_id_fk from spl_hpft_patient_admission_tbl padmsn 
+inner join  spl_hpft_user_patient_monitor_mapping upmm on upmm.sp_id_fk is null and  upmm.patient_id_fk is null
 
 ) t ;
 
