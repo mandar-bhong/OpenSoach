7313c2b1432115946dcd4f07324318832405bc32
amol.patil <amol.patil@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-05-31 11:44:35 +0000

    Description : create selection list custom control add all Bug ID: NA

--- trunk/technical/src/apps/user/patientcare/src/app/modules/shared-mudule.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/modules/shared-mudule.ts
@@ -1,12 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NetworkStatusComponent } from '~/app/network-status.component';
 import { PassDataService } from '~/app/services/pass-data-service';
+import { OsSelectionListComponent } from '../os-selection-list.component';
+
+import { NativeScriptUIListViewModule } from 'nativescript-ui-listview/angular/listview-directives';
+import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
 @NgModule({
-    declarations: [NetworkStatusComponent],
-    imports: [],
-    exports: [NetworkStatusComponent],
+    declarations: [
+        NetworkStatusComponent,
+        OsSelectionListComponent
+    ],
+    imports: [
+        NativeScriptUIListViewModule,
+        NativeScriptFormsModule,
+        NativeScriptCommonModule
+    ],
+    exports: [
+        NetworkStatusComponent,
+        OsSelectionListComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ],
+    entryComponents: [
+        OsSelectionListComponent
+    ], 
     providers: [],
 })
 export class sharedModule { }
