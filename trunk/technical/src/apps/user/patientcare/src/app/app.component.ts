cd98a1a4c7e411bcd6353046563c55e8cee4a524
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-06-14 10:02:12 +0000

    Removed android events as it is registered with main.ts

--- trunk/technical/src/apps/user/patientcare/src/app/app.component.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/app.component.ts
@@ -7,13 +7,7 @@ var bghttp = require("nativescript-background-http");
 import * as trace from 'tns-core-modules/trace';
 import { TraceCustomCategory } from "./helpers/trace-helper";
 import { AppStartupService } from "./services/app-startup.service";
-import * as app from 'tns-core-modules/application';
-import {
-    displayedEvent, exitEvent, launchEvent, lowMemoryEvent,
-    orientationChangedEvent, resumeEvent, suspendEvent, uncaughtErrorEvent,
-    ApplicationEventData, LaunchEventData, OrientationChangedEventData, UnhandledErrorEventData,
-    on as applicationOn, run as applicationRun
-} from "tns-core-modules/application";
+
 import { DatabaseService } from "./services/offline-store/database.service";
 
 @Component({
@@ -28,73 +22,7 @@ export class AppComponent implements OnInit, OnDestroy {
         private databaseService: DatabaseService) {
         trace.write("in app component constructor", TraceCustomCategory.APP_START, trace.messageType.info);
 
-
-        applicationOn(launchEvent, (args: LaunchEventData) => {
-            if (args.android) {
-                // For Android applications, args.android is an android.content.Intent class.
-                console.log("Launched Android application with the following intent: " + args.android + ".");
-            } else if (args.ios !== undefined) {
-                // For iOS applications, args.ios is NSDictionary (launchOptions).
-                console.log("Launched iOS application with options: " + args.ios);
-            }
-        });
-        
-        applicationOn(suspendEvent, (args: ApplicationEventData) => {
-            if (args.android) {
-                // For Android applications, args.android is an android activity class.
-                console.log("Activity: " + args.android);
-            } else if (args.ios) {
-                // For iOS applications, args.ios is UIApplication.
-                console.log("UIApplication: " + args.ios);
-            }
-        });
-        
-        applicationOn(resumeEvent, (args: ApplicationEventData) => {
-            if (args.android) {
-                // For Android applications, args.android is an android activity class.
-                console.log("Activity: " + args.android);
-            } else if (args.ios) {
-                // For iOS applications, args.ios is UIApplication.
-                console.log("UIApplication: " + args.ios);
-            }
-        });
-        
-        applicationOn(displayedEvent, (args: ApplicationEventData) => {
-            console.log("displayedEvent");
-        });
-        
-        applicationOn(orientationChangedEvent, (args: OrientationChangedEventData) => {
-            // "portrait", "landscape", "unknown"
-            console.log(args.newValue)
-        });
-        
-        applicationOn(exitEvent, (args: ApplicationEventData) => {
-            if (args.android) {
-                // For Android applications, args.android is an android activity class.
-                console.log("Activity: " + args.android);
-            } else if (args.ios) {
-                // For iOS applications, args.ios is UIApplication.
-                console.log("UIApplication: " + args.ios);
-            }
-        });
-        
-        applicationOn(lowMemoryEvent, (args: ApplicationEventData) => {
-            if (args.android) {
-                // For Android applications, args.android is an android activity class.
-                console.log("Activity: " + args.android);
-            } else if (args.ios) {
-                // For iOS applications, args.ios is UIApplication.
-                console.log("UIApplication: " + args.ios);
-            }
-        });
-        
-        applicationOn(uncaughtErrorEvent, function (args: UnhandledErrorEventData) {
-            console.log("Error: " + args.error);
-        });
-
-
-
-        this.appStartupService.init();
+        this.appStartupService.init(); 
     }
 
     ngOnInit() {
