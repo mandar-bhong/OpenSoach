b7b68473f73b360bdaf660fd5166605625d8590a
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-06-14 09:00:46 +0000

    Updated prod url and added method to get buildmode

--- technical/src/apps/user/patientcare/src/app/environments/environment.prod.ts
+++ technical/src/apps/user/patientcare/src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_SPL_BASE_URL: 'http://172.105.232.148/api',
-  API_APP_BASE_URL: 'http://172.105.232.148:91/api',
-  ERROR_REPORTING_URL: 'http://172.105.232.148:8086/write?db=spl'
+  API_SPL_BASE_URL: 'http://139.162.75.182:91/api', // this need to be updated with actual domain name
+  API_APP_BASE_URL: 'http://139.162.75.182/api',
+  ERROR_LOGGING_URL: 'http://139.162.75.182:8086/write?db=spl'  
 };
--- technical/src/apps/user/patientcare/src/app/helpers/common-helper.ts
+++ technical/src/apps/user/patientcare/src/app/helpers/common-helper.ts
@@ -1,12 +1,27 @@
-import { BuildMode } from "../app-constants";
+import { BuildMode } from "../app-constants.js";
 
 declare var process: any;
 export class CommonHelper {
-    static getEnvironmentVars(key: string): string {    
+    static getEnvironmentVars(key: string): string {
         if (typeof process !== 'undefined' && process && process.env) {
             return process.env[key];
         } else {
             return BuildMode.DEVELOPMENT
+        }
+    }
+
+    static getBuildEnvironment(): string {
+        if (typeof process !== 'undefined' && process && process.env) {
+            return process.env["buildmode"];
+        } else {
+            switch (process.env["buildmode"]) {
+                case "prod"://this flag need to pass from command line
+                    return BuildMode.PRODUCTION;
+               
+                default:
+                    return BuildMode.DEVELOPMENT
+            }
+
         }
     }
 }
