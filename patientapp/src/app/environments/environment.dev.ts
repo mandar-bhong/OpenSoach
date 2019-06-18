55b6d6c2b14f69151cdd0775d28ce806f4bb4aed
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-06-18 06:08:36 +0000

    Updated logging url var name

--- patientapp/src/app/environments/environment.dev.ts
+++ patientapp/src/app/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   API_SPL_BASE_URL: 'http://172.105.232.148/api',
   API_APP_BASE_URL: 'http://172.105.232.148:91/api',
-  ERROR_REPORTING_URL: 'http://172.105.232.148:8086/write?db=spl'
+  ERROR_LOGGING_URL: 'http://172.105.232.148:8086/write?db=spl'
 };
