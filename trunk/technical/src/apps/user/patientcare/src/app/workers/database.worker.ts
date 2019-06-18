b8ebde26af82fcfd023331b0e9cd98c51a4985d5
mandar.bhong <mandar.bhong@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-06-18 07:14:07 +0000

    Replace console with trace logging

--- trunk/technical/src/apps/user/patientcare/src/app/workers/database.worker.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/workers/database.worker.ts
@@ -1,9 +1,14 @@
 import "globals";
 import { DBWorkerTasks } from "./database-worker-tasks.js";
+import * as trace from 'tns-core-modules/trace'
+import { TraceCustomCategory } from "../helpers/trace-helper.js";
+
 const context: Worker = self as any;
 
 context.onmessage = msg => {
-    console.log("Inside DB TS worker....");
+
+    trace.write("Inside DB worker",TraceCustomCategory.DB_WORKER,trace.messageType.log);
+
     DBWorkerTasks.processMessage(msg.data);
 };
 
