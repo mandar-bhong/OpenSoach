b88064938142486aceb70882a09da19ea6214d25
sarjerao.ghadage <sarjerao.ghadage@9c6fb741-ff39-4f0a-917c-7ca4c78b9539>
2019-06-25 05:32:49 +0000

    patient address given in patient details.

--- trunk/technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-details/patient-info/patient-info.component.ts
+++ trunk/technical/src/apps/user/patientcare/src/app/modules/patient-management/patient-details/patient-info/patient-info.component.ts
@@ -14,46 +14,62 @@ import { PERSON_ACCMPANYING_GENDER } from '~/app/app-constants';
 
 export class PatientInfoComponent implements OnInit {
 	patientDetailsModel = new PatientDetailsModel();
+	patientAddress: string;
 	constructor(
 		private patientListService: PatientListService,
 		private passdataservice: PassDataService
 	) { }
 
-	ngOnInit() { 
+	ngOnInit() {
 		this.getAdmissionDetailsByUUID()
-		
+		this.getPersonAccompanyingByUUID();
 	}
 	public getAdmissionDetailsByUUID() {
 		this.patientListService.getPatientDetailsByUUID(this.passdataservice.getPatientID()).then(
 			(val) => {
 				val.forEach(item => {
-			
+
 					this.patientDetailsModel.fname = item.fname;
 					this.patientDetailsModel.lname = item.lname;
 					this.patientDetailsModel.blood_grp = item.blood_grp;
-					
+
 					this.patientDetailsModel.age = item.age;
 					this.patientDetailsModel.mob_no = item.mob_no;
 					let gender: any;
 					if (item.gender != null) {
-						switch( item.gender){
-						   case 0:
-						   gender = PERSON_ACCMPANYING_GENDER.GENDER_NOT_SELECTED;
-						   break;
-						   case 1:
-						   gender = PERSON_ACCMPANYING_GENDER.GENDER_MALE;
-						   break;
-						   case 2:
-						   gender= PERSON_ACCMPANYING_GENDER.GENDER_NOT_SELECTED;
-						   break;
+						switch (item.gender) {
+							case 0:
+								gender = PERSON_ACCMPANYING_GENDER.GENDER_NOT_SELECTED;
+								break;
+							case 1:
+								gender = PERSON_ACCMPANYING_GENDER.GENDER_MALE;
+								break;
+							case 2:
+								gender = PERSON_ACCMPANYING_GENDER.GENDER_NOT_SELECTED;
+								break;
 						}
 						this.patientDetailsModel.gender = gender;
-						
 					}
 				});
 			},
 			(error) => {
 				console.log("Patient details error:", error);
+			}
+		);
+	}
+	public getPersonAccompanyingByUUID() {
+		this.patientListService.getPersonAccompanyingByUUID(this.passdataservice.getAdmissionID()).then(
+			(val) => {				
+				if (val.length > 0 && val[0].other_details != null) {
+					const address = JSON.parse(val[0].other_details);
+					if (address && address.data) {
+						this.patientAddress = address.data.address;
+						console.log('this.patientAddress',	this.patientAddress);
+					}
+				}				
+			},
+			(error) => {
+				console.log("person_accompanying error:", error);
 			}
 		);
 	}
